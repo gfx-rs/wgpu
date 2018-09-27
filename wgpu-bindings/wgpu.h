c013fc94986bfa361ec51bc5a4b4cbe2dfb0a4b8
Dzmitry Malyshau <kvark@mozilla.com>
2018-09-27 19:04:17 -0400

    C example update, header, and better travis tests

--- wgpu-bindings/wgpu.h
+++ wgpu-bindings/wgpu.h
@@ -34,6 +34,10 @@ typedef WGPUId WGPUCommandBufferId;
 
 typedef WGPUId WGPUInstanceId;
 
+typedef struct {
+
+} WGPUCommandBufferDescriptor;
+
 typedef WGPUId WGPUShaderModuleId;
 
 typedef struct {
@@ -45,19 +49,30 @@ typedef struct {
   WGPUByteArray code;
 } WGPUShaderModuleDescriptor;
 
+typedef WGPUId WGPUQueueId;
+
 typedef struct {
   WGPUPowerPreference power_preference;
 } WGPUAdapterDescriptor;
 
-WGPUDeviceId wgpu_adapter_create_device(WGPUAdapterId adapter_id, WGPUDeviceDescriptor desc);
+WGPUDeviceId wgpu_adapter_create_device(WGPUAdapterId adapter_id, WGPUDeviceDescriptor _desc);
 
 WGPUComputePassId wgpu_command_buffer_begin_compute_pass(void);
 
-WGPURenderPassId wgpu_command_buffer_begin_render_pass(WGPUCommandBufferId command_buffer);
+WGPURenderPassId wgpu_command_buffer_begin_render_pass(WGPUCommandBufferId _command_buffer);
 
 WGPUInstanceId wgpu_create_instance(void);
+
+WGPUCommandBufferId wgpu_device_create_command_buffer(WGPUDeviceId device_id,
+                                                      WGPUCommandBufferDescriptor desc);
 
 WGPUShaderModuleId wgpu_device_create_shader_module(WGPUDeviceId device_id,
                                                     WGPUShaderModuleDescriptor desc);
 
+WGPUQueueId wgpu_device_get_queue(WGPUDeviceId device_id);
+
 WGPUAdapterId wgpu_instance_get_adapter(WGPUInstanceId instance_id, WGPUAdapterDescriptor desc);
+
+void wgpu_queue_submit(WGPUQueueId queue_id,
+                       const WGPUCommandBufferId *command_buffer_ptr,
+                       uintptr_t command_buffer_count);
