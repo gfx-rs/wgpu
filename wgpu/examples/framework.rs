7ff7eb2f227c8131b8659996f1074ad707ebef4d
Dzmitry Malyshau <kvarkus@gmail.com>
2020-03-26 12:33:50 -0400

    [rs] Change NDC Y axis

--- wgpu/examples/framework.rs
+++ wgpu/examples/framework.rs
@@ -4,7 +4,7 @@ use winit::event::WindowEvent;
 #[allow(unused)]
 pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     1.0, 0.0, 0.0, 0.0,
-    0.0, -1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
     0.0, 0.0, 0.5, 0.0,
     0.0, 0.0, 0.5, 1.0,
 );
