920c00931d3917a60badeb499b277a9b4549c685
Dzmitry Malyshau <kvarkus@gmail.com>
2020-04-09 23:25:16 -0400

    Fix tracking of the initial state during replacement.
    When multiple "replace" style transitions are happening,
    we weren't properly retaining the "first" state, which
    is required for proper stitching of command buffers.
    
    This logic is fixed and fortified with a new set of
    "change" and "merge" tests in the track module.

--- wgpu-core/src/command/render.rs
+++ wgpu-core/src/command/render.rs
@@ -1159,6 +1159,10 @@ impl<G: GlobalIdentityHandlerFactory> Global<G> {
             }
         }
 
+        log::trace!("Merging {:?} with the render pass", encoder_id);
+        unsafe {
+            raw.end_render_pass();
+        }
         super::CommandBuffer::insert_barriers(
             cmb.raw.last_mut().unwrap(),
             &mut cmb.trackers,
@@ -1168,7 +1172,6 @@ impl<G: GlobalIdentityHandlerFactory> Global<G> {
         );
         unsafe {
             cmb.raw.last_mut().unwrap().finish();
-            raw.end_render_pass();
         }
         cmb.raw.push(raw);
     }
