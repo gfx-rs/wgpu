c4be28fcd96f3c08bcf8f0081f8e4b53b5f2e840
Joshua Groves <josh@joshgroves.com>
2020-04-12 14:42:07 -0230

    [rs] Allow describe example to run on the web

--- wgpu/examples/describe/main.rs
+++ wgpu/examples/describe/main.rs
@@ -1,11 +1,4 @@
 /// This example shows how to describe the adapter in use.
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    env_logger::init();
-    futures::executor::block_on(run());
-}
-
-#[cfg(not(target_arch = "wasm32"))]
 async fn run() {
     let adapter = wgpu::Adapter::request(
         &wgpu::RequestAdapterOptions {
@@ -17,5 +10,25 @@ async fn run() {
     .await
     .unwrap();
 
+    #[cfg(not(target_arch = "wasm32"))]
     println!("{:?}", adapter.get_info())
+}
+
+fn main() {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+        futures::executor::block_on(run());
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        wasm_bindgen_futures::spawn_local(run());
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn wasm_main() {
+    main();
 }
