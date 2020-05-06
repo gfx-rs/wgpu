55b727e0c5e9b05caa8c69eb11792aebeda9d4e1
Dzmitry Malyshau <kvarkus@gmail.com>
2020-05-05 21:55:42 -0400

    [rs] Rustfmt pass on examples

--- wgpu/examples/hello-compute/main.rs
+++ wgpu/examples/hello-compute/main.rs
@@ -35,12 +35,15 @@ async fn execute_gpu(numbers: Vec<u32>) -> Vec<u32> {
         .unwrap();
 
     let (device, queue) = adapter
-        .request_device(&wgpu::DeviceDescriptor {
-            extensions: wgpu::Extensions {
-                anisotropic_filtering: false,
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions {
+                    anisotropic_filtering: false,
+                },
+                limits: wgpu::Limits::default(),
             },
-            limits: wgpu::Limits::default(),
-        }, None)
+            None,
+        )
         .await
         .unwrap();
 
