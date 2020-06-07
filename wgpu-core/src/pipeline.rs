6e1e2e037e81bfa45544d782623b47fd14b92522
Dzmitry Malyshau <kvarkus@gmail.com>
2020-06-06 22:57:02 -0400

    Validate stage inputs/outputs

--- wgpu-core/src/pipeline.rs
+++ wgpu-core/src/pipeline.rs
@@ -6,7 +6,7 @@ use crate::{
     binding_model::{BindEntryMap, BindGroupLayoutEntry, BindingType},
     device::RenderPassContext,
     id::{DeviceId, PipelineLayoutId, ShaderModuleId},
-    LifeGuard, RawString, RefCount, Stored, U32Array,
+    FastHashMap, LifeGuard, RawString, RefCount, Stored, U32Array,
 };
 use spirv_headers as spirv;
 use std::borrow::Borrow;
@@ -72,16 +72,29 @@ pub enum BindingError {
     WrongTextureMultisampled,
 }
 
+#[derive(Clone, Debug)]
+pub enum InputError {
+    /// The input is not provided by the earlier stage in the pipeline.
+    Missing,
+    /// The input type is not compatible with the provided.
+    WrongType,
+}
+
 /// Errors produced when validating a programmable stage of a pipeline.
 #[derive(Clone, Debug)]
 pub enum ProgrammableStageError {
     /// Unable to find an entry point matching the specified execution model.
     MissingEntryPoint(spirv::ExecutionModel),
-    /// Error matching a global binding to the pipeline layout.
+    /// Error matching a global binding against the pipeline layout.
     Binding {
         set: u32,
         binding: u32,
         error: BindingError,
+    },
+    /// Error matching the stage input against the previous stage outputs.
+    Input {
+        location: wgt::ShaderLocation,
+        error: InputError,
     },
 }
 
@@ -91,7 +104,12 @@ fn validate_binding(
     entry: &BindGroupLayoutEntry,
     usage: naga::GlobalUse,
 ) -> Result<(), BindingError> {
-    let allowed_usage = match module.types[var.ty].inner {
+    let mut ty_inner = &module.types[var.ty].inner;
+    //TODO: change naga's IR to avoid a pointer here
+    if let naga::TypeInner::Pointer { base, class: _ } = *ty_inner {
+        ty_inner = &module.types[base].inner;
+    }
+    let allowed_usage = match *ty_inner {
         naga::TypeInner::Struct { .. } => match entry.ty {
             BindingType::UniformBuffer => naga::GlobalUse::LOAD,
             BindingType::StorageBuffer => naga::GlobalUse::all(),
@@ -141,10 +159,7 @@ fn validate_binding(
                     match module.types[base].inner {
                         naga::TypeInner::Scalar { kind, .. }
                         | naga::TypeInner::Vector { kind, .. }
-                            if kind == expected_scalar_kind =>
-                        {
-                            ()
-                        }
+                            if kind == expected_scalar_kind => {}
                         naga::TypeInner::Scalar { kind, .. }
                         | naga::TypeInner::Vector { kind, .. } => {
                             return Err(BindingError::WrongTextureComponentType(Some(kind)))
@@ -174,12 +189,276 @@ fn validate_binding(
     }
 }
 
-pub(crate) fn validate_stage(
-    module: &naga::Module,
+fn is_sub_type(sub: &naga::TypeInner, provided: &naga::TypeInner) -> bool {
+    use naga::TypeInner as Ti;
+
+    match (sub, provided) {
+        (
+            &Ti::Scalar {
+                kind: k0,
+                width: w0,
+            },
+            &Ti::Scalar {
+                kind: k1,
+                width: w1,
+            },
+        ) => k0 == k1 && w0 <= w1,
+        (
+            &Ti::Scalar {
+                kind: k0,
+                width: w0,
+            },
+            &Ti::Vector {
+                size: _,
+                kind: k1,
+                width: w1,
+            },
+        ) => k0 == k1 && w0 <= w1,
+        (
+            &Ti::Vector {
+                size: s0,
+                kind: k0,
+                width: w0,
+            },
+            &Ti::Vector {
+                size: s1,
+                kind: k1,
+                width: w1,
+            },
+        ) => s0 as u8 <= s1 as u8 && k0 == k1 && w0 <= w1,
+        (
+            &Ti::Matrix {
+                columns: c0,
+                rows: r0,
+                kind: k0,
+                width: w0,
+            },
+            &Ti::Matrix {
+                columns: c1,
+                rows: r1,
+                kind: k1,
+                width: w1,
+            },
+        ) => c0 == c1 && r0 == r1 && k0 == k1 && w0 <= w1,
+        (&Ti::Struct { members: ref m0 }, &Ti::Struct { members: ref m1 }) => m0 == m1,
+        _ => false,
+    }
+}
+
+pub(crate) enum MaybeOwned<'a, T> {
+    Owned(T),
+    Borrowed(&'a T),
+}
+
+impl<'a, T> std::ops::Deref for MaybeOwned<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        match *self {
+            MaybeOwned::Owned(ref value) => value,
+            MaybeOwned::Borrowed(value) => value,
+        }
+    }
+}
+
+pub(crate) fn construct_vertex_format<'a>(
+    format: wgt::VertexFormat,
+) -> MaybeOwned<'a, naga::TypeInner> {
+    use naga::TypeInner as Ti;
+    use wgt::VertexFormat as Vf;
+    MaybeOwned::Owned(match format {
+        Vf::Uchar2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Uint,
+            width: 8,
+        },
+        Vf::Uchar4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Uint,
+            width: 8,
+        },
+        Vf::Char2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Sint,
+            width: 8,
+        },
+        Vf::Char4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Sint,
+            width: 8,
+        },
+        Vf::Uchar2Norm => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Float,
+            width: 8,
+        },
+        Vf::Uchar4Norm => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Float,
+            width: 8,
+        },
+        Vf::Char2Norm => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Float,
+            width: 8,
+        },
+        Vf::Char4Norm => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Float,
+            width: 8,
+        },
+        Vf::Ushort2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Uint,
+            width: 16,
+        },
+        Vf::Ushort4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Uint,
+            width: 16,
+        },
+        Vf::Short2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Sint,
+            width: 16,
+        },
+        Vf::Short4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Sint,
+            width: 16,
+        },
+        Vf::Ushort2Norm | Vf::Short2Norm | Vf::Half2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Float,
+            width: 16,
+        },
+        Vf::Ushort4Norm | Vf::Short4Norm | Vf::Half4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Float,
+            width: 16,
+        },
+        Vf::Float => Ti::Scalar {
+            kind: naga::ScalarKind::Float,
+            width: 32,
+        },
+        Vf::Float2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Float,
+            width: 32,
+        },
+        Vf::Float3 => Ti::Vector {
+            size: naga::VectorSize::Tri,
+            kind: naga::ScalarKind::Float,
+            width: 32,
+        },
+        Vf::Float4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Float,
+            width: 32,
+        },
+        Vf::Uint => Ti::Scalar {
+            kind: naga::ScalarKind::Uint,
+            width: 32,
+        },
+        Vf::Uint2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Uint,
+            width: 32,
+        },
+        Vf::Uint3 => Ti::Vector {
+            size: naga::VectorSize::Tri,
+            kind: naga::ScalarKind::Uint,
+            width: 32,
+        },
+        Vf::Uint4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Uint,
+            width: 32,
+        },
+        Vf::Int => Ti::Scalar {
+            kind: naga::ScalarKind::Sint,
+            width: 32,
+        },
+        Vf::Int2 => Ti::Vector {
+            size: naga::VectorSize::Bi,
+            kind: naga::ScalarKind::Sint,
+            width: 32,
+        },
+        Vf::Int3 => Ti::Vector {
+            size: naga::VectorSize::Tri,
+            kind: naga::ScalarKind::Sint,
+            width: 32,
+        },
+        Vf::Int4 => Ti::Vector {
+            size: naga::VectorSize::Quad,
+            kind: naga::ScalarKind::Sint,
+            width: 32,
+        },
+    })
+}
+
+/// Return true if the fragment `format` is covered by the provided `output`.
+pub(crate) fn check_texture_format(
+    format: wgt::TextureFormat,
+    output: &MaybeOwned<naga::TypeInner>,
+) -> bool {
+    use naga::ScalarKind as Sk;
+    use wgt::TextureFormat as Tf;
+
+    let (components, kind, width) = match *&**output {
+        naga::TypeInner::Scalar { kind, width } => (1, kind, width),
+        naga::TypeInner::Vector { size, kind, width } => (size as u8, kind, width),
+        _ => return false,
+    };
+    let (req_components, req_kind, req_width) = match format {
+        Tf::R8Unorm | Tf::R8Snorm => (1, Sk::Float, 8),
+        Tf::R8Uint => (1, Sk::Uint, 8),
+        Tf::R8Sint => (1, Sk::Sint, 8),
+        Tf::R16Uint => (1, Sk::Uint, 16),
+        Tf::R16Sint => (1, Sk::Sint, 16),
+        Tf::R16Float => (1, Sk::Float, 16),
+        Tf::Rg8Unorm | Tf::Rg8Snorm => (2, Sk::Float, 8),
+        Tf::Rg8Uint => (2, Sk::Uint, 8),
+        Tf::Rg8Sint => (2, Sk::Sint, 8),
+        Tf::R32Uint => (1, Sk::Uint, 32),
+        Tf::R32Sint => (1, Sk::Sint, 32),
+        Tf::R32Float => (1, Sk::Float, 32),
+        Tf::Rg16Uint => (2, Sk::Uint, 16),
+        Tf::Rg16Sint => (2, Sk::Sint, 16),
+        Tf::Rg16Float => (2, Sk::Float, 16),
+        Tf::Rgba8Unorm
+        | Tf::Rgba8UnormSrgb
+        | Tf::Rgba8Snorm
+        | Tf::Bgra8Unorm
+        | Tf::Bgra8UnormSrgb => (4, Sk::Float, 8),
+        Tf::Rgba8Uint => (4, Sk::Uint, 8),
+        Tf::Rgba8Sint => (4, Sk::Sint, 8),
+        Tf::Rgb10a2Unorm => (4, Sk::Float, 10),
+        Tf::Rg11b10Float => (3, Sk::Float, 11),
+        Tf::Rg32Uint => (2, Sk::Uint, 32),
+        Tf::Rg32Sint => (2, Sk::Sint, 32),
+        Tf::Rg32Float => (2, Sk::Float, 32),
+        Tf::Rgba16Uint => (4, Sk::Uint, 16),
+        Tf::Rgba16Sint => (4, Sk::Sint, 16),
+        Tf::Rgba16Float => (4, Sk::Float, 16),
+        Tf::Rgba32Uint => (4, Sk::Uint, 32),
+        Tf::Rgba32Sint => (4, Sk::Sint, 32),
+        Tf::Rgba32Float => (4, Sk::Float, 32),
+        Tf::Depth32Float | Tf::Depth24Plus | Tf::Depth24PlusStencil8 => return false,
+    };
+
+    components >= req_components && kind == req_kind && width >= req_width
+}
+
+pub(crate) type StageInterface<'a> =
+    FastHashMap<wgt::ShaderLocation, MaybeOwned<'a, naga::TypeInner>>;
+
+pub(crate) fn validate_stage<'a>(
+    module: &'a naga::Module,
     group_layouts: &[&BindEntryMap],
     entry_point_name: &str,
     execution_model: spirv::ExecutionModel,
-) -> Result<(), ProgrammableStageError> {
+    inputs: StageInterface<'a>,
+) -> Result<StageInterface<'a>, ProgrammableStageError> {
     // Since a shader module can have multiple entry points with the same name,
     // we need to look for one with the right execution model.
     let entry_point = module
@@ -198,6 +477,7 @@ pub(crate) fn validate_stage(
     };
 
     let function = &module.functions[entry_point.function];
+    let mut outputs = StageInterface::default();
     for ((_, var), &usage) in module.global_variables.iter().zip(&function.global_usage) {
         if usage.is_empty() {
             continue;
@@ -224,10 +504,35 @@ pub(crate) fn validate_stage(
                     });
                 }
             }
-            _ => {} //TODO
+            Some(naga::Binding::Location(location)) => {
+                let mut ty = &module.types[var.ty].inner;
+                //TODO: change naga's IR to not have pointer for varyings
+                if let naga::TypeInner::Pointer { base, class: _ } = *ty {
+                    ty = &module.types[base].inner;
+                }
+                if usage.contains(naga::GlobalUse::STORE) {
+                    outputs.insert(location, MaybeOwned::Borrowed(ty));
+                } else {
+                    let result =
+                        inputs
+                            .get(&location)
+                            .ok_or(InputError::Missing)
+                            .and_then(|provided| {
+                                if is_sub_type(ty, provided) {
+                                    Ok(())
+                                } else {
+                                    Err(InputError::WrongType)
+                                }
+                            });
+                    if let Err(error) = result {
+                        return Err(ProgrammableStageError::Input { location, error });
+                    }
+                }
+            }
+            _ => {}
         }
     }
-    Ok(())
+    Ok(outputs)
 }
 
 #[repr(C)]
