b76e56043257a01a72769de088bcf21901ebaa6f
Dzmitry Malyshau <kvarkus@gmail.com>
2020-07-23 22:35:13 -0400

    Update wgpu, use defaults for rasterizer states, use depth-clamping

--- examples/msaa-line/main.rs
+++ examples/msaa-line/main.rs
@@ -62,9 +62,7 @@ impl Example {
             rasterization_state: Some(wgpu::RasterizationStateDescriptor {
                 front_face: wgpu::FrontFace::Ccw,
                 cull_mode: wgpu::CullMode::None,
-                depth_bias: 0,
-                depth_bias_slope_scale: 0.0,
-                depth_bias_clamp: 0.0,
+                ..Default::default()
             }),
             primitive_topology: wgpu::PrimitiveTopology::LineList,
             color_states: Cow::Borrowed(&[wgpu::ColorStateDescriptor {
--- examples/skybox/main.rs
+++ examples/skybox/main.rs
@@ -102,9 +102,7 @@ impl framework::Example for Skybox {
             rasterization_state: Some(wgpu::RasterizationStateDescriptor {
                 front_face: wgpu::FrontFace::Cw,
                 cull_mode: wgpu::CullMode::None,
-                depth_bias: 0,
-                depth_bias_slope_scale: 0.0,
-                depth_bias_clamp: 0.0,
+                ..Default::default()
             }),
             primitive_topology: wgpu::PrimitiveTopology::TriangleList,
             color_states: Borrowed(&[wgpu::ColorStateDescriptor {
