e1e116537e83b27a3a63ef17d5bdaeb5f8e8b434
João Capucho <jcapucho7@gmail.com>
2020-09-09 22:22:13 +0100

    Extended Interface to be used in more places (#183)
    * Extended Interface to be used in more places
    
    * Applied comments and added tests
    
    * Removed the statement visitor
    
    * Fixed remaining comments

--- src/proc/interface.rs
+++ src/proc/interface.rs
@@ -1,34 +1,43 @@
 use crate::arena::{Arena, Handle};
 
-struct Interface<'a> {
+pub struct Interface<'a, T> {
     expressions: &'a Arena<crate::Expression>,
-    uses: Vec<crate::GlobalUse>,
+    visitor: &'a mut T,
 }
 
-impl<'a> Interface<'a> {
-    fn add_inputs(&mut self, handle: Handle<crate::Expression>) {
+pub trait Visitor {
+    fn visit_expr(&mut self, _: &crate::Expression) {}
+    fn visit_lhs_expr(&mut self, _: &crate::Expression) {}
+}
+
+impl<'a, T> Interface<'a, T>
+where
+    T: Visitor,
+{
+    fn traverse_expr(&mut self, handle: Handle<crate::Expression>) {
         use crate::Expression as E;
-        match self.expressions[handle] {
+
+        let expr = &self.expressions[handle];
+
+        self.visitor.visit_expr(expr);
+
+        match *expr {
             E::Access { base, index } => {
-                self.add_inputs(base);
-                self.add_inputs(index);
+                self.traverse_expr(base);
+                self.traverse_expr(index);
             }
             E::AccessIndex { base, .. } => {
-                self.add_inputs(base);
+                self.traverse_expr(base);
             }
             E::Constant(_) => {}
             E::Compose { ref components, .. } => {
                 for &comp in components {
-                    self.add_inputs(comp);
+                    self.traverse_expr(comp);
                 }
             }
-            E::FunctionParameter(_) => {}
-            E::GlobalVariable(handle) => {
-                self.uses[handle.index()] |= crate::GlobalUse::LOAD;
-            }
-            E::LocalVariable(_) => {}
+            E::FunctionParameter(_) | E::GlobalVariable(_) | E::LocalVariable(_) => {}
             E::Load { pointer } => {
-                self.add_inputs(pointer);
+                self.traverse_expr(pointer);
             }
             E::ImageSample {
                 image,
@@ -37,17 +46,17 @@ impl<'a> Interface<'a> {
                 level,
                 depth_ref,
             } => {
-                self.add_inputs(image);
-                self.add_inputs(sampler);
-                self.add_inputs(coordinate);
+                self.traverse_expr(image);
+                self.traverse_expr(sampler);
+                self.traverse_expr(coordinate);
                 match level {
                     crate::SampleLevel::Auto => (),
                     crate::SampleLevel::Exact(h) | crate::SampleLevel::Bias(h) => {
-                        self.add_inputs(h)
+                        self.traverse_expr(h)
                     }
                 }
                 if let Some(dref) = depth_ref {
-                    self.add_inputs(dref);
+                    self.traverse_expr(dref);
                 }
             }
             E::ImageLoad {
@@ -55,83 +64,83 @@ impl<'a> Interface<'a> {
                 coordinate,
                 index,
             } => {
-                self.add_inputs(image);
-                self.add_inputs(coordinate);
-                self.add_inputs(index);
+                self.traverse_expr(image);
+                self.traverse_expr(coordinate);
+                self.traverse_expr(index);
             }
             E::Unary { expr, .. } => {
-                self.add_inputs(expr);
+                self.traverse_expr(expr);
             }
             E::Binary { left, right, .. } => {
-                self.add_inputs(left);
-                self.add_inputs(right);
+                self.traverse_expr(left);
+                self.traverse_expr(right);
             }
             E::Intrinsic { argument, .. } => {
-                self.add_inputs(argument);
+                self.traverse_expr(argument);
             }
             E::Transpose(matrix) => {
-                self.add_inputs(matrix);
+                self.traverse_expr(matrix);
             }
             E::DotProduct(left, right) => {
-                self.add_inputs(left);
-                self.add_inputs(right);
+                self.traverse_expr(left);
+                self.traverse_expr(right);
             }
             E::CrossProduct(left, right) => {
-                self.add_inputs(left);
-                self.add_inputs(right);
+                self.traverse_expr(left);
+                self.traverse_expr(right);
             }
             E::As(expr, _) => {
-                self.add_inputs(expr);
+                self.traverse_expr(expr);
             }
             E::Derivative { expr, .. } => {
-                self.add_inputs(expr);
+                self.traverse_expr(expr);
             }
             E::Call { ref arguments, .. } => {
                 for &argument in arguments {
-                    self.add_inputs(argument);
+                    self.traverse_expr(argument);
                 }
             }
         }
     }
 
-    fn collect(&mut self, block: &[crate::Statement]) {
+    pub fn traverse(&mut self, block: &[crate::Statement]) {
         for statement in block {
             use crate::Statement as S;
             match *statement {
                 S::Empty | S::Break | S::Continue | S::Kill => (),
                 S::Block(ref b) => {
-                    self.collect(b);
+                    self.traverse(b);
                 }
                 S::If {
                     condition,
                     ref accept,
                     ref reject,
                 } => {
-                    self.add_inputs(condition);
-                    self.collect(accept);
-                    self.collect(reject);
+                    self.traverse_expr(condition);
+                    self.traverse(accept);
+                    self.traverse(reject);
                 }
                 S::Switch {
                     selector,
                     ref cases,
                     ref default,
                 } => {
-                    self.add_inputs(selector);
+                    self.traverse_expr(selector);
                     for &(ref case, _) in cases.values() {
-                        self.collect(case);
+                        self.traverse(case);
                     }
-                    self.collect(default);
+                    self.traverse(default);
                 }
                 S::Loop {
                     ref body,
                     ref continuing,
                 } => {
-                    self.collect(body);
-                    self.collect(continuing);
+                    self.traverse(body);
+                    self.traverse(continuing);
                 }
                 S::Return { value } => {
                     if let Some(expr) = value {
-                        self.add_inputs(expr);
+                        self.traverse_expr(expr);
                     }
                 }
                 S::Store { pointer, value } => {
@@ -139,22 +148,35 @@ impl<'a> Interface<'a> {
                     loop {
                         match self.expressions[left] {
                             crate::Expression::Access { base, index } => {
-                                self.add_inputs(index);
+                                self.traverse_expr(index);
                                 left = base;
                             }
                             crate::Expression::AccessIndex { base, .. } => {
                                 left = base;
                             }
-                            crate::Expression::GlobalVariable(handle) => {
-                                self.uses[handle.index()] |= crate::GlobalUse::STORE;
-                                break;
-                            }
                             _ => break,
                         }
                     }
-                    self.add_inputs(value);
+                    self.visitor.visit_lhs_expr(&self.expressions[left]);
+                    self.traverse_expr(value);
                 }
             }
+        }
+    }
+}
+
+struct GlobalUseVisitor(Vec<crate::GlobalUse>);
+
+impl Visitor for GlobalUseVisitor {
+    fn visit_expr(&mut self, expr: &crate::Expression) {
+        if let crate::Expression::GlobalVariable(handle) = expr {
+            self.0[handle.index()] |= crate::GlobalUse::LOAD;
+        }
+    }
+
+    fn visit_lhs_expr(&mut self, expr: &crate::Expression) {
+        if let crate::Expression::GlobalVariable(handle) = expr {
+            self.0[handle.index()] |= crate::GlobalUse::STORE;
         }
     }
 }
@@ -165,11 +187,72 @@ impl crate::GlobalUse {
         body: &[crate::Statement],
         globals: &Arena<crate::GlobalVariable>,
     ) -> Vec<Self> {
+        let mut visitor = GlobalUseVisitor(vec![crate::GlobalUse::empty(); globals.len()]);
+
         let mut io = Interface {
             expressions,
-            uses: vec![crate::GlobalUse::empty(); globals.len()],
+            visitor: &mut visitor,
         };
-        io.collect(body);
-        io.uses
+        io.traverse(body);
+        visitor.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        Arena, Expression, GlobalUse, GlobalVariable, Handle, Statement, StorageAccess,
+        StorageClass,
+    };
+
+    #[test]
+    fn global_use_scan() {
+        let test_global = GlobalVariable {
+            name: None,
+            class: StorageClass::Constant,
+            binding: None,
+            ty: Handle::new(std::num::NonZeroU32::new(1).unwrap()),
+            interpolation: None,
+            storage_access: StorageAccess::empty(),
+        };
+        let mut test_globals = Arena::new();
+
+        let global_1 = test_globals.append(test_global.clone());
+        let global_2 = test_globals.append(test_global.clone());
+        let global_3 = test_globals.append(test_global.clone());
+        let global_4 = test_globals.append(test_global);
+
+        let mut expressions = Arena::new();
+        let global_1_expr = expressions.append(Expression::GlobalVariable(global_1));
+        let global_2_expr = expressions.append(Expression::GlobalVariable(global_2));
+        let global_3_expr = expressions.append(Expression::GlobalVariable(global_3));
+        let global_4_expr = expressions.append(Expression::GlobalVariable(global_4));
+
+        let test_body = vec![
+            Statement::Return {
+                value: Some(global_1_expr),
+            },
+            Statement::Store {
+                pointer: global_2_expr,
+                value: global_1_expr,
+            },
+            Statement::Store {
+                pointer: expressions.append(Expression::Access {
+                    base: global_3_expr,
+                    index: global_4_expr,
+                }),
+                value: global_1_expr,
+            },
+        ];
+
+        assert_eq!(
+            GlobalUse::scan(&expressions, &test_body, &test_globals),
+            vec![
+                GlobalUse::LOAD,
+                GlobalUse::STORE,
+                GlobalUse::STORE,
+                GlobalUse::LOAD,
+            ]
+        )
     }
 }
--- src/proc/mod.rs
+++ src/proc/mod.rs
@@ -4,5 +4,6 @@ mod interface;
 mod typifier;
 mod validator;
 
+pub use interface::{Interface, Visitor};
 pub use typifier::{check_constant_type, ResolveError, Typifier};
 pub use validator::{ValidationError, Validator};
