6d5084b8ade6d14e22090d52ef5b70cded8c0b5e
Kunal Mohan <kunalmohan99@gmail.com>
2020-09-13 12:18:45 +0530

    address review comments

--- wgpu-core/src/swap_chain.rs
+++ wgpu-core/src/swap_chain.rs
@@ -247,45 +247,42 @@ impl<G: GlobalIdentityHandlerFactory> Global<G> {
             .acquired_view_id
             .take()
             .ok_or(SwapChainError::AlreadyAcquired)?;
-        let (view, _) = hub.texture_views.unregister(view_id.value.0, &mut token);
-        if let Some(view) = view {
-            let image = match view.inner {
-                resource::TextureViewInner::Native { .. } => unreachable!(),
-                resource::TextureViewInner::SwapChain { image, .. } => image,
-            };
+        let (view_maybe, _) = hub.texture_views.unregister(view_id.value.0, &mut token);
+        let view = view_maybe.ok_or(SwapChainError::Invalid)?;
+        let image = match view.inner {
+            resource::TextureViewInner::Native { .. } => unreachable!(),
+            resource::TextureViewInner::SwapChain { image, .. } => image,
+        };
 
-            let sem = if sc.active_submission_index > device.last_completed_submission_index() {
-                Some(&sc.semaphore)
-            } else {
-                None
-            };
-            let queue = &mut device.queue_group.queues[0];
-            let result = unsafe { queue.present(B::get_surface_mut(surface), image, sem) };
-
-            tracing::debug!(trace = true, "Presented. End of Frame");
-
-            for fbo in sc.acquired_framebuffers.drain(..) {
-                unsafe {
-                    device.raw.destroy_framebuffer(fbo);
-                }
-            }
-
-            match result {
-                Ok(None) => Ok(SwapChainStatus::Good),
-                Ok(Some(_)) => Ok(SwapChainStatus::Suboptimal),
-                Err(err) => match err {
-                    hal::window::PresentError::OutOfMemory(_) => {
-                        Err(SwapChainError::Device(DeviceError::OutOfMemory))
-                    }
-                    hal::window::PresentError::OutOfDate => Ok(SwapChainStatus::Outdated),
-                    hal::window::PresentError::SurfaceLost(_) => Ok(SwapChainStatus::Lost),
-                    hal::window::PresentError::DeviceLost(_) => {
-                        Err(SwapChainError::Device(DeviceError::Lost))
-                    }
-                },
-            }
+        let sem = if sc.active_submission_index > device.last_completed_submission_index() {
+            Some(&sc.semaphore)
         } else {
-            Err(SwapChainError::Invalid)
+            None
+        };
+        let queue = &mut device.queue_group.queues[0];
+        let result = unsafe { queue.present(B::get_surface_mut(surface), image, sem) };
+
+        tracing::debug!(trace = true, "Presented. End of Frame");
+
+        for fbo in sc.acquired_framebuffers.drain(..) {
+            unsafe {
+                device.raw.destroy_framebuffer(fbo);
+            }
+        }
+
+        match result {
+            Ok(None) => Ok(SwapChainStatus::Good),
+            Ok(Some(_)) => Ok(SwapChainStatus::Suboptimal),
+            Err(err) => match err {
+                hal::window::PresentError::OutOfMemory(_) => {
+                    Err(SwapChainError::Device(DeviceError::OutOfMemory))
+                }
+                hal::window::PresentError::OutOfDate => Ok(SwapChainStatus::Outdated),
+                hal::window::PresentError::SurfaceLost(_) => Ok(SwapChainStatus::Lost),
+                hal::window::PresentError::DeviceLost(_) => {
+                    Err(SwapChainError::Device(DeviceError::Lost))
+                }
+            },
         }
     }
 }
