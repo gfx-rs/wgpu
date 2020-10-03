98167103c2a5f019de7fab4ec547fbee1fbddb82
Timo de Kort <dekort.timo@gmail.com>
2020-10-04 01:13:40 +0200

    [spv-out] Remove instruction spec tests

--- src/back/spv/test_framework.rs
+++ src/back/spv/test_framework.rs
@@ -1,38 +1,4 @@
-use crate::back::spv::Instruction;
-use spirv::{Op, Word};
-
-pub(super) struct SpecRequirements {
-    pub(super) op: Op,
-    pub(super) wc: u32,
-    pub(super) type_id: bool,
-    pub(super) result_id: bool,
-    pub(super) operands: bool,
-}
-
-/// Basic validation for checking if the instruction complies to the spec requirements
-pub(super) fn validate_spec_requirements(
-    requirements: SpecRequirements,
-    instruction: &Instruction,
-) {
-    assert_eq!(requirements.op, instruction.op);
-
-    // Pass the assert if the minimum referred wc in the spec is met
-    assert!(instruction.wc >= requirements.wc);
-
-    if requirements.type_id {
-        assert!(instruction.type_id.is_some());
-    }
-
-    if requirements.result_id {
-        assert!(instruction.result_id.is_some());
-    }
-
-    if requirements.operands {
-        assert!(!instruction.operands.is_empty());
-    }
-}
-
-pub(super) fn validate_instruction(words: &[Word], instruction: &Instruction) {
+pub(super) fn validate_instruction(words: &[spirv::Word], instruction: &crate::back::spv::Instruction) {
     let mut inst_index = 0;
     let (wc, op) = ((words[inst_index] >> 16) as u16, words[inst_index] as u16);
     inst_index += 1;
