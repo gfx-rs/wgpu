78466fbb31266210be1073dc57a9c2abadbb442b
Andreas Reich <r_andreas2@web.de>
2021-01-10 22:12:09 +0100

    Enable storage read/write format feature

--- wgpu-core/src/instance.rs
+++ wgpu-core/src/instance.rs
@@ -281,10 +281,9 @@ impl<B: GfxBackend> Adapter<B> {
         if texture_format_properties.contains(hal::format::ImageFeature::STORAGE_ATOMIC) {
             flags |= wgt::TextureFormatFeatureFlags::STORAGE_ATOMICS;
         }
-        // TODO: Hal update required. This in turn is blocked by https://github.com/zakarumych/gpu-alloc/issues/33
-        //if texture_format_properties.contains(hal::format::ImageFeature::STORAGE_READ_WRITE) {
-        //    flags |= wgt::TextureFormatFeatureFlags::STORAGE_READ_WRITE;
-        //}
+        if texture_format_properties.contains(hal::format::ImageFeature::STORAGE_READ_WRITE) {
+            flags |= wgt::TextureFormatFeatureFlags::STORAGE_READ_WRITE;
+        }
 
         wgt::TextureFormatFeatures {
             allowed_usages,
