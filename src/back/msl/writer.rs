b08dfe51469447b6daf86b01ebd56465c8efe9ca
Ashley <ashley.ruglys@gmail.com>
2021-04-20 17:41:13 +0200

    [Metal] Impl `Expression::Splat` (#738)
    * [Metal] Impl `Expression::Splat`
    
    * Add changes to the snapshots
    
    * Apply suggestions

--- src/back/msl/writer.rs
+++ src/back/msl/writer.rs
@@ -608,8 +608,17 @@ impl<W: Write> Writer<W> {
                 };
                 write!(self.out, "{}", coco)?;
             }
-            crate::Expression::Splat { size: _, value } => {
-                self.put_expression(value, context, is_scoped)?;
+            crate::Expression::Splat { size, value } => {
+                let scalar_kind = match *context.resolve_type(value) {
+                    crate::TypeInner::Scalar { kind, .. } => kind,
+                    _ => return Err(Error::Validation)
+                };
+                let scalar = scalar_kind_string(scalar_kind);
+                let size = vector_size_string(size);
+
+                write!(self.out, "{}{}(", scalar, size)?;
+                self.put_expression(value, context, true)?;
+                write!(self.out, ")")?;
             }
             crate::Expression::Compose { ty, ref components } => {
                 let inner = &context.module.types[ty].inner;
