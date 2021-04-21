4664cd301aaca7e8f77b15aa5162b3730e255de4
Ashley <ashley.ruglys@gmail.com>
2021-04-21 15:16:11 +0200

    [Metal] Wrap a `packed_float3` with `float3` when loading from struct (#740)
    * Implement the wrapping in Expression::Load instead
    
    * Add changes to stack size
    
    * stable and nightly can't seem to agree on a stack size
    
    * Apply suggestions
    
    * Add a comment about unexpected path
    
    * Update skybox snapshot
    
    * should_pack_struct_member returns an Option<ScalarKind>
    
    * Remove accidental file

--- src/back/msl/writer.rs
+++ src/back/msl/writer.rs
@@ -311,6 +311,32 @@ fn separate(need_separator: bool) -> &'static str {
     }
 }
 
+fn should_pack_struct_member(
+    members: &[crate::StructMember],
+    span: u32,
+    index: usize,
+    module: &crate::Module,
+) -> Option<crate::ScalarKind> {
+    let member = &members[index];
+    let ty_inner = &module.types[member.ty].inner;
+
+    let last_offset = member.offset + ty_inner.span(&module.constants);
+    let next_offset = match members.get(index + 1) {
+        Some(next) => next.offset,
+        None => span,
+    };
+    let is_tight = next_offset == last_offset;
+
+    match *ty_inner {
+        crate::TypeInner::Vector {
+            size: crate::VectorSize::Tri,
+            width: 4,
+            kind,
+        } if member.offset & 0xF != 0 || is_tight => Some(kind),
+        _ => None,
+    }
+}
+
 impl crate::StorageClass {
     /// Returns true for storage classes, for which the global
     /// variables are passed in function arguments.
@@ -700,10 +726,48 @@ impl<W: Write> Writer<W> {
                 write!(self.out, "{}", name)?;
             }
             crate::Expression::Load { pointer } => {
-                // We don't do any dereferencing with `*` here as pointer arguments to functions
-                // are done by `&` references and not `*` pointers. These do not need to be
-                // dereferenced.
-                self.put_expression(pointer, context, is_scoped)?;
+                // Because packed vectors such as `packed_float3` cannot be directly multipied by
+                // matrices, we wrap them with `float3` on load.
+                let wrap_packed_vec_scalar_kind = match context.function.expressions[pointer] {
+                    crate::Expression::AccessIndex { base, index } => {
+                        let ty = match context.resolve_type(base) {
+                            &crate::TypeInner::Pointer { base, .. } => {
+                                &context.module.types[base].inner
+                            }
+                            // This path is unexpected and shouldn't happen, but it's easier
+                            // to leave in.
+                            ty => ty,
+                        };
+                        match *ty {
+                            crate::TypeInner::Struct {
+                                ref members, span, ..
+                            } => should_pack_struct_member(
+                                members,
+                                span,
+                                index as usize,
+                                &context.module,
+                            ),
+                            _ => None,
+                        }
+                    }
+                    _ => None,
+                };
+
+                if let Some(scalar_kind) = wrap_packed_vec_scalar_kind {
+                    write!(
+                        self.out,
+                        "{}::{}3(",
+                        NAMESPACE,
+                        scalar_kind_string(scalar_kind)
+                    )?;
+                    self.put_expression(pointer, context, true)?;
+                    write!(self.out, ")")?;
+                } else {
+                    // We don't do any dereferencing with `*` here as pointer arguments to functions
+                    // are done by `&` references and not `*` pointers. These do not need to be
+                    // dereferenced.
+                    self.put_expression(pointer, context, is_scoped)?;
+                }
             }
             crate::Expression::ImageSample {
                 image,
@@ -1428,7 +1492,9 @@ impl<W: Write> Writer<W> {
                         }
                     }
                 }
-                crate::TypeInner::Struct { ref members, .. } => {
+                crate::TypeInner::Struct {
+                    ref members, span, ..
+                } => {
                     writeln!(self.out, "struct {} {{", name)?;
                     let mut last_offset = 0;
                     for (index, member) in members.iter().enumerate() {
@@ -1440,19 +1506,12 @@ impl<W: Write> Writer<W> {
                         }
                         let ty_inner = &module.types[member.ty].inner;
                         last_offset = member.offset + ty_inner.span(&module.constants);
-                        let is_tight = match members.get(index + 1) {
-                            Some(next) => next.offset == last_offset,
-                            None => false,
-                        };
+
                         let member_name = &self.names[&NameKey::StructMember(handle, index as u32)];
 
-                        match *ty_inner {
-                            // for a misaligned vec3, issue a packed vector
-                            crate::TypeInner::Vector {
-                                size: crate::VectorSize::Tri,
-                                kind,
-                                width: 4,
-                            } if member.offset & 0xF != 0 || is_tight => {
+                        // If the member should be packed (as is the case for a misaligned vec3) issue a packed vector
+                        match should_pack_struct_member(members, span, index, module) {
+                            Some(kind) => {
                                 writeln!(
                                     self.out,
                                     "{}packed_{}3 {};",
@@ -1461,7 +1520,7 @@ impl<W: Write> Writer<W> {
                                     member_name
                                 )?;
                             }
-                            _ => {
+                            None => {
                                 let base_name = TypeContext {
                                     handle: member.ty,
                                     arena: &module.types,
@@ -2179,8 +2238,8 @@ fn test_stack_size() {
         }
         let stack_size = addresses.end - addresses.start;
         // check the size (in debug only)
-        // last observed macOS value: 21584
-        if stack_size < 20000 || stack_size > 22000 {
+        // last observed macOS value: 21760
+        if stack_size < 21000 || stack_size > 23000 {
             panic!("`put_expression` stack size {} has changed!", stack_size);
         }
     }
