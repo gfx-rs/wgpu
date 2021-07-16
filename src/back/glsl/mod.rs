10a3a6a827018dd53362475c7c593f55b588e13f
Igor Shaposhnik <Gordon-F@users.noreply.github.com>
2021-07-16 12:32:09 +0000

    [glsl-out] Write sint array size

--- src/back/glsl/mod.rs
+++ src/back/glsl/mod.rs
@@ -604,6 +604,10 @@ impl<'a, W: Write> Writer<'a, W> {
                         width: _,
                         value: crate::ScalarValue::Uint(size),
                     } => write!(self.out, "{}", size)?,
+                    crate::ConstantInner::Scalar {
+                        width: _,
+                        value: crate::ScalarValue::Sint(size),
+                    } => write!(self.out, "{}", size)?,
                     _ => unreachable!(),
                 }
             }
