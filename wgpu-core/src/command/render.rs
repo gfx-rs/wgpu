d1c62ff1c2b9c846a9c3bf52fb0e7e0d6b6d7407
Kyle Cesare <kcesare@gmail.com>
2021-08-05 21:41:56 -0700

    Do not error on zero-sized scissor commands.

--- wgpu-core/src/command/render.rs
+++ wgpu-core/src/command/render.rs
@@ -1279,9 +1279,7 @@ impl<G: GlobalIdentityHandlerFactory> Global<G> {
                     }
                     RenderCommand::SetScissor(ref rect) => {
                         let scope = PassErrorScope::SetScissorRect;
-                        if rect.w == 0
-                            || rect.h == 0
-                            || rect.x + rect.w > info.extent.width
+                        if rect.x + rect.w > info.extent.width
                             || rect.y + rect.h > info.extent.height
                         {
                             return Err(RenderCommandError::InvalidScissorRect).map_pass_err(scope);
