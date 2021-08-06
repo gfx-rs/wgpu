216ffab87cbec557a37036756fbcc666a7c6f8a9
Agorgianitis Loukas <agorglouk@gmail.com>
2021-08-06 09:29:51 +0300

    Fix scissor test leak when presenting gles backend

--- wgpu-hal/src/gles/egl.rs
+++ wgpu-hal/src/gles/egl.rs
@@ -840,6 +840,8 @@ impl Surface {
                 crate::SurfaceError::Lost
             })?;
 
+        gl.disable(glow::SCISSOR_TEST);
+
         gl.bind_framebuffer(glow::DRAW_FRAMEBUFFER, None);
         gl.bind_framebuffer(glow::READ_FRAMEBUFFER, Some(sc.framebuffer));
         // Note the Y-flipping here. GL's presentation is not flipped,
