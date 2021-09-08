gfx-rs/naga

hlsl: implement struct constructors

tests/out/glsl/operators.main.Compute.glsl
@@ -5,6 +5,11 @@ precision highp int;
 
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
+struct Foo {
+    vec4 a;
+    int b;
+};
+
 
 vec4 builtins() {
     int s1_ = (true ? 1 : 0);
@@ -31,10 +36,18 @@ int unary() {
     }
 }
 
+float constructors() {
+    Foo foo;
+    foo = Foo(vec4(1.0), 1);
+    vec4 _e10 = foo.a;
+    return _e10.x;
+}
+
 void main() {
     vec4 _e4 = builtins();
     vec4 _e5 = splat();
     int _e6 = unary();
+    float _e7 = constructors();
     return;
 }
 

tests/out/hlsl/operators.hlsl
@@ -3,6 +3,11 @@ static const float4 v_f32_zero = float4(0.0, 0.0, 0.0, 0.0);
 static const float4 v_f32_half = float4(0.5, 0.5, 0.5, 0.5);
 static const int4 v_i32_one = int4(1, 1, 1, 1);
 
+struct Foo {
+    float4 a;
+    int b;
+};
+
 float4 builtins()
 {
     int s1_ = (true ? 1 : 0);
@@ -31,11 +36,28 @@ int unary()
     }
 }
 
+Foo ConstructFoo(float4 arg0, int arg1) {
+    Foo ret;
+    ret.a = arg0;
+    ret.b = arg1;
+    return ret;
+}
+
+float constructors()
+{
+    Foo foo = (Foo)0;
+
+    foo = ConstructFoo(float4(1.0.xxxx), 1);
+    float4 _expr10 = foo.a;
+    return _expr10.x;
+}
+
 [numthreads(1, 1, 1)]
 void main()
 {
     const float4 _e4 = builtins();
     const float4 _e5 = splat();
     const int _e6 = unary();
+    const float _e7 = constructors();
     return;
 }

tests/out/wgsl/operators.wgsl
@@ -1,3 +1,8 @@
+struct Foo {
+    a: vec4<f32>;
+    b: i32;
+};
+
 let v_f32_one: vec4<f32> = vec4<f32>(1.0, 1.0, 1.0, 1.0);
 let v_f32_zero: vec4<f32> = vec4<f32>(0.0, 0.0, 0.0, 0.0);
 let v_f32_half: vec4<f32> = vec4<f32>(0.5, 0.5, 0.5, 0.5);
@@ -27,10 +32,19 @@ fn unary() -> i32 {
     }
 }
 
+fn constructors() -> f32 {
+    var foo: Foo;
+
+    foo = Foo(vec4<f32>(1.0), 1);
+    let _e10: vec4<f32> = foo.a;
+    return _e10.x;
+}
+
 [[stage(compute), workgroup_size(1, 1, 1)]]
 fn main() {
     let _e4: vec4<f32> = builtins();
     let _e5: vec4<f32> = splat();
     let _e6: i32 = unary();
+    let _e7: f32 = constructors();
     return;
 }

gfx-rs/wgpu

tests/out/glsl/operators.main.Compute.glsl
@@ -5,6 +5,11 @@ precision highp int;
 
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
+struct Foo {
+    vec4 a;
+    int b;
+};
+
 
 vec4 builtins() {
     int s1_ = (true ? 1 : 0);
@@ -29,10 +34,18 @@ int unary() {
     }
 }
 
+float constructors() {
+    Foo foo;
+    foo = Foo(vec4(1.0), 1);
+    vec4 _e9 = foo.a;
+    return _e9.x;
+}
+
 void main() {
     vec4 _e3 = builtins();
     vec4 _e4 = splat();
     int _e5 = unary();
+    float _e6 = constructors();
     return;
 }
 

tests/out/hlsl/operators.hlsl
@@ -2,6 +2,11 @@ static const float4 v_f32_one = float4(1.0, 1.0, 1.0, 1.0);
 static const float4 v_f32_zero = float4(0.0, 0.0, 0.0, 0.0);
 static const float4 v_f32_half = float4(0.5, 0.5, 0.5, 0.5);
 
+struct Foo {
+    float4 a;
+    int b;
+};
+
 float4 builtins()
 {
     int s1_ = (true ? 1 : 0);
@@ -28,11 +33,28 @@ int unary()
     }
 }
 
+Foo ConstructFoo(float4 arg0, int arg1) {
+    Foo ret;
+    ret.a = arg0;
+    ret.b = arg1;
+    return ret;
+}
+
+float constructors()
+{
+    Foo foo = (Foo)0;
+
+    foo = ConstructFoo(float4(1.0.xxxx), 1);
+    float4 _expr9 = foo.a;
+    return _expr9.x;
+}
+
 [numthreads(1, 1, 1)]
 void main()
 {
     const float4 _e3 = builtins();
     const float4 _e4 = splat();
     const int _e5 = unary();
+    const float _e6 = constructors();
     return;
 }

tests/out/wgsl/operators.wgsl
@@ -1,3 +1,8 @@
+struct Foo {
+    a: vec4<f32>;
+    b: i32;
+};
+
 let v_f32_one: vec4<f32> = vec4<f32>(1.0, 1.0, 1.0, 1.0);
 let v_f32_zero: vec4<f32> = vec4<f32>(0.0, 0.0, 0.0, 0.0);
 let v_f32_half: vec4<f32> = vec4<f32>(0.5, 0.5, 0.5, 0.5);
@@ -24,10 +29,19 @@ fn unary() -> i32 {
     }
 }
 
+fn constructors() -> f32 {
+    var foo: Foo;
+
+    foo = Foo(vec4<f32>(1.0), 1);
+    let _e9: vec4<f32> = foo.a;
+    return _e9.x;
+}
+
 [[stage(compute), workgroup_size(1, 1, 1)]]
 fn main() {
     let _e3: vec4<f32> = builtins();
     let _e4: vec4<f32> = splat();
     let _e5: i32 = unary();
+    let _e6: f32 = constructors();
     return;
 }