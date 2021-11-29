1b1c826ac756d62c0bbb25c3bf054284efdfcb8c
Aleksander Guryanov <caiiiycuk@gmail.com>
2021-11-29 20:34:15 +0700

    Remove duplicate line in mod.rs (#2223)

--- wgpu/src/backend/mod.rs
+++ wgpu/src/backend/mod.rs
@@ -6,7 +6,6 @@ pub(crate) use web::{BufferMappedRange, Context};
 #[cfg(any(not(target_arch = "wasm32"), feature = "webgl"))]
 mod direct;
 #[cfg(any(not(target_arch = "wasm32"), feature = "webgl"))]
-#[cfg(any(not(target_arch = "wasm32"), feature = "webgl"))]
 pub(crate) use direct::{BufferMappedRange, Context};
 
 #[cfg(any(not(target_arch = "wasm32"), feature = "webgl"))]
