adf19e669d5b596561882e136de9c5ceb0362673
UpsettingBoy <jeronimosg@hotmail.es>
2021-12-03 21:28:26 +0100

    Extended Queue::write_texture doc to reflect overrun error

--- wgpu/src/lib.rs
+++ wgpu/src/lib.rs
@@ -3158,7 +3158,7 @@ impl Queue {
     /// As such, the write is not immediately submitted, and instead enqueued
     /// internally to happen at the start of the next `submit()` call.
     ///
-    /// This fails if `data` overruns the size of `buffer` starting at `offset`.
+    /// This method fails if `data` overruns the size of `buffer` starting at `offset`.
     pub fn write_buffer(&self, buffer: &Buffer, offset: BufferAddress, data: &[u8]) {
         Context::queue_write_buffer(&*self.context, &self.id, &buffer.id, offset, data)
     }
@@ -3168,6 +3168,8 @@ impl Queue {
     /// This method is intended to have low performance costs.
     /// As such, the write is not immediately submitted, and instead enqueued
     /// internally to happen at the start of the next `submit()` call.
+    ///
+    /// This method fails if `data` overruns the size of fragment of `texture` specified with `size`.
     pub fn write_texture(
         &self,
         texture: ImageCopyTexture,
