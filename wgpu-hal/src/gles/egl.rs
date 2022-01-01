43f648f7e52010fa977478df6bef2b68cf04d93f
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2022-01-01 09:29:55 +0300

    Unify types for GLES config tiers
    Signed-off-by: Dmitry Sharshakov <d3dx12.xx@gmail.com>

--- wgpu-hal/src/gles/egl.rs
+++ wgpu-hal/src/gles/egl.rs
@@ -161,11 +161,8 @@ fn choose_config(
 ) -> Result<(egl::Config, bool), crate::InstanceError> {
     //TODO: EGL_SLOW_CONFIG
     let tiers = [
-        ("surfaceless", &[egl::SURFACE_TYPE, egl::PBUFFER_BIT][..]),
-        (
-            "off-screen",
-            &[egl::RENDERABLE_TYPE, egl::OPENGL_ES2_BIT][..],
-        ),
+        ("surfaceless", &[egl::SURFACE_TYPE, egl::PBUFFER_BIT]),
+        ("off-screen", &[egl::RENDERABLE_TYPE, egl::OPENGL_ES2_BIT]),
         ("presentation", &[egl::SURFACE_TYPE, egl::WINDOW_BIT]),
         #[cfg(not(target_os = "android"))]
         ("native-render", &[egl::NATIVE_RENDERABLE, egl::TRUE as _]),
