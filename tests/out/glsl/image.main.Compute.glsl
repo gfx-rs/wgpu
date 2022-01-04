c8fc8c13bad9e986784dad74b71f8d7bdeadca7b
Jim Blandy <jimb@red-bean.com>
2022-01-03 17:34:55 -0800

    [msl-out] Don't give level in texture1d accesses.
    Fixes #1642.
    
    Since 1d textures cannot have mipmaps, MSL requires that the `level` argument to
    texel accesses and dimension queries be a constexpr 0. For our purposes, just
    omit the level argument altogether.

--- tests/out/glsl/image.main.Compute.glsl
+++ tests/out/glsl/image.main.Compute.glsl
@@ -14,6 +14,8 @@ layout(rgba8ui) readonly uniform highp uimage2D _group_0_binding_1_cs;
 
 uniform highp usampler2DArray _group_0_binding_5_cs;
 
+uniform highp usampler2D _group_0_binding_7_cs;
+
 layout(r32ui) writeonly uniform highp uimage2D _group_0_binding_2_cs;
 
 
@@ -25,7 +27,8 @@ void main() {
     uvec4 value2_ = texelFetch(_group_0_binding_3_cs, itc, int(local_id.z));
     uvec4 value4_ = imageLoad(_group_0_binding_1_cs, itc);
     uvec4 value5_ = texelFetch(_group_0_binding_5_cs, ivec3(itc, int(local_id.z)), (int(local_id.z) + 1));
-    imageStore(_group_0_binding_2_cs, ivec2(itc.x, 0.0), (((value1_ + value2_) + value4_) + value5_));
+    uvec4 value6_ = texelFetch(_group_0_binding_7_cs, ivec2(int(local_id.x), 0.0), int(local_id.z));
+    imageStore(_group_0_binding_2_cs, ivec2(itc.x, 0.0), ((((value1_ + value2_) + value4_) + value5_) + value6_));
     return;
 }
 
