ce567bdc91fac4bf46eb6d2d75c3253719c86271
Aaron O'Mullan <aaron.omullan@gmail.com>
2022-01-17 23:01:18 +0100

    chore: bump naga to a1840be
    So tip no longer indirectly pins indexmap, etc...

--- wgpu-hal/src/metal/device.rs
+++ wgpu-hal/src/metal/device.rs
@@ -117,7 +117,10 @@ impl super::Device {
         let mut immutable_buffer_mask = 0;
         for (var_handle, var) in module.global_variables.iter() {
             if var.class == naga::StorageClass::WorkGroup {
-                let size = module.types[var.ty].inner.span(&module.constants);
+                let size = module.types[var.ty]
+                    .inner
+                    .try_size(&module.constants)
+                    .unwrap();
                 wg_memory_sizes.push(size);
             }
 
