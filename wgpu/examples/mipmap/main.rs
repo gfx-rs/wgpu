31369e3f90831116824b53ea529156f9f4286572
Layl <3094382+LaylBongers@users.noreply.github.com>
2022-03-06 18:40:29 +0100

    Fix incorrect filtering used in mipmap generation

--- wgpu/examples/mipmap/main.rs
+++ wgpu/examples/mipmap/main.rs
@@ -114,7 +114,7 @@ impl Example {
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
+            min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
