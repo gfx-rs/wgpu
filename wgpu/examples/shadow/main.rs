7f3559638c2f2650036fd1463001510ca539f605
Dzmitry Malyshau <kvark@fastmail.com>
2022-03-18 22:37:26 -0700

    Expect shadow example to pass on GL

--- wgpu/examples/shadow/main.rs
+++ wgpu/examples/shadow/main.rs
@@ -859,7 +859,6 @@ fn shadow() {
         optional_features: wgpu::Features::default(),
         base_test_parameters: framework::test_common::TestParameters::default()
             .downlevel_flags(wgpu::DownlevelFlags::COMPARISON_SAMPLERS)
-            .backend_failure(wgpu::Backends::GL) // https://github.com/gfx-rs/naga/issues/1747
             .specific_failure(Some(wgpu::Backends::VULKAN), None, Some("V3D"), false), // rpi4 on VK doesn't work: https://gitlab.freedesktop.org/mesa/mesa/-/issues/3916
         tolerance: 2,
         max_outliers: 500, // bounded by rpi4
