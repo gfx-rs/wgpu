26537c1a6042e906f278eda5eb40aa331dfa21fa
Jonathan Behrens <fintelia@gmail.com>
2022-03-27 17:40:39 -0400

    Call ctx.emit_start after flushing

--- src/front/glsl/builtins.rs
+++ src/front/glsl/builtins.rs
@@ -1999,6 +1999,7 @@ impl MacroCall {
             ),
             MacroCall::Barrier => {
                 ctx.emit_flush(body);
+                ctx.emit_start();
                 body.push(crate::Statement::Barrier(crate::Barrier::all()), meta);
                 return Ok(None);
             }
