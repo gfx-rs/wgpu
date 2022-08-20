39fafde2b6b3e013d91d8f3be60cfc0946e5edfe
Frank Steffahn <fdsteffahn@gmail.com>
2022-08-20 22:33:32 +0200

    v0.7 Fix build failure due to rust-lang/rust PR 99413 (#2966)
    * Fix build failure due to rust-lang/rust PR 99413
    
    * Remove extraneous indentation from comments.
    
    The module comments on `command/bundle.rs` and `swap_chain.rs` are
    indented by four spaces, which causes rustdoc to try to treat them
    as code examples and compile them as doctests. Removing the
    indentation causes rustdoc to treat them as text, as intended.
    
    Co-authored-by: Jim Blandy <jimb@red-bean.com>

--- wgpu-core/src/device/mod.rs
+++ wgpu-core/src/device/mod.rs
@@ -1236,6 +1236,7 @@ impl<B: GfxBackend> Device<B> {
 
         // `BTreeMap` has ordered bindings as keys, which allows us to coalesce
         // the descriptor writes into a single transaction.
+        let mut desc_set; // early declaration so it's dropped after write_map
         let mut write_map = BTreeMap::new();
         for entry in desc.entries.iter() {
             let binding = entry.binding;
@@ -1513,7 +1514,7 @@ impl<B: GfxBackend> Device<B> {
             self.desc_allocator
                 .lock()
                 .allocate(&self.raw, &layout.raw, &layout.desc_count, 1)?;
-        let mut desc_set = desc_sets.pop().unwrap();
+        desc_set = desc_sets.pop().unwrap();
 
         // Set the descriptor set's label for easier debugging.
         if let Some(label) = desc.label.as_ref() {
