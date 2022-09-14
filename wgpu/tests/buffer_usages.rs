2b72e8c87380f61155a51c096738e25f0c58ccac
Imbris <imbrisf@gmail.com>
2022-09-13 22:46:22 -0400

    Refactor buffer_usages test to spend less time setting up to test each case

--- wgpu/tests/buffer_usages.rs
+++ wgpu/tests/buffer_usages.rs
@@ -7,7 +7,7 @@ use crate::common::{initialize_test, TestParameters};
 #[test]
 fn buffer_usage() {
     fn try_create(
-        usage: wgpu::BufferUsages,
+        usages: &[wgpu::BufferUsages],
         enable_mappable_primary_buffers: bool,
         should_panic: bool,
     ) {
@@ -20,34 +20,28 @@ fn buffer_usage() {
         }
 
         initialize_test(parameters, |ctx| {
-            let _buffer = ctx.device.create_buffer(&wgpu::BufferDescriptor {
-                label: None,
-                size: BUFFER_SIZE,
-                usage,
-                mapped_at_creation: false,
-            });
+            for usage in usages.iter().copied() {
+                let _buffer = ctx.device.create_buffer(&wgpu::BufferDescriptor {
+                    label: None,
+                    size: BUFFER_SIZE,
+                    usage,
+                    mapped_at_creation: false,
+                });
+            }
         });
     }
 
     use wgpu::BufferUsages as Bu;
 
-    // These are always valid
-    [
+    let always_valid = [
         Bu::MAP_READ,
         Bu::MAP_WRITE,
         Bu::MAP_READ | Bu::COPY_DST,
         Bu::MAP_WRITE | Bu::COPY_SRC,
-    ]
-    .into_iter()
-    .for_each(|usages| {
-        // Should only pass validation when MAPPABLE_PRIMARY_BUFFERS is enabled.
-        try_create(usages, false, false);
-        try_create(usages, true, false);
-    });
-
+    ];
     // MAP_READ can only be paired with COPY_DST and MAP_WRITE can only be paired with COPY_SRC
     // (unless Features::MAPPABlE_PRIMARY_BUFFERS is enabled).
-    [
+    let needs_mappable_primary_buffers = [
         Bu::MAP_READ | Bu::COPY_DST | Bu::COPY_SRC,
         Bu::MAP_WRITE | Bu::COPY_SRC | Bu::COPY_DST,
         Bu::MAP_READ | Bu::MAP_WRITE,
@@ -55,20 +49,17 @@ fn buffer_usage() {
         Bu::MAP_READ | Bu::COPY_DST | Bu::STORAGE,
         Bu::MAP_WRITE | Bu::COPY_SRC | Bu::STORAGE,
         wgpu::BufferUsages::all(),
-    ]
-    .into_iter()
-    .for_each(|usages| {
-        // Only valid when MAPPABLE_PRIMARY_BUFFERS is enabled.
-        try_create(usages, false, true);
-        try_create(usages, true, false);
-    });
+    ];
+    let always_fail = [Bu::empty()];
 
-    // Buffers cannot have empty usage flags
-    [Bu::empty()].into_iter().for_each(|usages| {
-        // Should always fail
-        try_create(usages, false, true);
-        try_create(usages, true, true);
-    });
+    try_create(&always_valid, false, false);
+    try_create(&always_valid, true, false);
+
+    try_create(&needs_mappable_primary_buffers, false, true);
+    try_create(&needs_mappable_primary_buffers, true, false);
+
+    try_create(&always_fail, false, true);
+    try_create(&always_fail, true, true);
 }
 
 const BUFFER_SIZE: BufferAddress = 1234;
