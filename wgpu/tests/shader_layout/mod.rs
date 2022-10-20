c7e5ac33098d7df0e51c387f078c0f17bb26c263
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2022-10-19 20:13:54 -0400

    Improve tests

--- wgpu/tests/shader_layout/mod.rs
+++ wgpu/tests/shader_layout/mod.rs
@@ -1,7 +1,7 @@
 use std::{borrow::Cow, fmt::Write};
 
 use wgpu::{
-    BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor, BindGroupLayoutEntry,
+    Backends, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor, BindGroupLayoutEntry,
     BindingType, BufferDescriptor, BufferUsages, CommandEncoderDescriptor, ComputePassDescriptor,
     ComputePipelineDescriptor, DownlevelFlags, Limits, Maintain, MapMode, PipelineLayoutDescriptor,
     ShaderModuleDescriptor, ShaderSource, ShaderStages,
@@ -24,43 +24,171 @@ impl StorageType {
     }
 }
 
-#[derive(Clone, Copy)]
-enum OutputType {
-    Float,
-    Uint,
-    Sint,
-}
-
-impl OutputType {
-    fn as_str(&self) -> &'static str {
-        match self {
-            OutputType::Float => "f32",
-            OutputType::Uint => "u32",
-            OutputType::Sint => "i32",
-        }
-    }
+struct ShaderLayoutTest {
+    member_types: &'static [&'static str],
+    accessors: &'static [&'static [&'static str]],
+    output_values: &'static [u32],
+    uniform_failures: Backends,
+    storage_failures: Backends,
 }
 
 const MAX_BUFFER_SIZE: u64 = 128;
-#[rustfmt::skip]
-const TESTS: &[(&[&str], &[&[&str]], &[u32], OutputType)] = &[
-    // member types    accessors                   output values        output type
-    (&["f32"],       &[&[""]],                     &[1],                OutputType::Float),
-    (&["vec2<f32>"], &[&[".x", ".y"]],             &[1, 2],             OutputType::Float),
-    (&["vec3<f32>"], &[&[".x", ".y", ".z"]],       &[1, 2, 3],          OutputType::Float),
-    (&["vec4<f32>"], &[&[".x", ".y", ".z", ".w"]], &[1, 2, 3, 4],       OutputType::Float),
-
-    (&["mat2x2<f32>"], &[&["[0].x", "[0].y", "[1].x", "[1].y"]],                   &[1, 2, 3, 4],  OutputType::Float),
-    (&["mat2x3<f32>"], &[&["[0].x", "[0].y", "[0].z", "[1].x", "[1].y", "[1].z"]], &[1, 2, 3, 5, 6, 7],  OutputType::Float),
-    (&["mat2x4<f32>"], &[&["[0].x", "[0].y", "[0].z", "[0].w", "[1].x", "[1].y", "[1].z", "[1].w"]], &[1, 2, 3, 4, 5, 6, 7, 8],  OutputType::Float),
-    
-    (&["mat3x2<f32>"], &[&["[0].x", "[0].y", "[1].x", "[1].y", "[2].x", "[2].y"]], &[1, 2, 3, 4, 5, 6],  OutputType::Float),
-    (&["mat3x3<f32>"], &[&["[0].x", "[0].y", "[0].z", "[1].x", "[1].y", "[1].z", "[2].x", "[2].y", "[2].z"]], &[1, 2, 3, 5, 6, 7, 9, 10, 11],  OutputType::Float),
-    (&["mat3x4<f32>"], &[&["[0].x", "[0].y", "[0].z", "[0].w", "[1].x", "[1].y", "[1].z", "[1].w", "[2].x", "[2].y", "[2].z", "[2].w"]], &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],  OutputType::Float),
-    
-    (&["mat4x2<f32>"], &[&["[0].x", "[0].y", "[1].x", "[1].y", "[2].x", "[2].y", "[3].x", "[3].y"]], &[1, 2, 3, 4, 5, 6, 7, 8],  OutputType::Float),
-    (&["mat4x3<f32>"], &[&["[0].x", "[0].y", "[0].z", "[1].x", "[1].y", "[1].z", "[2].x", "[2].y", "[2].z", "[3].x", "[3].y", "[3].z"]], &[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15],  OutputType::Float),
-    (&["mat4x4<f32>"], &[&["[0].x", "[0].y", "[0].z", "[0].w", "[1].x", "[1].y", "[1].z", "[1].w", "[2].x", "[2].y", "[2].z", "[2].w", "[3].x", "[3].y", "[3].z", "[3].w"]], &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],  OutputType::Float),
+const TESTS: &[ShaderLayoutTest] = &[
+    ShaderLayoutTest {
+        member_types: &["f32"],
+        accessors: &[&[""]],
+        output_values: &[1],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec2<f32>"],
+        accessors: &[&[".x", ".y"]],
+        output_values: &[1, 2],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec3<f32>"],
+        accessors: &[&[".x", ".y", ".z"]],
+        output_values: &[1, 2, 3],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec4<f32>"],
+        accessors: &[&[".x", ".y", ".z", ".w"]],
+        output_values: &[1, 2, 3, 4],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["u32"],
+        accessors: &[&[""]],
+        output_values: &[1],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec2<u32>"],
+        accessors: &[&[".x", ".y"]],
+        output_values: &[1, 2],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec3<u32>"],
+        accessors: &[&[".x", ".y", ".z"]],
+        output_values: &[1, 2, 3],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["vec4<u32>"],
+        accessors: &[&[".x", ".y", ".z", ".w"]],
+        output_values: &[1, 2, 3, 4],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat2x2<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", //
+            "[1].x", "[1].y", //
+        ]],
+        output_values: &[1, 2, 3, 4],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat2x3<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", //
+            "[1].x", "[1].y", "[1].z", //
+        ]],
+        output_values: &[1, 2, 3, 5, 6, 7],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat2x4<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", "[0].w", //
+            "[1].x", "[1].y", "[1].z", "[1].w", //
+        ]],
+        output_values: &[1, 2, 3, 4, 5, 6, 7, 8],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::DX12,
+    },
+    ShaderLayoutTest {
+        member_types: &["mat3x2<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", //
+            "[1].x", "[1].y", //
+            "[2].x", "[2].y", //
+        ]],
+        output_values: &[1, 2, 3, 4, 5, 6],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::DX12,
+    },
+    ShaderLayoutTest {
+        member_types: &["mat3x3<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", //
+            "[1].x", "[1].y", "[1].z", //
+            "[2].x", "[2].y", "[2].z", //
+        ]],
+        output_values: &[1, 2, 3, 5, 6, 7, 9, 10, 11],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat3x4<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", "[0].w", //
+            "[1].x", "[1].y", "[1].z", "[1].w", //
+            "[2].x", "[2].y", "[2].z", "[2].w", //
+        ]],
+        output_values: &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat4x2<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", //
+            "[1].x", "[1].y", //
+            "[2].x", "[2].y", //
+            "[3].x", "[3].y", //
+        ]],
+        output_values: &[1, 2, 3, 4, 5, 6, 7, 8],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::DX12,
+    },
+    ShaderLayoutTest {
+        member_types: &["mat4x3<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", //
+            "[1].x", "[1].y", "[1].z", //
+            "[2].x", "[2].y", "[2].z", //
+            "[3].x", "[3].y", "[3].z", //
+        ]],
+        output_values: &[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
+    ShaderLayoutTest {
+        member_types: &["mat4x4<f32>"],
+        accessors: &[&[
+            "[0].x", "[0].y", "[0].z", "[0].w", //
+            "[1].x", "[1].y", "[1].z", "[1].w", //
+            "[2].x", "[2].y", "[2].z", "[2].w", //
+            "[3].x", "[3].y", "[3].z", "[3].w", //
+        ]],
+        output_values: &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        uniform_failures: Backends::empty(),
+        storage_failures: Backends::empty(),
+    },
 ];
 
 fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
@@ -68,9 +196,7 @@ fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
 
     // We go from [1, MAX] for indices so that unwritten values (cleared zeros)
     // won't be confused for a valid written offset of 0.
-    let uint_values: Vec<_> = (1..=(MAX_BUFFER_SIZE / 4) as u32).collect();
-    let sint_values: Vec<_> = (1..=(MAX_BUFFER_SIZE / 4) as i32).collect();
-    let float_values: Vec<_> = (1..=(MAX_BUFFER_SIZE / 4)).map(|v| v as f32).collect();
+    let values: Vec<_> = (1..=(MAX_BUFFER_SIZE as u32 / 4)).collect();
 
     let bgl = ctx
         .device
@@ -149,20 +275,22 @@ fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
             push_constant_ranges: &[],
         });
 
-    for &(member_types, accessors, output_values, output_type) in TESTS {
-        let test_name = format!("{member_types:?}");
+    let mut fail = false;
+    for test in TESTS {
+        let test_name = format!("{:?}", test.member_types);
 
         let mut input_members = String::new();
-        for (idx, &ty) in member_types.iter().enumerate() {
+        for (idx, &ty) in test.member_types.iter().enumerate() {
             writeln!(&mut input_members, "member_{idx}: {ty},").unwrap();
         }
 
         let mut body = String::new();
-        for (member_idx, &member_accessors) in accessors.iter().enumerate() {
+        for (member_idx, &member_accessors) in test.accessors.iter().enumerate() {
             for &member_accessor in member_accessors {
+                // We bitcast as the values are really u32/i32 values.
                 writeln!(
                     &mut body,
-                    "output[i] = input.member_{member_idx}{member_accessor};"
+                    "output[i] = bitcast<u32>(input.member_{member_idx}{member_accessor});"
                 )
                 .unwrap();
                 writeln!(&mut body, "i += 1u;").unwrap();
@@ -172,8 +300,7 @@ fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
         let processed = source
             .replace("{{storage_type}}", storage_type.as_str())
             .replace("{{input_members}}", &input_members)
-            .replace("{{body}}", &body)
-            .replace("{{output_type}}", output_type.as_str());
+            .replace("{{body}}", &body);
 
         let sm = ctx.device.create_shader_module(ShaderModuleDescriptor {
             label: Some(&format!("shader {test_name}")),
@@ -189,13 +316,8 @@ fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
                 entry_point: "cs_main",
             });
 
-        let bytes: &[u8] = match output_type {
-            OutputType::Float => bytemuck::cast_slice(&float_values),
-            OutputType::Uint => bytemuck::cast_slice(&uint_values),
-            OutputType::Sint => bytemuck::cast_slice(&sint_values),
-        };
-
-        ctx.queue.write_buffer(&input_buffer, 0, bytes);
+        ctx.queue
+            .write_buffer(&input_buffer, 0, bytemuck::cast_slice(&values));
 
         let mut encoder = ctx
             .device
@@ -220,38 +342,30 @@ fn input_layout_test(ctx: TestingContext, storage_type: StorageType) {
         ctx.device.poll(Maintain::Wait);
 
         let mapped = mapping_buffer.slice(..).get_mapped_range();
-        match output_type {
-            OutputType::Float => {
-                let typed: &[f32] = bytemuck::cast_slice(&*mapped);
-                let expected: Vec<f32> = output_values.iter().map(|&v| v as f32).collect();
-                assert_eq!(
-                    &typed[..expected.len()],
-                    expected,
-                    "Left is actual. Right is expected. Inner test {test_name}"
-                );
-            }
-            OutputType::Uint => {
-                let typed: &[u32] = bytemuck::cast_slice(&*mapped);
-                assert_eq!(
-                    &typed[..output_values.len()],
-                    output_values,
-                    "Left is actual. Right is expected."
-                );
-            }
-            OutputType::Sint => {
-                let typed: &[i32] = bytemuck::cast_slice(&*mapped);
-                let expected: Vec<i32> = output_values.iter().map(|&v| v as i32).collect();
-                assert_eq!(
-                    &typed[..expected.len()],
-                    expected,
-                    "Left is actual. Right is expected."
-                );
+
+        let typed: &[u32] = bytemuck::cast_slice(&*mapped);
+
+        let left = &typed[..test.output_values.len()];
+        let right = &*test.output_values;
+        let failure = left != right;
+        if failure {
+            eprintln!("Inner test failure. Actual {left:?}. Expected {right:?}. Test {test_name}");
+        }
+        let backend_failures = match storage_type {
+            StorageType::Uniform => test.uniform_failures,
+            StorageType::Storage => test.storage_failures,
+        };
+        if failure != backend_failures.contains(ctx.adapter.get_info().backend.into()) {
+            fail |= true;
+            if !failure {
+                eprintln!("Unexpected test success. Test {test_name}");
             }
         }
         drop(mapped);
 
         mapping_buffer.unmap();
     }
+    assert!(!fail);
 }
 
 #[test]
