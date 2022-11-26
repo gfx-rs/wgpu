242a1a77a238de54be0aaed23dccc8efc34d6270
Harald Reingruber <harald.reingruber@gmx.at>
2022-11-26 22:38:01 +0100

    Fix clippy

--- wgpu/tests/wasm.rs
+++ wgpu/tests/wasm.rs
@@ -103,41 +103,38 @@ where
 
     event_loop.spawn(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
-        match event {
-            Event::RedrawRequested(_) => {
-                let frame = surface
-                    .get_current_texture()
-                    .expect("Failed to acquire next swap chain texture");
-                let view = frame
-                    .texture
-                    .create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder =
-                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-                {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-                                store: true,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
-                }
-
-                queue.submit(Some(encoder.finish()));
-                frame.present();
-
-                assert_rendering_result(&window);
-
-                *control_flow = ControlFlow::Exit;
+        if let Event::RedrawRequested(_) = event {
+            let frame = surface
+                .get_current_texture()
+                .expect("Failed to acquire next swap chain texture");
+            let view = frame
+                .texture
+                .create_view(&wgpu::TextureViewDescriptor::default());
+            let mut encoder =
+                device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+            {
+                let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: None,
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                            store: true,
+                        },
+                    })],
+                    depth_stencil_attachment: None,
+                });
+                rpass.set_pipeline(&render_pipeline);
+                rpass.draw(0..3, 0..1);
             }
-            _ => (),
+
+            queue.submit(Some(encoder.finish()));
+            frame.present();
+
+            assert_rendering_result(&window);
+
+            *control_flow = ControlFlow::Exit;
         }
     });
 }
