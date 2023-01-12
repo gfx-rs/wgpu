8c491b80c27c782e6a81cef1de5aad0ed97995f8
Jinlei Li <jinleili0@outlook.com>
2023-01-12 20:23:25 +0800

    Remove  DownlevelFlags::VIEW_FORMATS from gles/adapter.rs

--- wgpu-hal/src/gles/adapter.rs
+++ wgpu-hal/src/gles/adapter.rs
@@ -283,8 +283,7 @@ impl super::Adapter {
         let mut downlevel_flags = wgt::DownlevelFlags::empty()
             | wgt::DownlevelFlags::NON_POWER_OF_TWO_MIPMAPPED_TEXTURES
             | wgt::DownlevelFlags::CUBE_ARRAY_TEXTURES
-            | wgt::DownlevelFlags::COMPARISON_SAMPLERS
-            | wgt::DownlevelFlags::VIEW_FORMATS;
+            | wgt::DownlevelFlags::COMPARISON_SAMPLERS;
         downlevel_flags.set(wgt::DownlevelFlags::COMPUTE_SHADERS, ver >= (3, 1));
         downlevel_flags.set(
             wgt::DownlevelFlags::FRAGMENT_WRITABLE_STORAGE,
