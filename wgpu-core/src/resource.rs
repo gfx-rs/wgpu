b8d9c52fafe3b0b9662a5e532953a9690cf54215
teoxoy <28601907+teoxoy@users.noreply.github.com>
2023-01-20 16:45:01 +0100

    update TextureView validation

--- wgpu-core/src/resource.rs
+++ wgpu-core/src/resource.rs
@@ -584,7 +584,8 @@ pub struct TextureView<A: hal::Api> {
     //TODO: store device_id for quick access?
     pub(crate) desc: HalTextureViewDescriptor,
     pub(crate) format_features: wgt::TextureFormatFeatures,
-    pub(crate) extent: wgt::Extent3d,
+    /// This is `None` only if the texture view is not renderable
+    pub(crate) render_extent: Option<wgt::Extent3d>,
     pub(crate) samples: u32,
     pub(crate) selector: TextureSelector,
     pub(crate) life_guard: LifeGuard,
@@ -607,6 +608,12 @@ pub enum CreateTextureViewError {
     InvalidCubemapTextureDepth { depth: u32 },
     #[error("Invalid texture depth `{depth}` for texture view of dimension `CubemapArray`. Cubemap views must use images with sizes which are a multiple of 6.")]
     InvalidCubemapArrayTextureDepth { depth: u32 },
+    #[error("Source texture width and height must be equal for a texture view of dimension `Cube`/`CubeArray`")]
+    InvalidCubeTextureViewSize,
+    #[error("mip level count is 0")]
+    ZeroMipLevelCount,
+    #[error("array layer count is 0")]
+    ZeroArrayLayerCount,
     #[error(
         "TextureView mip level count + base mip level {requested} must be <= Texture mip level count {total}"
     )]
