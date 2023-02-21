17fbcf71a842b259b31d224a5b01bbd1e6f1cd3d
Red Artist <redartofcode@gmail.com>
2023-02-22 05:17:12 +0530

    enable debug extension on emscripten

--- wgpu-hal/src/gles/adapter.rs
+++ wgpu-hal/src/gles/adapter.rs
@@ -8,7 +8,14 @@ use crate::auxil::db;
 
 const GL_UNMASKED_VENDOR_WEBGL: u32 = 0x9245;
 const GL_UNMASKED_RENDERER_WEBGL: u32 = 0x9246;
-
+extern "C" {
+    /// returns 1 if success. 0 if failure. extension name must be null terminated
+    pub fn emscripten_webgl_enable_extension(
+        context: std::ffi::c_int,
+        extension: *const std::ffi::c_char,
+    ) -> std::ffi::c_int;
+    pub fn emscripten_webgl_get_current_context() -> std::ffi::c_int;
+}
 impl super::Adapter {
     /// According to the OpenGL specification, the version information is
     /// expected to follow the following syntax:
@@ -184,8 +191,18 @@ impl super::Adapter {
     ) -> Option<crate::ExposedAdapter<super::Api>> {
         let gl = context.lock();
         let extensions = gl.supported_extensions();
-
-        let (vendor_const, renderer_const) = if extensions.contains("WEBGL_debug_renderer_info") {
+        // if emscripten, has debug extension and if we can enable it -> use unmasked vendor/renderer
+        // https://github.com/gfx-rs/wgpu/issues/3245
+        let (vendor_const, renderer_const) = if cfg!(target_os = "emscripten")
+            && extensions.contains("WEBGL_debug_renderer_info")
+            && unsafe {
+                emscripten_webgl_enable_extension(
+                    emscripten_webgl_get_current_context(),
+                    "WEBGL_debug_renderer_info\0".as_ptr() as _,
+                )
+            } == 1
+        {
+            // if we try querying unmasked constants without enabling extension, we crash on emscripten.
             (GL_UNMASKED_VENDOR_WEBGL, GL_UNMASKED_RENDERER_WEBGL)
         } else {
             (glow::VENDOR, glow::RENDERER)
