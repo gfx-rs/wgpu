23fa71216ed562b6feef43cc2c904dc33c346b5b
teoxoy <28601907+teoxoy@users.noreply.github.com>
2023-02-27 17:39:09 +0100

    update feature documentation
    rename:
    SHADER_FLOAT16 -> SHADER_F16
    SHADER_FLOAT64 -> SHADER_F64
    SHADER_INT16 -> SHADER_I16
    TEXTURE_COMPRESSION_ASTC_LDR -> TEXTURE_COMPRESSION_ASTC
    WRITE_TIMESTAMP_INSIDE_PASSES -> TIMESTAMP_QUERY_INSIDE_PASSES

--- wgpu-core/src/command/compute.rs
+++ wgpu-core/src/command/compute.rs
@@ -714,7 +714,7 @@ impl<G: GlobalIdentityHandlerFactory> Global<G> {
                     let scope = PassErrorScope::WriteTimestamp;
 
                     device
-                        .require_features(wgt::Features::WRITE_TIMESTAMP_INSIDE_PASSES)
+                        .require_features(wgt::Features::TIMESTAMP_QUERY_INSIDE_PASSES)
                         .map_pass_err(scope)?;
 
                     let query_set: &resource::QuerySet<A> = cmd_buf
