92df5b5c967186185d58276b6162be4eae335652
Daniel Keitel <daniel.mg.keitel@fau.de>
2023-03-19 14:32:55 +0100

    multiple AS builds at once (better allocation strategy required)

--- wgpu-hal/src/dx12/command.rs
+++ wgpu-hal/src/dx12/command.rs
@@ -1552,9 +1552,9 @@ impl crate::CommandEncoder for super::CommandEncoder {
         }
     }
 
-    unsafe fn build_acceleration_structure(
+    unsafe fn build_acceleration_structures(
         &mut self,
-        _desc: &crate::BuildAccelerationStructureDescriptor<super::Api>,
+        _desc: &[&crate::BuildAccelerationStructureDescriptor<super::Api>],
     ) {
         // Implement using `BuildRaytracingAccelerationStructure`:
         // https://microsoft.github.io/DirectX-Specs/d3d/Raytracing.html#buildraytracingaccelerationstructure
