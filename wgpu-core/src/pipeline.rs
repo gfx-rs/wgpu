a89e35a4e0f774142342c24dde3b6294b0ad5cf5
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-03-22 14:56:52 +0100

    Validate shader location clashes (#3613)
    * Validate shader location clashes
    
    * Add line to changelog

--- wgpu-core/src/pipeline.rs
+++ wgpu-core/src/pipeline.rs
@@ -352,6 +352,8 @@ pub enum CreateRenderPipelineError {
         location: wgt::ShaderLocation,
         offset: wgt::BufferAddress,
     },
+    #[error("Two or more attributes were assigned to the same shader lcoation {0}")]
+    ShaderLocationClash(u32),
     #[error("Strip index format was not set to None but to {strip_index_format:?} while using the non-strip topology {topology:?}")]
     StripIndexFormatForNonStripTopology {
         strip_index_format: Option<wgt::IndexFormat>,
