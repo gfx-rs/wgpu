b71e073dec20dd91f4be4a320a74496c7391e940
Andreas Reich <r_andreas2@web.de>
2023-06-06 21:31:01 +0200

    allow deprecated methods in metal adapter.rs for the time being

--- wgpu-hal/src/metal/mod.rs
+++ wgpu-hal/src/metal/mod.rs
@@ -13,6 +13,11 @@ end of the VS buffer table.
 
 !*/
 
+// `MTLFeatureSet` is superseded by `MTLGpuFamily`.
+// However, `MTLGpuFamily` is only supported starting MacOS 10.15, whereas our minimum target is MacOS 10.13,
+// See https://github.com/gpuweb/gpuweb/issues/1069 for minimum spec.
+// TODO: Eventually all deprecated features should be abstracted and use new api when available.
+#[allow(deprecated)]
 mod adapter;
 mod command;
 mod conv;
