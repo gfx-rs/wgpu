ce615e9174677e4b8f75c370e3896d95ea188bc4
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2023-06-15 23:07:04 -0400

    Use adapter index in test name

--- tests/src/infra/single.rs
+++ tests/src/infra/single.rs
@@ -60,7 +60,7 @@ pub fn run_test(
         String::from("Executed")
     };
 
-    let full_name = format!("[{running_msg}] [{backend:?}/{device_name}] {base_name}");
+    let full_name = format!("[{running_msg}] [{backend:?}/{device_name}/{adapter_index}] {base_name}");
 
     libtest_mimic::Trial::test(full_name, move || {
         if should_skip {
