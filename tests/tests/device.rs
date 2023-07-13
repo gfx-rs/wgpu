cd13befc5739cece9d514e7baed0a1b5626114d6
Nicolas Silva <nical@fastmail.com>
2023-07-13 12:16:48 +0200

    Add a basic test for mismatched devices when creating bind group layouts.

--- tests/tests/device.rs
+++ tests/tests/device.rs
@@ -9,3 +9,29 @@ fn device_initialization() {
         // intentionally empty
     })
 }
+
+#[test]
+fn device_mismatch() {
+    initialize_test(TestParameters::default().failure(), |ctx| {
+        // Create a bind group uisng a lyaout from another device. This should be a validation
+        // error but currently crashes.
+        let (device2, _) =
+            pollster::block_on(ctx.adapter.request_device(&Default::default(), None)).unwrap();
+
+        {
+            let bind_group_layout =
+                device2.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    label: None,
+                    entries: &[],
+                });
+
+            let _bind_group = ctx.device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: None,
+                layout: &bind_group_layout,
+                entries: &[],
+            });
+        }
+
+        ctx.device.poll(wgpu::Maintain::Poll);
+    });
+}
