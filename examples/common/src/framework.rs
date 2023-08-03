5deb26fd32414f4b6c3e66e1c62c9f40780ac59c
JustAnotherCodemonkey <sophia.waggoner.personal@gmail.com>
2023-08-03 19:43:42 +0300

    Fix output_image_wasm. (Formerly did not handle pre-existing output image targets.)

--- examples/common/src/framework.rs
+++ examples/common/src/framework.rs
@@ -43,8 +43,8 @@ pub fn output_image_wasm(image_data: Vec<u8>, texture_dims: (usize, usize)) {
             Err(e) => {
                 log::error!(
                     "In searching for a staging canvas for outputting an image \
-                (element with id \"staging-canvas\"), found non-canvas element: {:?}.
-                Replacing with standard staging canvas.",
+                    (element with id \"staging-canvas\"), found non-canvas element: {:?}.
+                    Replacing with standard staging canvas.",
                     e
                 );
                 e.remove();
@@ -55,7 +55,7 @@ pub fn output_image_wasm(image_data: Vec<u8>, texture_dims: (usize, usize)) {
         create_staging_canvas(&document)
     };
     // Having the size attributes the right size is so important, we should always do it
-    // just to be safe.
+    // just to be safe. Also, what if we might want the image size to be able to change?
     let image_dimension_strings = (texture_dims.0.to_string(), texture_dims.1.to_string());
     canvas
         .set_attribute("width", image_dimension_strings.0.as_str())
@@ -78,16 +78,29 @@ pub fn output_image_wasm(image_data: Vec<u8>, texture_dims: (usize, usize)) {
     context.put_image_data(&image_data, 0.0, 0.0).unwrap();
     log::info!("Put image data in canvas.");
 
-    // The canvas is now the image we ultimately want. We can create a data url from it now.
+    // Get the img element that will act as our target for rendering from the canvas.
+    let image_element = if let Some(found_image_element) =
+        document.get_element_by_id("output-image-target")
+    {
+        match found_image_element.dyn_into::<web_sys::HtmlImageElement>() {
+            Ok(e) => e,
+            Err(e) => {
+                log::error!(
+                    "Found an element with the id \"output-image-target\" but it was not an image: {:?}.
+                    Replacing with default image output element.",
+                    e
+                );
+                e.remove();
+                create_output_image_element(&document)
+            }
+        }
+    } else {
+        create_output_image_element(&document)
+    };
+    // The canvas is currently the image we ultimately want. We can create a data url from it now.
     let data_url = canvas.to_data_url().unwrap();
-    let image_element = document
-        .create_element("img")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlImageElement>()
-        .unwrap();
     image_element.set_src(&data_url);
     body.append_child(&image_element).unwrap();
-    log::info!("Created image element with data url.");
 
     if document.get_element_by_id("image-for-you-text").is_none() {
         let p = document
@@ -108,7 +121,7 @@ fn create_staging_canvas(document: &web_sys::Document) -> web_sys::HtmlCanvasEle
     let body = document.body().expect("Failed to get document body.");
     let new_canvas = document
         .create_element("canvas")
-        .expect("Failed to create replacement staging canvas.")
+        .expect("Failed to create staging canvas.")
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .unwrap();
     // We don't want to show the canvas, we just want it to exist in the background.
@@ -117,6 +130,21 @@ fn create_staging_canvas(document: &web_sys::Document) -> web_sys::HtmlCanvasEle
     body.append_child(&new_canvas).unwrap();
     log::info!("Created new staging canvas: {:?}", &new_canvas);
     new_canvas
+}
+
+#[cfg(target_arch = "wasm32")]
+fn create_output_image_element(document: &web_sys::Document) -> web_sys::HtmlImageElement {
+    let body = document.body().expect("Failed to get document body.");
+    let new_image = document
+        .create_element("img")
+        .expect("Failed to create output image element.")
+        .dyn_into::<web_sys::HtmlImageElement>()
+        .unwrap();
+    new_image.set_id("output-image-target");
+    body.append_child(&new_image)
+        .expect("Failed to append output image target to document body.");
+    log::info!("Created new output target image: {:?}", &new_image);
+    new_image
 }
 
 #[allow(dead_code)]
