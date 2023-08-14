b3628898fc5c8f378ad4f231fa8973e33379335f
Nicolas Silva <nical@fastmail.com>
2023-08-14 16:05:11 +0200

    Add a test.

--- tests/tests/device.rs
+++ tests/tests/device.rs
@@ -36,3 +36,76 @@ fn device_mismatch() {
         ctx.device.poll(wgpu::Maintain::Poll);
     });
 }
+
+#[test]
+#[cfg(not(target_arch = "wasm32"))]
+fn extra_limits() {
+    let max_buffer_size = 8192;
+
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        extra_limits: Some(Box::new(wgpu::Limits {
+            max_buffer_size,
+            ..Default::default()
+        })),
+        ..Default::default()
+    });
+
+    for adapter in instance.enumerate_adapters(wgpu::Backends::all()) {
+        assert!(
+            adapter.limits().max_buffer_size <= max_buffer_size,
+            "adapter.max_buffer_size {:?} should be <= {max_buffer_size}",
+            adapter.limits().max_buffer_size
+        );
+    }
+
+    let adapter = pollster::block_on(wgpu::util::initialize_adapter_from_env_or_default(
+        &instance, None,
+    ))
+    .expect("No suitable GPU adapters found on the system!");
+
+    assert!(adapter.limits().max_buffer_size <= max_buffer_size);
+
+    let should_fail = pollster::block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            label: None,
+            features: wgpu::Features::default(),
+            limits: wgpu::Limits {
+                max_buffer_size: max_buffer_size + 1,
+                ..Default::default()
+            },
+        },
+        None,
+    ));
+
+    assert!(should_fail.is_err());
+
+    let (device, _) = pollster::block_on(adapter.request_device(
+        &wgpu::DeviceDescriptor {
+            label: None,
+            features: wgpu::Features::default(),
+            limits: wgpu::Limits {
+                max_buffer_size,
+                ..Default::default()
+            },
+        },
+        None,
+    ))
+    .unwrap();
+
+    device.push_error_scope(wgpu::ErrorFilter::Validation);
+
+    let panicked = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
+        let _buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: 9000,
+            usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE,
+            mapped_at_creation: false,
+        });
+    }))
+    .is_err();
+
+    let validation_failed = pollster::block_on(device.pop_error_scope()).is_some();
+
+    assert!(!panicked);
+    assert!(validation_failed);
+}
