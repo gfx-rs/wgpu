fe49dc757f292b9c652d52d67203b77f6bb841ac
JustAnotherCodemonkey <sophia.waggoner.personal@gmail.com>
2023-09-03 06:45:07 -0700

    Add small documentation to add_web_nothing_to_see_msg and change it to use h1 instead of p.

--- examples/common/src/utils.rs
+++ examples/common/src/utils.rs
@@ -3,13 +3,14 @@ use std::io::Write;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
+/// Replaces the site body with a message telling the user to open the console and use that.
 #[cfg(target_arch = "wasm32")]
 pub fn add_web_nothing_to_see_msg() {
     web_sys::window()
         .and_then(|window| window.document())
         .and_then(|document| document.body())
         .expect("Could not get document / body.")
-        .set_inner_html("<p>Nothing to see here! Open the console!</p>");
+        .set_inner_html("<h1>Nothing to see here! Open the console!</h1>");
 }
 
 #[cfg(not(target_arch = "wasm32"))]
