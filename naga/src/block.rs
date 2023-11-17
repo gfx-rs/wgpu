033268617785fa7759c5fec222b7c8de2dc3058b
teoxoy <28601907+teoxoy@users.noreply.github.com>
2023-11-17 01:40:36 +0100

    [naga] remove `span` feature

--- naga/src/block.rs
+++ naga/src/block.rs
@@ -8,7 +8,6 @@ use std::ops::{Deref, DerefMut, RangeBounds};
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub struct Block {
     body: Vec<Statement>,
-    #[cfg(feature = "span")]
     #[cfg_attr(feature = "serialize", serde(skip))]
     span_info: Vec<Span>,
 }
@@ -17,27 +16,20 @@ impl Block {
     pub const fn new() -> Self {
         Self {
             body: Vec::new(),
-            #[cfg(feature = "span")]
             span_info: Vec::new(),
         }
     }
 
     pub fn from_vec(body: Vec<Statement>) -> Self {
-        #[cfg(feature = "span")]
         let span_info = std::iter::repeat(Span::default())
             .take(body.len())
             .collect();
-        Self {
-            body,
-            #[cfg(feature = "span")]
-            span_info,
-        }
+        Self { body, span_info }
     }
 
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
             body: Vec::with_capacity(capacity),
-            #[cfg(feature = "span")]
             span_info: Vec::with_capacity(capacity),
         }
     }
@@ -45,7 +37,6 @@ impl Block {
     #[allow(unused_variables)]
     pub fn push(&mut self, end: Statement, span: Span) {
         self.body.push(end);
-        #[cfg(feature = "span")]
         self.span_info.push(span);
     }
 
@@ -56,43 +47,31 @@ impl Block {
     }
 
     pub fn extend_block(&mut self, other: Self) {
-        #[cfg(feature = "span")]
         self.span_info.extend(other.span_info);
         self.body.extend(other.body);
     }
 
     pub fn append(&mut self, other: &mut Self) {
-        #[cfg(feature = "span")]
         self.span_info.append(&mut other.span_info);
         self.body.append(&mut other.body);
     }
 
     pub fn cull<R: RangeBounds<usize> + Clone>(&mut self, range: R) {
-        #[cfg(feature = "span")]
         self.span_info.drain(range.clone());
         self.body.drain(range);
     }
 
     pub fn splice<R: RangeBounds<usize> + Clone>(&mut self, range: R, other: Self) {
-        #[cfg(feature = "span")]
         self.span_info.splice(range.clone(), other.span_info);
         self.body.splice(range, other.body);
     }
     pub fn span_iter(&self) -> impl Iterator<Item = (&Statement, &Span)> {
-        #[cfg(feature = "span")]
         let span_iter = self.span_info.iter();
-        #[cfg(not(feature = "span"))]
-        let span_iter = std::iter::repeat_with(|| &Span::UNDEFINED);
-
         self.body.iter().zip(span_iter)
     }
 
     pub fn span_iter_mut(&mut self) -> impl Iterator<Item = (&mut Statement, Option<&mut Span>)> {
-        #[cfg(feature = "span")]
         let span_iter = self.span_info.iter_mut().map(Some);
-        #[cfg(not(feature = "span"))]
-        let span_iter = std::iter::repeat_with(|| None);
-
         self.body.iter_mut().zip(span_iter)
     }
 
