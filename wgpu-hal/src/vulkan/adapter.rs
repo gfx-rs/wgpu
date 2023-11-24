1247f99a19c810af6fd5d8c1a48094aa4c2f6996
Almar Klein <almar@almarklein.org>
2023-11-24 22:01:30 +0100

    Add for Vulkan

--- wgpu-hal/src/vulkan/adapter.rs
+++ wgpu-hal/src/vulkan/adapter.rs
@@ -535,6 +535,8 @@ impl PhysicalDeviceFeatures {
             supports_bgra8unorm_storage(instance, phd, caps.device_api_version),
         );
 
+        features.set(F::FLOAT32_FILTERABLE, is_float32_filterable_supported(instance, phd) );
+
         (features, dl_flags)
     }
 
@@ -1754,6 +1756,15 @@ fn is_format_16bit_norm_supported(instance: &ash::Instance, phd: vk::PhysicalDev
     );
 
     r16unorm && r16snorm && rg16unorm && rg16snorm && rgba16unorm && rgba16snorm
+}
+
+fn is_float32_filterable_supported(instance: &ash::Instance, phd: vk::PhysicalDevice) -> bool {
+    let tiling = vk::ImageTiling::OPTIMAL;
+    let features = vk::FormatFeatureFlags::SAMPLED_IMAGE_FILTER_LINEAR;
+    let r_float = supports_format(instance, phd, vk::Format::R32_SFLOAT, tiling, features);
+    let rg_float = supports_format(instance, phd, vk::Format::R32G32_SFLOAT, tiling, features);
+    let rgba_float = supports_format(instance, phd, vk::Format::R32G32B32A32_SFLOAT, tiling, features);
+    r_float && rg_float && rgba_float
 }
 
 fn supports_format(
