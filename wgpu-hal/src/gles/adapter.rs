5c44b88349929e849b524ffdd148401d17dd1144
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2024-01-03 22:25:42 -0500

    Fix unreachable

--- wgpu-hal/src/gles/adapter.rs
+++ wgpu-hal/src/gles/adapter.rs
@@ -1072,7 +1072,7 @@ impl crate::Adapter<super::Api> for super::Adapter {
             | Tf::Depth32FloatStencil8
             | Tf::Depth24Plus
             | Tf::Depth24PlusStencil8 => depth,
-            Tf::NV12 => unreachable!(),
+            Tf::NV12 => empty,
             Tf::Rgb9e5Ufloat => filterable,
             Tf::Bc1RgbaUnorm
             | Tf::Bc1RgbaUnormSrgb
