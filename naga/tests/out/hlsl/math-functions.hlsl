85b8457d63e08b0b368b187425d33831c9c086c1
Erich Gubler <erichdongubler@gmail.com>
2024-01-10 17:30:24 -0500

    feat(const_eval): impl. `abs` with new `component_wise_scalar`

--- naga/tests/out/hlsl/math-functions.hlsl
+++ naga/tests/out/hlsl/math-functions.hlsl
@@ -77,7 +77,7 @@ void main()
     float sign_c = sign(-1.0);
     float4 sign_d = sign((-1.0).xxxx);
     int const_dot = dot((int2)0, (int2)0);
-    uint first_leading_bit_abs = firstbithigh(abs(0u));
+    uint first_leading_bit_abs = firstbithigh(0u);
     int flb_a = asint(firstbithigh(-1));
     int2 flb_b = asint(firstbithigh((-1).xx));
     uint2 flb_c = firstbithigh((1u).xx);
@@ -95,8 +95,8 @@ void main()
     int2 ctz_h = asint(min((32u).xx, firstbitlow((1).xx)));
     int clz_a = (-1 < 0 ? 0 : 31 - asint(firstbithigh(-1)));
     uint clz_b = (31u - firstbithigh(1u));
-    int2 _expr68 = (-1).xx;
-    int2 clz_c = (_expr68 < (0).xx ? (0).xx : (31).xx - asint(firstbithigh(_expr68)));
+    int2 _expr67 = (-1).xx;
+    int2 clz_c = (_expr67 < (0).xx ? (0).xx : (31).xx - asint(firstbithigh(_expr67)));
     uint2 clz_d = ((31u).xx - firstbithigh((1u).xx));
     float lde_a = ldexp(1.0, 2);
     float2 lde_b = ldexp(float2(1.0, 2.0), int2(3, 4));
