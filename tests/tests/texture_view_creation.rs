ac9a3a69dffeb19e8ca998bb01ab93d7ffbda8c7
teoxoy <28601907+teoxoy@users.noreply.github.com>
2024-01-19 21:57:28 +0100

    skip stencil only view creation on WebGL

--- tests/tests/texture_view_creation.rs
+++ tests/tests/texture_view_creation.rs
@@ -1,9 +1,14 @@
 use wgpu::*;
-use wgpu_test::{gpu_test, GpuTestConfiguration};
+use wgpu_test::{gpu_test, FailureCase, GpuTestConfiguration, TestParameters};
 
 #[gpu_test]
-static STENCIL_ONLY_VIEW_CREATION: GpuTestConfiguration =
-    GpuTestConfiguration::new().run_async(|ctx| async move {
+static STENCIL_ONLY_VIEW_CREATION: GpuTestConfiguration = GpuTestConfiguration::new()
+    .parameters(
+        TestParameters::default()
+            .skip(FailureCase::webgl2()) // WebGL doesn't have stencil only views
+            .limits(wgpu::Limits::downlevel_defaults()),
+    )
+    .run_async(|ctx| async move {
         for format in [TextureFormat::Stencil8, TextureFormat::Depth24PlusStencil8] {
             let texture = ctx.device.create_texture(&TextureDescriptor {
                 label: None,
