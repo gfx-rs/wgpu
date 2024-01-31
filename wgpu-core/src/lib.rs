44b96c1be7dd70593dbe05af72015a312bfc2fa2
Erich Gubler <erichdongubler@gmail.com>
2024-01-30 21:45:04 -0500

    Revert "docs: inline `document-features` usage, remove dep."
    This reverts commit 3d5bec659b9cf19f1c64274de0d11808d771cc66, with an
    update to `document-features`, and preferring to keep new `feature`
    content. To be clear, the only difference I have observed is the
    addition of the `serde` feature.
    
    In case it shortens anyone's search, the specific issue resolved is
    [`slint-ui/document-features`#20](https://github.com/slint-ui/document-features/issues/20).

--- wgpu-core/src/lib.rs
+++ wgpu-core/src/lib.rs
@@ -3,36 +3,8 @@
 //! into other language-specific user-friendly libraries.
 //!
 //! ## Feature flags
-// NOTE: feature docs. below should be kept in sync. with `Cargo.toml`!
+#![doc = document_features::document_features!()]
 //!
-//! - **`api_log_info`** --- Log all API entry points at info instead of trace level.
-//! - **`resource_log_info`** --- Log resource lifecycle management at info instead of trace level.
-//! - **`link`** _(enabled by default)_ --- Use static linking for libraries. Disable to manually
-//!   link. Enabled by default.
-//! - **`renderdoc`** --- Support the Renderdoc graphics debugger:
-//!   [https://renderdoc.org/](https://renderdoc.org/)
-//! - **`strict_asserts`** --- Apply run-time checks, even in release builds. These are in addition
-//!   to the validation carried out at public APIs in all builds.
-//! - **`serde`** --- Enables serialization via `serde` on common wgpu types.
-//! - **`trace`** --- Enable API tracing.
-//! - **`replay`** --- Enable API replaying
-//! - **`wgsl`** --- Enable `ShaderModuleSource::Wgsl`
-//! - **`fragile-send-sync-non-atomic-wasm`** --- Implement `Send` and `Sync` on Wasm, but only if
-//!   atomics are not enabled.
-//!
-//!   WebGL/WebGPU objects can not be shared between threads. However, it can be useful to
-//!   artificially mark them as `Send` and `Sync` anyways to make it easier to write cross-platform
-//!   code. This is technically _very_ unsafe in a multithreaded environment, but on a wasm binary
-//!   compiled without atomics we know we are definitely not in a multithreaded environment.
-//!
-//!  ### Backends, passed through to wgpu-hal
-//!
-//!  - **`metal`** --- Enable the `metal` backend.
-//!  - **`vulkan`** --- Enable the `vulkan` backend.
-//!  - **`gles`** --- Enable the `GLES` backend.
-//!
-//!    This is used for all of GLES, OpenGL, and WebGL.
-//!  - **`dx12`** --- Enable the `dx12` backend.
 
 // When we have no backends, we end up with a lot of dead or otherwise unreachable code.
 #![cfg_attr(
