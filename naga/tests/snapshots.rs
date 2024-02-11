50f196632dfa5124b0a8b74150111c72a9aa6960
Imbris <imbrisf@gmail.com>
2024-02-11 13:48:46 -0500

    Add test case to naga/spv-in for an unnamed glPerVertex struct

--- naga/tests/snapshots.rs
+++ naga/tests/snapshots.rs
@@ -886,6 +886,11 @@ fn convert_spv_all() {
         true,
         Targets::METAL | Targets::GLSL | Targets::HLSL | Targets::WGSL,
     );
+    convert_spv(
+        "unnamed-gl-per-vertex",
+        true,
+        Targets::METAL | Targets::GLSL | Targets::HLSL | Targets::WGSL,
+    );
 }
 
 #[cfg(feature = "glsl-in")]
