98f4ca0742eae8e618b3d843f671dc88e8d9aced
teoxoy <28601907+teoxoy@users.noreply.github.com>
2024-02-14 15:20:29 +0100

    rename `ExpressionConstnessTracker` to `ExpressionKindTracker`

--- naga/src/valid/interface.rs
+++ naga/src/valid/interface.rs
@@ -400,7 +400,7 @@ impl super::Validator {
         var: &crate::GlobalVariable,
         gctx: crate::proc::GlobalCtx,
         mod_info: &ModuleInfo,
-        global_expr_kind: &crate::proc::ExpressionConstnessTracker,
+        global_expr_kind: &crate::proc::ExpressionKindTracker,
     ) -> Result<(), GlobalVariableError> {
         use super::TypeFlags;
 
@@ -548,7 +548,7 @@ impl super::Validator {
         ep: &crate::EntryPoint,
         module: &crate::Module,
         mod_info: &ModuleInfo,
-        global_expr_kind: &crate::proc::ExpressionConstnessTracker,
+        global_expr_kind: &crate::proc::ExpressionKindTracker,
     ) -> Result<FunctionInfo, WithSpan<EntryPointError>> {
         if ep.early_depth_test.is_some() {
             let required = Capabilities::EARLY_DEPTH_TEST;
