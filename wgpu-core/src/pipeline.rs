86cbb652afc29f73cf232dbd028b6cdd6ff57786
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-03-14 09:37:25 +0000

    Address (most) review comments

--- wgpu-core/src/pipeline.rs
+++ wgpu-core/src/pipeline.rs
@@ -260,6 +260,7 @@ pub struct ComputePipelineDescriptor<'a> {
     pub layout: Option<PipelineLayoutId>,
     /// The compiled compute stage and its entry point.
     pub stage: ProgrammableStageDescriptor<'a>,
+    /// The pipeline cache to use when creating this pipeline.
     pub cache: Option<PipelineCacheId>,
 }
 
@@ -447,6 +448,7 @@ pub struct RenderPipelineDescriptor<'a> {
     /// If the pipeline will be used with a multiview render pass, this indicates how many array
     /// layers the attachments will have.
     pub multiview: Option<NonZeroU32>,
+    /// The pipeline cache to use when creating this pipeline.
     pub cache: Option<PipelineCacheId>,
 }
 
