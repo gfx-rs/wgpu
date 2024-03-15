79693877025da923df0e053f36362b1214a0c296
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-03-15 18:05:59 +0000

    Sketch out the cache header API fully

--- wgpu-core/src/pipeline.rs
+++ wgpu-core/src/pipeline.rs
@@ -1,5 +1,6 @@
 #[cfg(feature = "trace")]
 use crate::device::trace;
+pub use crate::pipeline_cache::PipelineCacheValidationError;
 use crate::{
     binding_model::{CreateBindGroupLayoutError, CreatePipelineLayoutError, PipelineLayout},
     command::ColorAttachmentError,
@@ -267,7 +268,7 @@ pub enum CreatePipelineCacheError {
     #[error(transparent)]
     Device(#[from] DeviceError),
     #[error("Pipeline cache validation failed")]
-    Validation,
+    Validation(PipelineCacheValidationError),
     #[error(transparent)]
     MissingFeatures(#[from] MissingFeatures),
     #[error("Internal error: {0}")]
