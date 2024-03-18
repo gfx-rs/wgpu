86d134397e64bd39a6129f009c7187520e68439d
robtfm <50659922+robtfm@users.noreply.github.com>
2024-03-18 14:53:41 +0000

    suspect all the future suspects (#5413)
    * suspect all the future suspects
    
    * changelog

--- wgpu-core/src/device/life.rs
+++ wgpu-core/src/device/life.rs
@@ -309,12 +309,12 @@ impl<A: HalApi> LifetimeTracker<A> {
     }
 
     pub fn post_submit(&mut self) {
-        for v in self.future_suspected_buffers.drain(..).take(1) {
+        for v in self.future_suspected_buffers.drain(..) {
             self.suspected_resources
                 .buffers
                 .insert(v.as_info().tracker_index(), v);
         }
-        for v in self.future_suspected_textures.drain(..).take(1) {
+        for v in self.future_suspected_textures.drain(..) {
             self.suspected_resources
                 .textures
                 .insert(v.as_info().tracker_index(), v);
