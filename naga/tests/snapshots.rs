17ef6cac9054163b450c2f89c8fcc183aada9db1
wicast <wicastchen@hotmail.com>
2024-04-06 18:00:10 +0800

    fix(spv-out): OpSourceContinued for large source (gfx-rs#5390)

--- naga/tests/snapshots.rs
+++ naga/tests/snapshots.rs
@@ -886,11 +886,15 @@ fn convert_wgsl() {
         let inputs = [
             ("debug-symbol-simple", Targets::SPIRV),
             ("debug-symbol-terrain", Targets::SPIRV),
+            ("debug-symbol-large-source", Targets::SPIRV),
         ];
         for &(name, targets) in inputs.iter() {
             // WGSL shaders lives in root dir as a privileged.
             let input = Input::new(None, name, "wgsl");
             let source = input.read_source();
+
+            // crlf will make the large split output different on different platform
+            let source = source.replace('\r', "");
             match naga::front::wgsl::parse_str(&source) {
                 Ok(mut module) => check_targets(&input, &mut module, targets, Some(&source)),
                 Err(e) => panic!(
