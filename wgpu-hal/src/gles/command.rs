548fe573e518c8dffd9006e4fb87819cdfe5caa8
Nick Guletskii <nick@nickguletskii.com>
2024-05-05 14:54:38 +0300

    Issue SetDrawColorBuffers commands before issuing ClearColor
    This is necessary for glClearBuffer calls to work correctly on some machines (e.g. AMD Renoir graphics running on Linux). Without this, glClearBuffer calls are ignored.

--- wgpu-hal/src/gles/command.rs
+++ wgpu-hal/src/gles/command.rs
@@ -604,6 +604,14 @@ impl crate::CommandEncoder for super::CommandEncoder {
             depth: 0.0..1.0,
         });
 
+
+        if !rendering_to_external_framebuffer {
+            // set the draw buffers and states
+            self.cmd_buffer
+                .commands
+                .push(C::SetDrawColorBuffers(desc.color_attachments.len() as u8));
+        }
+
         // issue the clears
         for (i, cat) in desc
             .color_attachments
@@ -632,13 +640,6 @@ impl crate::CommandEncoder for super::CommandEncoder {
                     },
                 );
             }
-        }
-
-        if !rendering_to_external_framebuffer {
-            // set the draw buffers and states
-            self.cmd_buffer
-                .commands
-                .push(C::SetDrawColorBuffers(desc.color_attachments.len() as u8));
         }
 
         if let Some(ref dsat) = desc.depth_stencil_attachment {
