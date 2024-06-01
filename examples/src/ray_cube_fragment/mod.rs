b814fc497fe6e4d74f89d848dd63bc65ef109bc6
Vecvec <vectorsofvectors@gmail.com>
2024-06-02 08:10:22 +1200

    fix merge

--- examples/src/ray_cube_fragment/mod.rs
+++ examples/src/ray_cube_fragment/mod.rs
@@ -202,11 +202,13 @@ impl crate::framework::Example for Example {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
+                compilation_options: Default::default(),
                 buffers: &[],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
                 entry_point: "fs_main",
+                compilation_options: Default::default(),
                 targets: &[Some(config.format.into())],
             }),
             primitive: wgpu::PrimitiveState {
@@ -216,6 +218,7 @@ impl crate::framework::Example for Example {
             depth_stencil: None,
             multisample: wgpu::MultisampleState::default(),
             multiview: None,
+            cache: None,
         });
 
         let bind_group_layout = pipeline.get_bind_group_layout(0);
