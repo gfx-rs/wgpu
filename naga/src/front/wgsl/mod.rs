25d133f972cc218c43d247f5d590e3e30146a9b3
stefnotch <brandmairstefan@gmail.com>
2024-06-07 14:48:32 +0200

    Design public API

--- naga/src/front/wgsl/mod.rs
+++ naga/src/front/wgsl/mod.rs
@@ -18,7 +18,9 @@ use thiserror::Error;
 
 pub use crate::front::wgsl::error::ParseError;
 use crate::front::wgsl::lower::Lowerer;
-use crate::Scalar;
+use crate::{Arena, FastHashMap, Scalar};
+
+use self::parse::ast::TranslationUnit;
 
 pub struct Frontend {
     parser: Parser,
@@ -32,18 +34,56 @@ impl Frontend {
     }
 
     pub fn parse(&mut self, source: &str) -> Result<crate::Module, ParseError> {
-        self.inner(source).map_err(|x| x.as_parse_error(source))
+        self.inner_to_ast(source)
+            .map_err(|x| x.as_parse_error(source))
+            .and_then(|v| v.to_module())
     }
 
-    fn inner<'a>(&mut self, source: &'a str) -> Result<crate::Module, Error<'a>> {
-        let tu = self.parser.parse(source)?;
-        let index = index::Index::generate(&tu)?;
-        let module = Lowerer::new(&index).lower(&tu)?;
+    /// Two-step module conversion, can be used to modify the intermediate structure before it becomes a module.
+    pub fn parse_to_ast<'a>(&mut self, source: &'a str) -> Result<ParsedWgsl<'a>, ParseError> {
+        self.inner_to_ast(source)
+            .map_err(|x| x.as_parse_error(source))
+    }
 
-        Ok(module)
+    fn inner_to_ast<'a>(&mut self, source: &'a str) -> Result<ParsedWgsl<'a>, Error<'a>> {
+        let mut result = ParsedWgsl {
+            source,
+            translation_unit: self.parser.parse(source)?,
+            index: index::Index::empty(),
+            globals: Default::default(),
+        };
+        result.index = index::Index::generate(&result.translation_unit)?;
+        Ok(result)
     }
 }
 
+pub enum ProvidedGlobalDecl {
+    Function(crate::Function),
+    Var(crate::GlobalVariable),
+    Const(crate::Constant),
+    Override(crate::Override),
+    Type(crate::Type),
+    EntryPoint(crate::EntryPoint),
+}
+
+pub struct ParsedWgsl<'a> {
+    source: &'a str,
+    translation_unit: TranslationUnit<'a>,
+    index: index::Index<'a>,
+    // global_names: Arena<String>,
+    globals: FastHashMap<String, ProvidedGlobalDecl>,
+}
+impl<'a> ParsedWgsl<'a> {
+    pub fn to_module(&self) -> Result<crate::Module, ParseError> {
+        Lowerer::new(&self.index)
+            .lower(&self.translation_unit, &self.globals)
+            .map_err(|x| x.as_parse_error(self.source))
+    }
+
+    pub fn add_global(&mut self, name: String, value: ProvidedGlobalDecl) {
+        self.globals.insert(name, value);
+    }
+}
 /// <div class="warning">
 // NOTE: Keep this in sync with `wgpu::Device::create_shader_module`!
 // NOTE: Keep this in sync with `wgpu_core::Global::device_create_shader_module`!
