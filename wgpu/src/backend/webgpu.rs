6c370522a72e89c8784ed64c9eb574f1f54d5bd4
Jim Blandy <jimb@red-bean.com>
2024-06-10 11:20:42 -0700

    [wgpu] Remove unused `ObjectId` field of `SubmissionIndex`. (#5780)
    Drop the first field of `wgpu::SubmissionIndex`, as it is unused.
    Adjust uses of the types' other field.
    
    Delete the `wgpu::context::Context` trait's `SubmissionIndex`
    associated type. Remove it from implementations.
    
    Drop the first field of the `Context` trait's `queue_submit` method's
    return value. Adjust implementations.

--- wgpu/src/backend/webgpu.rs
+++ wgpu/src/backend/webgpu.rs
@@ -1158,7 +1158,6 @@ impl crate::context::Context for ContextWebGpu {
     type SurfaceData = Sendable<(Canvas, webgpu_sys::GpuCanvasContext)>;
 
     type SurfaceOutputDetail = SurfaceOutputDetail;
-    type SubmissionIndex = Unused;
     type SubmissionIndexData = ();
     type PipelineCacheId = Unused;
     type PipelineCacheData = ();
@@ -2950,14 +2949,12 @@ impl crate::context::Context for ContextWebGpu {
         _queue: &Self::QueueId,
         queue_data: &Self::QueueData,
         command_buffers: I,
-    ) -> (Self::SubmissionIndex, Self::SubmissionIndexData) {
+    ) -> Self::SubmissionIndexData {
         let temp_command_buffers = command_buffers
             .map(|(_, data)| data.0)
             .collect::<js_sys::Array>();
 
         queue_data.0.submit(&temp_command_buffers);
-
-        (Unused, ())
     }
 
     fn queue_get_timestamp_period(
