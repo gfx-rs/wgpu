4548b5724eb836c065b14537d446ee22804e5a07
Andreas Reich <r_andreas2@web.de>
2024-06-30 10:14:18 +0200

    reduce number of draws & computes when only running tests

--- benches/benches/renderpass.rs
+++ benches/benches/renderpass.rs
@@ -10,7 +10,12 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::DeviceState;
 
+#[cfg(test)]
+const DRAW_COUNT: usize = 8; // Running with up to 8 threads.
+
+#[cfg(not(test))]
 const DRAW_COUNT: usize = 10_000;
+
 // Must match the number of textures in the renderpass.wgsl shader
 const TEXTURES_PER_DRAW: usize = 7;
 const VERTEX_BUFFERS_PER_DRAW: usize = 2;
