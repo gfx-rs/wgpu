a0ff000560e9e024b5923d3197fba94006107293
Andreas Reich <r_andreas2@web.de>
2024-07-06 11:04:02 +0200

    Introduce `msl-out-if-target-apple` feature to Naga

--- naga/build.rs
+++ naga/build.rs
@@ -3,7 +3,7 @@ fn main() {
         dot_out: { feature = "dot-out" },
         glsl_out: { feature = "glsl-out" },
         hlsl_out: { feature = "hlsl-out" },
-        msl_out: { feature = "msl-out" },
+        msl_out: { any(feature = "msl-out", all(any(target_os = "ios", target_os = "macos"), feature = "msl-out-if-target-apple")) },
         spv_out: { feature = "spv-out" },
         wgsl_out: { feature = "wgsl-out" },
     }
