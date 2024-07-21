942ba2e64a44bb0951fbd776970f018f57b7806f
Andreas Reich <r_andreas2@web.de>
2024-07-21 12:52:25 +0200

    implement buffer operations on DynDevice

--- wgpu-hal/src/dynamic/device.rs
+++ wgpu-hal/src/dynamic/device.rs
@@ -1,14 +1,67 @@
-use crate::{Device, DynBuffer, DynResource};
+use crate::{
+    Api, BufferDescriptor, BufferMapping, Device, DeviceError, DynBuffer, DynResource, MemoryRange,
+};
 
-use super::DynResourceExt;
+use super::DynResourceExt as _;
 
 pub trait DynDevice: DynResource {
+    unsafe fn create_buffer(
+        &self,
+        desc: &BufferDescriptor,
+    ) -> Result<Box<dyn DynBuffer>, DeviceError>;
+
     unsafe fn destroy_buffer(&self, buffer: Box<dyn DynBuffer>);
+
+    unsafe fn map_buffer(
+        &self,
+        buffer: &dyn DynBuffer,
+        range: MemoryRange,
+    ) -> Result<BufferMapping, DeviceError>;
+
+    unsafe fn unmap_buffer(&self, buffer: &dyn DynBuffer);
+
+    unsafe fn flush_mapped_ranges(&self, buffer: &dyn DynBuffer, ranges: &[MemoryRange]);
+    unsafe fn invalidate_mapped_ranges(&self, buffer: &dyn DynBuffer, ranges: &[MemoryRange]);
 }
 
 impl<D: Device + DynResource> DynDevice for D {
+    unsafe fn create_buffer(
+        &self,
+        desc: &BufferDescriptor,
+    ) -> Result<Box<dyn DynBuffer>, DeviceError> {
+        unsafe { D::create_buffer(self, desc) }.map(|b| {
+            let boxed_buffer: Box<<D::A as Api>::Buffer> = Box::new(b);
+            let boxed_buffer: Box<dyn DynBuffer> = boxed_buffer;
+            boxed_buffer
+        })
+    }
+
     unsafe fn destroy_buffer(&self, mut buffer: Box<dyn DynBuffer>) {
         let buffer = buffer.expect_downcast_mut();
-        unsafe { self.destroy_buffer(buffer) };
+        unsafe { D::destroy_buffer(self, buffer) };
+    }
+
+    unsafe fn map_buffer(
+        &self,
+        buffer: &dyn DynBuffer,
+        range: MemoryRange,
+    ) -> Result<BufferMapping, DeviceError> {
+        let buffer = buffer.expect_downcast_ref();
+        unsafe { D::map_buffer(self, buffer, range) }
+    }
+
+    unsafe fn unmap_buffer(&self, buffer: &dyn DynBuffer) {
+        let buffer = buffer.expect_downcast_ref();
+        unsafe { D::unmap_buffer(self, buffer) }
+    }
+
+    unsafe fn flush_mapped_ranges(&self, buffer: &dyn DynBuffer, ranges: &[MemoryRange]) {
+        let buffer = buffer.expect_downcast_ref();
+        unsafe { D::flush_mapped_ranges(self, buffer, ranges.iter().cloned()) }
+    }
+
+    unsafe fn invalidate_mapped_ranges(&self, buffer: &dyn DynBuffer, ranges: &[MemoryRange]) {
+        let buffer = buffer.expect_downcast_ref();
+        unsafe { D::invalidate_mapped_ranges(self, buffer, ranges.iter().cloned()) }
     }
 }
