35bfbf74da48bc8221e6895758def2399e4c2525
Matthew Wong <110081332+matthew-wong1@users.noreply.github.com>
2024-07-24 23:10:08 +0100

    fix error message in create_render_pass

--- wgpu/src/backend/wgpu_core.rs
+++ wgpu/src/backend/wgpu_core.rs
@@ -1975,7 +1975,7 @@ impl crate::Context for ContextWgpuCore {
                 &encoder_data.error_sink,
                 cause,
                 desc.label,
-                "CommandEncoder::begin_compute_pass",
+                "CommandEncoder::begin_render_pass",
             );
         }
 
