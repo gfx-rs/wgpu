e54f96acded4415518f0e1c13e5cdcec2f1c6265
Marijn Suijten <marijns95@gmail.com>
2024-08-23 23:06:50 +0200

    [wgpu-hal] Upgrade `glutin` to `0.31`
    `glutin 0.30` onwards completely refactored its internals to no longer
    be reliant on `winit`, as they (by default) have no direct relation
    except needing to perform _some_ operations (platform-specific) at
    strategic times in window creation and event loop handling.  Most of
    that is handled by a new `glutin-winit` introp crate, while the core
    `glutin` crate now exclusively focuses on wrapping the various OpenGL
    context APIs (CGL, EGL, WGL, ...).
    
    This does result in a little more verbose handling to get the right
    `GLDisplay`, `GLConfig`, `GLContext` and `GLSurface`, but gives much
    more control and makes all intricacies more explicit.  Most of the
    code was copied from `glutin 0.31`'s example crate, with the code for
    transparency support removed.
    
    Note that the example doesn't at all handle event loop events properly:
    resizes and redraws are not listened to, and mobile-specific surface
    events (`Resumed` and `Suspended`) are equally ignored.

--- wgpu-hal/examples/raw-gles.rs
+++ wgpu-hal/examples/raw-gles.rs
@@ -10,61 +10,151 @@
 
 extern crate wgpu_hal as hal;
 
-#[cfg(not(any(windows, target_arch = "wasm32")))]
+#[cfg(not(any(windows, target_arch = "wasm32", target_os = "ios")))]
 fn main() {
+    use std::ffi::CString;
+
+    use glutin::{
+        config::GlConfig as _,
+        context::{NotCurrentGlContext as _, Version},
+        display::{GetGlDisplay as _, GlDisplay as _},
+        surface::GlSurface as _,
+    };
+    use glutin_winit::GlWindow as _;
+    use rwh_05::HasRawWindowHandle as _;
+
     env_logger::init();
     println!("Initializing external GL context");
 
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let window_builder = glutin::window::WindowBuilder::new();
-    let gl_context = unsafe {
-        glutin::ContextBuilder::new()
-            .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGlEs, (3, 0)))
-            .build_windowed(window_builder, &event_loop)
-            .unwrap()
-            .make_current()
-            .unwrap()
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    // Only Windows requires the window to be present before creating the display.
+    // Other platforms don't really need one.
+    let window_builder = cfg!(windows).then(|| {
+        winit::window::WindowBuilder::new()
+            .with_title("WGPU raw GLES example (press Escape to exit)")
+    });
+
+    // The template will match only the configurations supporting rendering
+    // to Windows.
+    let template = glutin::config::ConfigTemplateBuilder::new();
+
+    let display_builder = glutin_winit::DisplayBuilder::new().with_window_builder(window_builder);
+
+    // Find the config with the maximum number of samples, so our triangle will be
+    // smooth.
+    pub fn gl_config_picker(
+        configs: Box<dyn Iterator<Item = glutin::config::Config> + '_>,
+    ) -> glutin::config::Config {
+        configs
+            .reduce(|accum, config| {
+                if config.num_samples() > accum.num_samples() {
+                    config
+                } else {
+                    accum
+                }
+            })
+            .expect("Failed to find a matching config")
+    }
+
+    let (mut window, gl_config) = display_builder
+        .build(&event_loop, template, gl_config_picker)
+        .expect("Failed to build window and config from display");
+
+    println!("Picked a config with {} samples", gl_config.num_samples());
+
+    let raw_window_handle = window.as_ref().map(|window| window.raw_window_handle());
+
+    // XXX The display could be obtained from any object created by it, so we can
+    // query it from the config.
+    let gl_display = gl_config.display();
+
+    // Glutin tries to create an OpenGL context by default.  Force it to use any version of GLES.
+    let context_attributes = glutin::context::ContextAttributesBuilder::new()
+        // WGPU expects GLES 3.0+.
+        .with_context_api(glutin::context::ContextApi::Gles(Some(Version::new(3, 0))))
+        .build(raw_window_handle);
+
+    // TODO: Wrap in Option
+    let mut not_current_gl_context = unsafe {
+        gl_display
+            .create_context(&gl_config, &context_attributes)
+            .expect("failed to create context")
     };
-    let inner_size = gl_context.window().inner_size();
+
+    // TODO: For Android support this should happen inside Event::Resumed, and the inverse inside ::Suspended.
+    let window = window.take().unwrap_or_else(|| {
+        let window_builder = winit::window::WindowBuilder::new()
+            .with_title("WGPU raw GLES example (press Escape to exit)");
+        glutin_winit::finalize_window(&event_loop, window_builder, &gl_config).unwrap()
+    });
+
+    let attrs = window.build_surface_attributes(Default::default());
+    let gl_surface = unsafe {
+        gl_config
+            .display()
+            .create_window_surface(&gl_config, &attrs)
+            .expect("Cannot create GL WindowSurface")
+    };
+
+    // Make it current.
+    // let gl_context = not_current_gl_context
+    //     .take()
+    //     .unwrap()
+    //     .make_current(&gl_surface)
+    //     .unwrap();
+    let gl_context = not_current_gl_context
+        .make_current(&gl_surface)
+        .expect("GL context cannot be made current with WindowSurface");
 
     println!("Hooking up to wgpu-hal");
     let exposed = unsafe {
         <hal::api::Gles as hal::Api>::Adapter::new_external(|name| {
-            gl_context.get_proc_address(name)
+            // XXX: On WGL this should only be called after the context was made current
+            gl_config
+                .display()
+                .get_proc_address(&CString::new(name).expect(name))
         })
     }
     .expect("GL adapter can't be initialized");
 
+    let inner_size = window.inner_size();
+
     fill_screen(&exposed, inner_size.width, inner_size.height);
 
+    // TODO: Swap in RedrawRequested
     println!("Showing the window");
-    gl_context.swap_buffers().unwrap();
+    gl_surface
+        .swap_buffers(&gl_context)
+        .expect("Failed to swap buffers");
 
-    event_loop.run(move |event, _, control_flow| {
-        use glutin::{
-            event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-            event_loop::ControlFlow,
-        };
-        *control_flow = ControlFlow::Wait;
+    event_loop
+        .run(move |event, window_target| {
+            use winit::{
+                event::{Event, KeyEvent, WindowEvent},
+                event_loop::ControlFlow,
+                keyboard::{Key, NamedKey},
+            };
+            window_target.set_control_flow(ControlFlow::Wait);
 
-        match event {
-            Event::LoopDestroyed => (),
-            Event::WindowEvent {
-                event:
-                    WindowEvent::CloseRequested
-                    | WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
+            match event {
+                // Event::LoopExiting => (),
+                Event::WindowEvent {
+                    window_id: _,
+                    event:
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            event:
+                                KeyEvent {
+                                    logical_key: Key::Named(NamedKey::Escape),
+                                    ..
+                                },
+                            ..
+                        },
+                } => window_target.exit(),
+                _ => (),
+            }
+        })
+        .expect("Couldn't run event loop");
 }
 
 #[cfg(target_os = "emscripten")]
@@ -117,10 +207,20 @@ fn main() {
     fill_screen(&exposed, 640, 400);
 }
 
-#[cfg(any(windows, all(target_arch = "wasm32", not(target_os = "emscripten"))))]
-fn main() {}
+#[cfg(any(
+    windows,
+    all(target_arch = "wasm32", not(target_os = "emscripten")),
+    target_os = "ios"
+))]
+fn main() {
+    eprintln!("This example is not supported on Windows and non-emscripten wasm32")
+}
 
-#[cfg(any(not(any(windows, target_arch = "wasm32")), target_os = "emscripten"))]
+#[cfg(not(any(
+    windows,
+    all(target_arch = "wasm32", not(target_os = "emscripten")),
+    target_os = "ios"
+)))]
 fn fill_screen(exposed: &hal::ExposedAdapter<hal::api::Gles>, width: u32, height: u32) {
     use hal::{Adapter as _, CommandEncoder as _, Device as _, Queue as _};
 
