ed7bd0da8361761402396a7cddab30f0e1d5faaf
teoxoy <28601907+teoxoy@users.noreply.github.com>
2024-09-12 14:49:45 +0200

    move queue methods on the `Queue` type

--- wgpu-types/src/lib.rs
+++ wgpu-types/src/lib.rs
@@ -6986,7 +6986,7 @@ pub struct ImageCopyTextureTagged<T> {
     pub premultiplied_alpha: bool,
 }
 
-impl<T: Copy> ImageCopyTextureTagged<T> {
+impl<T> ImageCopyTextureTagged<T> {
     /// Removes the colorspace information from the type.
     pub fn to_untagged(self) -> ImageCopyTexture<T> {
         ImageCopyTexture {
