b74beca153a11e47f6fadba42bdf9fb933fc5051
Hamir Mahal <hamirmahal@gmail.com>
2024-09-23 10:15:37 -0700

    fix: formatting in `tests/` directory

--- tests/src/image.rs
+++ tests/src/image.rs
@@ -188,9 +188,8 @@ pub async fn compare_image_output(
         sanitize_for_path(&adapter_info.driver)
     );
     // Determine the paths to write out the various intermediate files
-    let actual_path = Path::new(&path).with_file_name(
-        OsString::from_str(&format!("{file_stem}-{renderer}-actual.png")).unwrap(),
-    );
+    let actual_path = Path::new(&path)
+        .with_file_name(OsString::from_str(&format!("{file_stem}-{renderer}-actual.png")).unwrap());
     let difference_path = Path::new(&path).with_file_name(
         OsString::from_str(&format!("{file_stem}-{renderer}-difference.png",)).unwrap(),
     );
