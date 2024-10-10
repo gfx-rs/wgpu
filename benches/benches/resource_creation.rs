16f5d354054358170d73a9039f037055fa9f6186
Erich Gubler <erichdongubler@gmail.com>
2024-10-10 14:28:42 -0400

    refactor: s/once_cell::Lazy/std::sync::LazyLock
    Weaken our dependence on the `once_cell` crate by using functionality
    from `std` instead that was upstreamed from `once_cell`, this time with
    what's available in Rust 1.80+.
    
    It's not yet possible to eliminate this dependency entirely, but do what
    we can for now.

--- benches/benches/resource_creation.rs
+++ benches/benches/resource_creation.rs
@@ -1,13 +1,13 @@
 use std::time::{Duration, Instant};
 
 use criterion::{criterion_group, Criterion, Throughput};
-use once_cell::sync::Lazy;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::sync::LazyLock;
 
 use crate::DeviceState;
 
 fn run_bench(ctx: &mut Criterion) {
-    let state = Lazy::new(DeviceState::new);
+    let state = LazyLock::new(DeviceState::new);
 
     const RESOURCES_TO_CREATE: usize = 8;
 
@@ -19,7 +19,7 @@ fn run_bench(ctx: &mut Criterion) {
         group.bench_function(
             format!("{threads} threads x {resources_per_thread} resource"),
             |b| {
-                Lazy::force(&state);
+                LazyLock::force(&state);
 
                 b.iter_custom(|iters| {
                     profiling::scope!("benchmark invocation");
