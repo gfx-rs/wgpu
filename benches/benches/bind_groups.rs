c7676b0047259b39ae05cd6f62d4d3c0e41d6344
Erich Gubler <erichdongubler@gmail.com>
2024-10-10 14:28:42 -0400

    refactor(benches): s/once_cell::Lazy/std::sync::LazyLock
    Weaken our dependence on the `once_cell` crate by using functionality
    from `std` instead that was upstreamed from `once_cell`, this time with
    what's available in Rust 1.80+.
    
    It's not yet possible to eliminate this dependency entirely, but do what
    we can with `REPO_MSRV` for now.

--- benches/benches/bind_groups.rs
+++ benches/benches/bind_groups.rs
@@ -5,7 +5,7 @@ use std::{
 
 use criterion::{criterion_group, Criterion, Throughput};
 use nanorand::{Rng, WyRand};
-use once_cell::sync::Lazy;
+use std::sync::LazyLock;
 
 use crate::DeviceState;
 
@@ -60,7 +60,7 @@ impl BindGroupState {
 }
 
 fn run_bench(ctx: &mut Criterion) {
-    let state = Lazy::new(BindGroupState::new);
+    let state = LazyLock::new(BindGroupState::new);
 
     if !state
         .device_state
