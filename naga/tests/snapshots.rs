57d787eb53ce95e6ff125ef472f413ee9bb2ee36
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2024-10-20 23:50:47 -0400

    Slang Binding Array Test

--- naga/tests/snapshots.rs
+++ naga/tests/snapshots.rs
@@ -1043,6 +1043,7 @@ fn convert_spv_all() {
         Targets::HLSL | Targets::WGSL | Targets::METAL,
     );
     convert_spv("degrees", false, Targets::empty());
+    convert_spv("binding-arrays.stress", true, Targets::WGSL | Targets::IR);
     convert_spv("binding-arrays.dynamic", true, Targets::WGSL);
     convert_spv("binding-arrays.static", true, Targets::WGSL);
     convert_spv(
