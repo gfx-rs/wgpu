7e5e14764141d0835918646c2bed3da570a5df30
Erich Gubler <erichdongubler@gmail.com>
2024-11-04 22:44:12 -0500

    fixup! Fix Issue 6467: Prevent MacOS crash on invalid workgroup size definition

--- tests/tests/regression/issue_6467.rs
+++ tests/tests/regression/issue_6467.rs
@@ -9,12 +9,14 @@ use wgpu_test::{gpu_test, GpuTestConfiguration, TestParameters};
 /// The following test should successfully do nothing on all platforms.
 #[gpu_test]
 static ZERO_WORKGROUP_SIZE: GpuTestConfiguration = GpuTestConfiguration::new()
-    .parameters(TestParameters::default()
-        .limits(wgpu::Limits::default()))
+    .parameters(TestParameters::default().limits(wgpu::Limits::default()))
     .run_async(|ctx| async move {
-        let module = ctx.device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: None,
-            source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed("
+        let module = ctx
+            .device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: None,
+                source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(
+                    "
                 @group(0)
                 @binding(0)
                 var<storage, read_write> vals: array<i32>;
@@ -24,22 +26,32 @@ static ZERO_WORKGROUP_SIZE: GpuTestConfiguration = GpuTestConfiguration::new()
                 fn main(@builtin(global_invocation_id) id: vec3u) {
                     vals[id.x] = vals[id.x] * i32(id.x);
                 }
-            ")),
-        });
-        let compute_pipeline = ctx.device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-            label: None,
-            layout: None,
-            module: &module,
-            entry_point: Some("main"),
-            compilation_options: wgpu::PipelineCompilationOptions::default(),
-            cache: None,
-        });
-        let buffer = DeviceExt::create_buffer_init(&ctx.device, &wgpu::util::BufferInitDescriptor {
-            label: None,
-            contents: &[1, 1, 1, 1, 1, 1, 1, 1],
-            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
-        });
-        let mut encoder = ctx.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None, });
+            ",
+                )),
+            });
+        let compute_pipeline =
+            ctx.device
+                .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                    label: None,
+                    layout: None,
+                    module: &module,
+                    entry_point: Some("main"),
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    cache: None,
+                });
+        let buffer = DeviceExt::create_buffer_init(
+            &ctx.device,
+            &wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: &[1, 1, 1, 1, 1, 1, 1, 1],
+                usage: wgpu::BufferUsages::STORAGE
+                    | wgpu::BufferUsages::COPY_DST
+                    | wgpu::BufferUsages::COPY_SRC,
+            },
+        );
+        let mut encoder = ctx
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
         {
             let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
                 label: None,
@@ -61,4 +73,3 @@ static ZERO_WORKGROUP_SIZE: GpuTestConfiguration = GpuTestConfiguration::new()
         }
         ctx.queue.submit(Some(encoder.finish()));
     });
-
