324ae8dab68ccfbdaca8702969c646228199a2c5
Erich Gubler <erichdongubler@gmail.com>
2024-11-24 15:49:52 -0500

    chore: satisfy `clippy::doc_lazy_continuation`

--- wgpu/src/api/blas.rs
+++ wgpu/src/api/blas.rs
@@ -35,8 +35,8 @@ static_assertions::assert_impl_all!(CreateBlasDescriptor<'_>: Send, Sync);
 ///
 /// Each one contains:
 /// - A reference to a BLAS, this ***must*** be interacted with using [TlasInstance::new] or [TlasInstance::set_blas], a
-/// TlasInstance that references a BLAS keeps that BLAS from being dropped, but if the BLAS is explicitly destroyed (e.g.
-/// using [Blas::destroy]) the TlasInstance becomes invalid
+///   TlasInstance that references a BLAS keeps that BLAS from being dropped, but if the BLAS is explicitly destroyed (e.g.
+///   using [Blas::destroy]) the TlasInstance becomes invalid
 /// - A user accessible transformation matrix
 /// - A user accessible mask
 /// - A user accessible custom index
