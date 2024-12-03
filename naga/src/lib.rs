25fcfc2e2f7e74ee4bde4d7ab89b383f6a72c25f
Kent Slaney <kent@slaney.org>
2024-12-03 09:22:28 -0800

    document workgroup_size_overrides' arena

--- naga/src/lib.rs
+++ naga/src/lib.rs
@@ -2186,7 +2186,7 @@ pub struct EntryPoint {
     pub early_depth_test: Option<EarlyDepthTest>,
     /// Workgroup size for compute stages
     pub workgroup_size: [u32; 3],
-    /// Override expressions for workgroup size
+    /// Override expressions in the global_expressions arena for workgroup size
     pub workgroup_size_overrides: Option<[Option<Handle<Override>>; 3]>,
     /// The entrance function.
     pub function: Function,
