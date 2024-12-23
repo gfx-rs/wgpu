5a47cde23f0c0aaefa7e6e178fbe77c9897b3e50
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2024-12-23 17:38:36 -0500

    Fix Errors

--- wgpu-core/src/resource.rs
+++ wgpu-core/src/resource.rs
@@ -1499,8 +1499,8 @@ pub enum CreateTextureError {
     )]
     InvalidMipLevelCount { requested: u32, maximum: u32 },
     #[error(
-        "Texture usages {0:?} are not allowed on a texture of type {1:?}{}",
-        if *.2 { " due to downlevel restrictions" } else { "" }
+        "Texture usages {0:?} are not allowed on a texture of type {1:?}{downlevel_suffix}",
+        downlevel_suffix = if *.2 { " due to downlevel restrictions" } else { "" }
     )]
     InvalidFormatUsages(wgt::TextureUsages, wgt::TextureFormat, bool),
     #[error("The view format {0:?} is not compatible with texture format {1:?}, only changing srgb-ness is allowed.")]
