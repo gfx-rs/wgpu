0848f0784715247a0e2dd20125b985eca1e64535
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-01-06 21:09:24 -0500

    Comments

--- wgpu-hal/build.rs
+++ wgpu-hal/build.rs
@@ -11,6 +11,7 @@ fn main() {
         gles: { all(feature = "gles") },
         metal: { all(any(target_os = "ios", target_os = "macos"), feature = "metal") },
         vulkan: { all(not(target_arch = "wasm32"), feature = "vulkan") },
+        // ⚠️ Keep in sync with target.cfg() definition in Cargo.toml and cfg_alias in `wgpu` crate ⚠️
         static_dxc: { all(target_os = "windows", feature = "static-dxc", not(target_arch = "aarch64")) }
     }
 }
--- wgpu-types/src/lib.rs
+++ wgpu-types/src/lib.rs
@@ -7683,7 +7683,9 @@ pub enum Dx12Compiler {
         dxil_path: PathBuf,
     },
     /// The statically-linked variant of Dxc.
-    /// The `static-dxc` feature is required to use this. Not available on windows-aarch64.
+    ///
+    /// The `static-dxc` feature is required for this setting to be used successfully on DX12.
+    /// Not available on `windows-aarch64-pc-*` targets.
     StaticDxc,
 }
 
