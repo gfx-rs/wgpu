bd8a5bde01c4197a1d62953bcbda45e650bd68f1
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-01-09 14:04:05 -0500

    Fixes

--- wgpu-hal/examples/ray-traced-triangle/main.rs
+++ wgpu-hal/examples/ray-traced-triangle/main.rs
@@ -1107,7 +1107,7 @@ impl<A: hal::Api> Example<A> {
 
 cfg_if::cfg_if! {
     // Apple + Metal
-    if #[cfg(all(any(target_vendor = "apple"), feature = "metal"))] {
+    if #[cfg(all(target_vendor = "apple", feature = "metal"))] {
         type Api = hal::api::Metal;
     }
     // Wasm + Vulkan
