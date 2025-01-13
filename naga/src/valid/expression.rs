0eb31cc88c398a08a09d15f9c1f527cc40d031ef
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-01-13 17:48:23 -0500

    Update naga/src/valid/expression.rs

--- naga/src/valid/expression.rs
+++ naga/src/valid/expression.rs
@@ -258,7 +258,7 @@ impl super::Validator {
                     | Ti::Array { .. }
                     | Ti::Pointer { .. }
                     | Ti::ValuePointer { size: Some(_), .. }
-                    | Ti::BindingArray { .. } => {},
+                    | Ti::BindingArray { .. } => {}
                     ref other => {
                         log::error!("Indexing of {:?}", other);
                         return Err(ExpressionError::InvalidBaseType(base));
