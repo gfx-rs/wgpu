8f6fbf545760eba0d4965226b7f0a2817336abf9
Vecvec <vectorsofvectors@gmail.com>
2025-01-16 09:16:31 +0000

    Fix clippy.

--- tests/tests/ray_tracing/mod.rs
+++ tests/tests/ray_tracing/mod.rs
@@ -84,7 +84,7 @@ impl AsBuildContext {
                 first_vertex: 0,
                 vertex_stride: mem::size_of::<[f32; 3]>() as BufferAddress,
                 index_buffer: None,
-                index_buffer_offset: None,
+                first_index: None,
                 transform_buffer: None,
                 transform_buffer_offset: None,
             }]),
