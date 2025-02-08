910526b719bba44f7e4834d6581cb921ddae5664
Vecvec <vectorsofvectors@gmail.com>
2025-02-09 08:39:09 +1300

    Don't output dot.

--- naga/src/back/dot/mod.rs
+++ naga/src/back/dot/mod.rs
@@ -717,10 +717,8 @@ fn write_function_expressions(
             }
             E::SubgroupBallotResult => ("SubgroupBallotResult".into(), 4),
             E::SubgroupOperationResult { .. } => ("SubgroupOperationResult".into(), 4),
-            E::RayQueryVertexPositions { query, committed } => {
-                edges.insert("", query);
-                let ty = if committed { "Committed" } else { "Candidate" };
-                (format!("get{}HitVertexPositions", ty).into(), 4)
+            E::RayQueryVertexPositions { .. } => {
+                unimplemented!("RayQueryVertexPositions not supported")
             }
         };
 
