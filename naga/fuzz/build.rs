ffb32b6639ffa77f6a7c7a3b1e89ed659567c2d9
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-02-10 23:09:15 -0500

    Fix Build on Windows

--- naga/fuzz/build.rs
+++ naga/fuzz/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     cfg_aliases::cfg_aliases! {
-        enable_fuzzing: { not(any(target_arch = "wasm32", target_os = "ios", all(windows, target_arch = "aarch64"))) },
+        fuzzable_platform: { not(any(target_arch = "wasm32", target_os = "ios", all(windows, target_arch = "aarch64"))) },
     }
+    println!("cargo::rustc-check-cfg=cfg(fuzzing)");
 }
