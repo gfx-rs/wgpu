5c26d358843fb700199086de107122be310f3ada
SupaMaggie70Incorporated <magnus.larsson.mn@gmail.com>
2025-03-16 01:01:10 -0500

    Hopefully fixed linux, doctest

--- wgpu-types/src/lib.rs
+++ wgpu-types/src/lib.rs
@@ -665,11 +665,11 @@ impl Limits {
             max_subgroup_size: 0,
             max_push_constant_size: 0,
             max_non_sampler_bindings: 1_000_000,
-            max_mesh_invocations_per_workgroup: 0,
-            max_mesh_workgroup_size_x: 0,
-            max_mesh_workgroup_size_y: 0,
-            max_mesh_workgroup_size_z: 0,
-            max_mesh_workgroups_per_dimension: 0,
+            max_mesh_invocations_per_workgroup: 256,
+            max_mesh_workgroup_size_x: 256,
+            max_mesh_workgroup_size_y: 256,
+            max_mesh_workgroup_size_z: 64,
+            max_mesh_workgroups_per_dimension: 65535,
         }
     }
 
@@ -781,6 +781,12 @@ impl Limits {
     ///     max_compute_workgroups_per_dimension: 0, // +
     ///     max_buffer_size: 256 << 20, // (256 MiB),
     ///     max_non_sampler_bindings: 1_000_000,
+    ///
+    ///     max_mesh_invocations_per_workgroup: 0,
+    ///     max_mesh_workgroup_size_x: 0,
+    ///     max_mesh_workgroup_size_y: 0,
+    ///     max_mesh_workgroup_size_z: 0,
+    ///     max_mesh_workgroups_per_dimension: 0,
     /// });
     /// ```
     #[must_use]
