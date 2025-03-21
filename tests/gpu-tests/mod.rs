bfd35a204e11d5a1dda107d4bcfadd08a69c456e
Erich Gubler <erichdongubler@gmail.com>
2025-03-21 14:21:44 -0400

    refactor: make `gpu-tests/regression/` get enumerated like everything else

--- tests/gpu-tests/mod.rs
+++ tests/gpu-tests/mod.rs
@@ -1,7 +1,3 @@
 automod::dir!("gpu-tests/");
 
-mod regression {
-    automod::dir!("gpu-tests/regression/");
-}
-
 wgpu_test::gpu_test_main!();
---
+++ tests/gpu-tests/regression.rs
@@ -0,0 +1,1 @@
+automod::dir!("gpu-tests/regression/");
