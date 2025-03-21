31101cc17b63bc5a8c3fc84336a5113b23dbeaa7
Erich Gubler <erichdongubler@gmail.com>
2025-03-21 12:31:06 -0400

    test: repair `multi-instance` tests

--- tests/validation-tests/api/instance.rs
+++ tests/validation-tests/api/instance.rs
@@ -1,3 +1,39 @@
+mod multi_instance {
+    #![cfg(not(target_arch = "wasm32"))]
+
+    async fn get() -> wgpu::Adapter {
+        let adapter = {
+            let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+                backends: wgpu::Backends::from_env().unwrap_or_default(),
+                ..Default::default()
+            });
+            instance
+                .request_adapter(&wgpu::RequestAdapterOptions::default())
+                .await
+                .unwrap()
+        };
+
+        log::info!("Selected adapter: {:?}", adapter.get_info());
+
+        adapter
+    }
+
+    #[test]
+    pub fn multi_instance() {
+        {
+            env_logger::init();
+
+            // Sequential instances.
+            for _ in 0..3 {
+                pollster::block_on(get());
+            }
+
+            // Concurrent instances
+            let _instances: Vec<_> = (0..3).map(|_| pollster::block_on(get())).collect();
+        }
+    }
+}
+
 mod request_adapter_error {
     fn id(backends: wgpu::Backends) -> wgpu::InstanceDescriptor {
         wgpu::InstanceDescriptor {
