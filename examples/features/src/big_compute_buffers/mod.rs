f0b8675b125191ba28d96024e128d3e580ed4e53
jer <alphastrata@gmail.com>
2025-03-27 18:38:18 +1100

    pipeline want's us to allow allows...

--- examples/features/src/big_compute_buffers/mod.rs
+++ examples/features/src/big_compute_buffers/mod.rs
@@ -220,7 +220,7 @@ fn create_staging_buffers(device: &wgpu::Device, numbers: &[f32]) -> Vec<wgpu::B
         .collect()
 }
 
-#[cfg_attr(target_arch = "wasm32", allow(dead_code))]
+#[cfg_attr(target_arch = "wasm32", allow(clippy::allow_attributes, dead_code))]
 async fn run() {
     let numbers = {
         const BYTES_PER_GB: usize = 1024 * 1024 * 1024;
