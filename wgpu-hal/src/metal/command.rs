53571ee8a1f5e54429d6cfc67782c875fd476d01
Alexander Meißner <AlexanderMeissner@gmx.net>
2025-04-30 19:01:48 +0200

    Implements CommandEncoder::copy_acceleration_structure_to_acceleration_structure().

--- wgpu-hal/src/metal/command.rs
+++ wgpu-hal/src/metal/command.rs
@@ -669,11 +669,22 @@ impl crate::CommandEncoder for super::CommandEncoder {
 
     unsafe fn copy_acceleration_structure_to_acceleration_structure(
         &mut self,
-        _src: &super::AccelerationStructure,
-        _dst: &super::AccelerationStructure,
-        _copy: wgt::AccelerationStructureCopy,
+        src: &super::AccelerationStructure,
+        dst: &super::AccelerationStructure,
+        copy: wgt::AccelerationStructureCopy,
     ) {
-        unimplemented!()
+        let command_encoder = self.enter_acceleration_structure_builder();
+        match copy {
+            wgt::AccelerationStructureCopy::Clone => unsafe {
+                command_encoder
+                    .copyAccelerationStructure_toAccelerationStructure(&src.raw, &dst.raw);
+            },
+            wgt::AccelerationStructureCopy::Compact => {
+                command_encoder.copyAndCompactAccelerationStructure_toAccelerationStructure(
+                    &src.raw, &dst.raw,
+                );
+            }
+        };
     }
 
     unsafe fn begin_query(&mut self, set: &super::QuerySet, index: u32) {
