99ec1d487d0c860e12492c118ad3601e5f3e8cda
Erich Gubler <erichdongubler@gmail.com>
2025-05-21 12:47:59 -0400

    docs for `AsPath`

--- naga/src/as_path.rs
+++ naga/src/as_path.rs
@@ -1,3 +1,5 @@
+//! [`AsPath`] and its supporting items.
+
 use alloc::borrow::Cow;
 
 #[cfg(feature = "std")]
@@ -6,6 +8,14 @@ use std::path::Path;
 #[cfg(not(feature = "std"))]
 use alloc::string::String;
 
+/// A trait that abstracts over types accepted for conversion to the most
+/// featureful path representation possible; that is:
+///
+/// - When `no_std` is active, this represents types can be converted to `Cow<'_, str>`.
+/// - Otherwise, types that implement `AsRef<Path>` (to extract a `&Path`).
+///
+/// This type is used as the type bounds for various diagnostic rendering methods, i.e.,
+/// [`WithSpan::emit_to_string_with_path`](crate::span::WithSpan::emit_to_string_with_path).
 pub trait AsPath {
     #[cfg(feature = "std")]
     fn as_ref(&self) -> &Path;
