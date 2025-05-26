4814122377525197b06ae93f4969e3d8e74698f4
Dmitry Zamkov <dmitry94@gmail.com>
2025-05-26 18:20:57 -0500

    Update feature array size

--- wgpu/src/backend/webgpu.rs
+++ wgpu/src/backend/webgpu.rs
@@ -712,7 +712,7 @@ fn map_map_mode(mode: crate::MapMode) -> u32 {
     }
 }
 
-const FEATURES_MAPPING: [(wgt::Features, webgpu_sys::GpuFeatureName); 14] = [
+const FEATURES_MAPPING: [(wgt::Features, webgpu_sys::GpuFeatureName); 15] = [
     (
         wgt::Features::DEPTH_CLIP_CONTROL,
         webgpu_sys::GpuFeatureName::DepthClipControl,
