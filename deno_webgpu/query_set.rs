e5a200fb04993e167f529dc37c70bc472f0bf187
Andy Leiserson <aleiserson@mozilla.com>
2025-06-09 09:07:39 -0700

    Use full github link

--- deno_webgpu/query_set.rs
+++ deno_webgpu/query_set.rs
@@ -43,9 +43,9 @@ impl GPUQuerySet {
 
     #[fast]
     fn destroy(&self) -> Result<(), JsErrorBox> {
-        // TODO(#6495): Destroy the query set. Until that is supported, it is
-        // okay to do nothing here, the query set will be garbage collected
-        // and dropped eventually.
+        // TODO(https://github.com/gfx-rs/wgpu/issues/6495): Destroy the query
+        // set. Until that is supported, it is okay to do nothing here, the
+        // query set will be garbage collected and dropped eventually.
         Ok(())
     }
 
