2d8d131fc82085702d4d5ec7338c00e27182e86a
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-07-13 02:48:47 -0400

    Add cargo xtask miri

---
+++ xtask/src/miri.rs
@@ -0,0 +1,29 @@
+use pico_args::Arguments;
+use xshell::Shell;
+
+pub fn run_miri(shell: Shell, mut args: Arguments) -> anyhow::Result<()> {
+    let toolchain: String = args
+        .opt_value_from_str("--toolchain")?
+        .unwrap_or_else(|| String::from("nightly"));
+
+    shell
+        .cmd("rustup")
+        .args([
+            "run",
+            &toolchain,
+            "cargo",
+            "miri",
+            "nextest",
+            "run",
+            "--target",
+            "x86_64-unknown-linux-gnu",
+        ])
+        .env(
+            "MIRIFLAGS",
+            "-Zmiri-disable-isolation -Zmiri-deterministic-floats",
+        )
+        .quiet()
+        .run()?;
+
+    Ok(())
+}
