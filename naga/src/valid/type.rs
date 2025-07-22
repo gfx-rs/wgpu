422bc0a35e83fb927844c655382f977a9b3603de
Jamie Nicol <jnicol@mozilla.com>
2025-07-22 09:38:58 +0100

    fixup! [naga wgsl-in wgsl-out] WGSL support for texture_external texture type

--- naga/src/valid/type.rs
+++ naga/src/valid/type.rs
@@ -733,6 +733,13 @@ impl super::Validator {
                     self.require_type_capability(Capabilities::CUBE_ARRAY_TEXTURES)?;
                 }
                 if matches!(class, crate::ImageClass::External) {
+                    if dim != crate::ImageDimension::D2 || arrayed {
+                        return Err(TypeError::UnsupportedImageType {
+                            dim,
+                            arrayed,
+                            class,
+                        });
+                    }
                     self.require_type_capability(Capabilities::TEXTURE_EXTERNAL)?;
                 }
                 TypeInfo::new(
