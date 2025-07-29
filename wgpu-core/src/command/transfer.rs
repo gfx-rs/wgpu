fc1ee23af3c859d8cb2e2f52ffbe2201ffc0426a
Andy Leiserson <aleiserson@mozilla.com>
2025-07-29 11:57:52 -0700

    Fix CopyT2T of multi-layer depth/stencil textures

--- wgpu-core/src/command/transfer.rs
+++ wgpu-core/src/command/transfer.rs
@@ -1,4 +1,4 @@
-use alloc::{format, string::String, sync::Arc, vec, vec::Vec};
+use alloc::{format, string::String, sync::Arc, vec::Vec};
 
 use arrayvec::ArrayVec;
 use thiserror::Error;
@@ -1337,45 +1337,32 @@ impl Global {
                 height: src_copy_size.height.min(dst_copy_size.height),
                 depth: src_copy_size.depth.min(dst_copy_size.depth),
             };
+
+            let regions = (0..array_layer_count).map(|rel_array_layer| {
+                let mut src_base = src_tex_base.clone();
+                let mut dst_base = dst_tex_base.clone();
+                src_base.array_layer += rel_array_layer;
+                dst_base.array_layer += rel_array_layer;
+                hal::TextureCopy {
+                    src_base,
+                    dst_base,
+                    size: hal_copy_size,
+                }
+            });
+
             let regions = if dst_tex_base.aspect == hal::FormatAspects::DEPTH_STENCIL {
-                vec![
-                    hal::TextureCopy {
-                        src_base: hal::TextureCopyBase {
-                            aspect: hal::FormatAspects::DEPTH,
-                            ..src_tex_base
-                        },
-                        dst_base: hal::TextureCopyBase {
-                            aspect: hal::FormatAspects::DEPTH,
-                            ..dst_tex_base
-                        },
-                        size: hal_copy_size,
-                    },
-                    hal::TextureCopy {
-                        src_base: hal::TextureCopyBase {
-                            aspect: hal::FormatAspects::STENCIL,
-                            ..src_tex_base
-                        },
-                        dst_base: hal::TextureCopyBase {
-                            aspect: hal::FormatAspects::STENCIL,
-                            ..dst_tex_base
-                        },
-                        size: hal_copy_size,
-                    },
-                ]
-            } else {
-                (0..array_layer_count)
-                    .map(|rel_array_layer| {
-                        let mut src_base = src_tex_base.clone();
-                        let mut dst_base = dst_tex_base.clone();
-                        src_base.array_layer += rel_array_layer;
-                        dst_base.array_layer += rel_array_layer;
-                        hal::TextureCopy {
-                            src_base,
-                            dst_base,
-                            size: hal_copy_size,
-                        }
+                regions
+                    .flat_map(|region| {
+                        let (mut depth, mut stencil) = (region.clone(), region);
+                        depth.src_base.aspect = hal::FormatAspects::DEPTH;
+                        depth.dst_base.aspect = hal::FormatAspects::DEPTH;
+                        stencil.src_base.aspect = hal::FormatAspects::STENCIL;
+                        stencil.dst_base.aspect = hal::FormatAspects::STENCIL;
+                        [depth, stencil]
                     })
-                    .collect()
+                    .collect::<Vec<_>>()
+            } else {
+                regions.collect::<Vec<_>>()
             };
             let cmd_buf_raw = cmd_buf_data.encoder.open()?;
             unsafe {
