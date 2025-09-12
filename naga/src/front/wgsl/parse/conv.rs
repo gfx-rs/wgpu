333bcb3d5dbd4fbe62c90cc9e30c4ef4414bc858
EriKWDev <ErikWDev@gmail.com>
2025-09-12 12:31:24 +0200

    naga: Correct spelling in wgsl parsing rg11b10float -> rg11b10ufloat in accordance with spec https://www.w3.org/TR/WGSL/#texel-format-rg11b10ufloat

--- naga/src/front/wgsl/parse/conv.rs
+++ naga/src/front/wgsl/parse/conv.rs
@@ -115,7 +115,7 @@ pub fn map_storage_format(word: &str, span: Span) -> Result<'_, crate::StorageFo
         "rgba8sint" => Sf::Rgba8Sint,
         "rgb10a2uint" => Sf::Rgb10a2Uint,
         "rgb10a2unorm" => Sf::Rgb10a2Unorm,
-        "rg11b10float" => Sf::Rg11b10Ufloat,
+        "rg11b10ufloat" => Sf::Rg11b10Ufloat,
         "r64uint" => Sf::R64Uint,
         "rg32uint" => Sf::Rg32Uint,
         "rg32sint" => Sf::Rg32Sint,
