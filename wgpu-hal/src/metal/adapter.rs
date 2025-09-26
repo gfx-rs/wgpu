440964fe2f4f9cd5a2c937ab82e9b76ef0a2cc76
Opstic <46141527+opstic@users.noreply.github.com>
2025-09-26 13:03:10 -0400

    `MTLGPUFamily::Metal4` doesn't exist yet in the version on `metal` we're using

--- wgpu-hal/src/metal/adapter.rs
+++ wgpu-hal/src/metal/adapter.rs
@@ -905,7 +905,6 @@ impl super::PrivateCapabilities {
             // https://developer.apple.com/metal/Metal-Feature-Set-Tables.pdf#page=3
             supports_memoryless_storage: if family_check {
                 device.supports_family(MTLGPUFamily::Apple2)
-                    || device.supports_family(MTLGPUFamily::Metal4)
             } else {
                 version.at_least((11, 0), (10, 0), os_is_mac)
             },
