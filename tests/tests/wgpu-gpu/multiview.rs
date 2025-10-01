bd696d43a179b2c28364df89760d0d9e0624dff0
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-10-01 13:27:11 -0500

    Updated multiview test to provide more information

--- tests/tests/wgpu-gpu/multiview.rs
+++ tests/tests/wgpu-gpu/multiview.rs
@@ -50,7 +50,7 @@ async fn run_test(ctx: TestingContext) {
 
             @fragment
             fn fs_main(@builtin(view_index) view_index: u32) -> @location(0) vec4f {
-                return vec4f(f32(view_index));
+                return vec4f(f32(view_index) * 0.5);
             }
         ";
 
@@ -177,13 +177,15 @@ async fn run_test(ctx: TestingContext) {
     ctx.async_poll(wgpu::PollType::wait()).await.unwrap();
 
     let data = slice.get_mapped_range();
+    let each_texture_size = (TEXTURE_SIZE * TEXTURE_SIZE) as usize;
+    assert!(data.len() == each_texture_size * 2);
     for view_idx in 0..2 {
-        let each_texture_size = (TEXTURE_SIZE * TEXTURE_SIZE) as usize;
-        let target_value = view_idx as u8 * u8::MAX;
-        let succeeded = data[each_texture_size * view_idx..each_texture_size * (view_idx + 1)]
+        // Some metal devices automatically initialize stuff to 255, so I decided to use 128 instead of that
+        let target_value = view_idx as u8 * 128;
+        let failed_value = data[each_texture_size * view_idx..each_texture_size * (view_idx + 1)]
             .iter()
-            .all(|b| *b == target_value);
-        assert!(data.len() == each_texture_size * 2);
-        assert!(succeeded, "Failed with view {view_idx}");
+            .copied()
+            .find(|b| *b != target_value);
+        assert_eq!(failed_value, None);
     }
 }
