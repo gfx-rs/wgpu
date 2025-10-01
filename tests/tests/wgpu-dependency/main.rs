78a63eca3de7e55d1e670d4cafe094d796c3ea8e
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-10-01 19:23:47 -0400

    Fix issues Copilot found

--- tests/tests/wgpu-dependency/main.rs
+++ tests/tests/wgpu-dependency/main.rs
@@ -1,4 +1,4 @@
-//   doesn't compile on wasm due to old cargo-util-schemas dependency.
+// Cargo-metadata doesn't compile on wasm due to old cargo-util-schemas dependency.
 // Since this test isn't dependent on the current architecture, we can just skip it on wasm without any issues.
 #![cfg(not(any(target_arch = "wasm32", miri)))]
 
