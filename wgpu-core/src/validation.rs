b28453f4d1636a03a85a53ed817c602199f2ff21
Inner Daemons <85136135+inner-daemons@users.noreply.github.com>
2025-11-26 11:46:57 -0600

    Update wgpu-core/src/validation.rs
    Co-authored-by: Connor Fitzgerald <connorwadefitzgerald@gmail.com>

--- wgpu-core/src/validation.rs
+++ wgpu-core/src/validation.rs
@@ -347,7 +347,7 @@ pub enum StageError {
     #[error("Primitive index can only be used in a fragment shader if the preceding shader was a vertex shader or a mesh shader that writes to primitive index.
     If a mesh shader writes to primitive index, it must be read by the fragment shader.")]
     PrimitiveIndexError,
-    #[error("Draw id can only be used in a mesh shader if the pipeline has no task shader.")]
+    #[error("DrawId cannot be used in the same pipeline as a task shader")]
     DrawIdError,
 }
 
