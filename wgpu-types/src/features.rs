47d70a005efcb4d6e5a7c30f26db8221026e8f22
Layl <3094382+LaylBongers@users.noreply.github.com>
2025-12-05 13:43:28 +0100

    Remove DX12 from list of supported platforms for `MULTISAMPLE_ARRAY`.
    Co-authored-by: Teodor Tanasoaia <28601907+teoxoy@users.noreply.github.com>

--- wgpu-types/src/features.rs
+++ wgpu-types/src/features.rs
@@ -1246,7 +1246,6 @@ bitflags_array! {
         ///
         /// Supported platforms:
         /// - Vulkan (except VK_KHR_portability_subset if multisampleArrayImage is not available)
-        /// - DX12
         const MULTISAMPLE_ARRAY = 1 << 56;
     }
 
