285a3099482dfee898a8dda84b36342583c98153
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2025-12-05 10:12:47 +0100

    Remove multisample array from DX12 backend.

--- wgpu-hal/src/dx12/adapter.rs
+++ wgpu-hal/src/dx12/adapter.rs
@@ -403,8 +403,7 @@ impl super::Adapter {
             | wgt::Features::FLOAT32_FILTERABLE
             | wgt::Features::TEXTURE_ATOMIC
             | wgt::Features::EXPERIMENTAL_PASSTHROUGH_SHADERS
-            | wgt::Features::EXTERNAL_TEXTURE
-            | wgt::Features::MULTISAMPLE_ARRAY;
+            | wgt::Features::EXTERNAL_TEXTURE;
 
         //TODO: in order to expose this, we need to run a compute shader
         // that extract the necessary statistics out of the D3D12 result.
