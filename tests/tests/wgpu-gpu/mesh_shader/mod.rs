9b1ea2c102d58488525d53713d37e9801356c425
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2025-12-19 01:47:33 -0600

    Unfortunate but not too unexpected at this point

--- tests/tests/wgpu-gpu/mesh_shader/mod.rs
+++ tests/tests/wgpu-gpu/mesh_shader/mod.rs
@@ -387,16 +387,16 @@ fn default_gpu_test_config(draw_type: DrawType) -> GpuTestConfiguration {
                         DrawType::MultiIndirectCount => wgpu::Features::MULTI_DRAW_INDIRECT_COUNT,
                     },
             )
-            .limits(wgpu::Limits::default().using_recommended_minimum_mesh_shader_values()),
-        /* .skip(wgpu_test::FailureCase {
-            backends: None,
-            // Skip Mesa because LLVMPIPE has what is believed to be a driver bug
-            vendor: Some(0x10005),
-            adapter: None,
-            driver: None,
-            reasons: vec![],
-            behavior: wgpu_test::FailureBehavior::Ignore,
-        }),*/
+            .limits(wgpu::Limits::default().using_recommended_minimum_mesh_shader_values())
+            .skip(wgpu_test::FailureCase {
+                backends: None,
+                // Skip Mesa because LLVMPIPE has what is believed to be a driver bug
+                vendor: Some(0x10005),
+                adapter: None,
+                driver: None,
+                reasons: vec![],
+                behavior: wgpu_test::FailureBehavior::Ignore,
+            }),
     )
 }
 
