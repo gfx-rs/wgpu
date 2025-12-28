569234c8cbd7483997d6189f1872659268dcc355
Luo Zhihao <luo_zhihao@outlook.com>
2025-12-29 06:01:09 +0800

    Add comment

--- wgpu-hal/src/vulkan/swapchain/native.rs
+++ wgpu-hal/src/vulkan/swapchain/native.rs
@@ -468,6 +468,7 @@ impl Swapchain for NativeSwapchain {
         // latency, depending on how the platform implements waiting within
         // acquire.
         unsafe {
+            // The `wait_all` argument must be `true` to avoid crash on some Android devices. See https://github.com/gfx-rs/wgpu/pull/8769
             self.device
                 .raw
                 .wait_for_fences(&[self.fence], true, timeout_ns)
