4cea595514cea6e536843ee611789b68d4de12b2
SupaMaggie70 <magnus.larsson.mn@gmail.com>
2026-01-16 02:45:17 -0600

    Slight cleanup

--- naga/src/back/wgsl/writer.rs
+++ naga/src/back/wgsl/writer.rs
@@ -275,11 +275,50 @@ impl<W: Write> Writer<W> {
     /// Helper method which writes all the `enable` declarations
     /// needed for a module.
     fn write_enable_declarations(&mut self, module: &Module) -> BackendResult {
-        let mut needs_f16 = false;
-        let mut needs_dual_source_blending = false;
-        let mut needs_clip_distances = false;
-        let mut needs_mesh_shaders = false;
-        let mut needs_primitive_index = false;
+        #[derive(Default)]
+        struct RequiredEnabled {
+            f16: bool,
+            dual_source_blending: bool,
+            clip_distances: bool,
+            mesh_shaders: bool,
+            primitive_index: bool,
+        }
+        let mut needed = RequiredEnabled::default();
+
+        let check_binding = |binding: &crate::Binding, needed: &mut RequiredEnabled| match *binding
+        {
+            crate::Binding::Location {
+                blend_src: Some(_), ..
+            } => {
+                needed.dual_source_blending = true;
+            }
+            crate::Binding::BuiltIn(crate::BuiltIn::ClipDistance) => {
+                needed.clip_distances = true;
+            }
+            crate::Binding::BuiltIn(crate::BuiltIn::PrimitiveIndex) => {
+                needed.primitive_index = true;
+            }
+            crate::Binding::Location {
+                per_primitive: true,
+                ..
+            } => {
+                needed.mesh_shaders = true;
+            }
+            crate::Binding::BuiltIn(
+                crate::BuiltIn::MeshTaskSize
+                | crate::BuiltIn::CullPrimitive
+                | crate::BuiltIn::PointIndex
+                | crate::BuiltIn::LineIndices
+                | crate::BuiltIn::TriangleIndices
+                | crate::BuiltIn::VertexCount
+                | crate::BuiltIn::Vertices
+                | crate::BuiltIn::PrimitiveCount
+                | crate::BuiltIn::Primitives,
+            ) => {
+                needed.mesh_shaders = true;
+            }
+            _ => {}
+        };
 
         // Determine which `enable` declarations are needed
         for (_, ty) in module.types.iter() {
@@ -287,43 +326,11 @@ impl<W: Write> Writer<W> {
                 TypeInner::Scalar(scalar)
                 | TypeInner::Vector { scalar, .. }
                 | TypeInner::Matrix { scalar, .. } => {
-                    needs_f16 |= scalar == crate::Scalar::F16;
+                    needed.f16 |= scalar == crate::Scalar::F16;
                 }
                 TypeInner::Struct { ref members, .. } => {
                     for binding in members.iter().filter_map(|m| m.binding.as_ref()) {
-                        match *binding {
-                            crate::Binding::Location {
-                                blend_src: Some(_), ..
-                            } => {
-                                needs_dual_source_blending = true;
-                            }
-                            crate::Binding::BuiltIn(crate::BuiltIn::ClipDistance) => {
-                                needs_clip_distances = true;
-                            }
-                            crate::Binding::BuiltIn(crate::BuiltIn::PrimitiveIndex) => {
-                                needs_primitive_index = true;
-                            }
-                            crate::Binding::Location {
-                                per_primitive: true,
-                                ..
-                            } => {
-                                needs_mesh_shaders = true;
-                            }
-                            crate::Binding::BuiltIn(
-                                crate::BuiltIn::MeshTaskSize
-                                | crate::BuiltIn::CullPrimitive
-                                | crate::BuiltIn::PointIndex
-                                | crate::BuiltIn::LineIndices
-                                | crate::BuiltIn::TriangleIndices
-                                | crate::BuiltIn::VertexCount
-                                | crate::BuiltIn::Vertices
-                                | crate::BuiltIn::PrimitiveCount
-                                | crate::BuiltIn::Primitives,
-                            ) => {
-                                needs_mesh_shaders = true;
-                            }
-                            _ => {}
-                        }
+                        check_binding(binding, &mut needed);
                     }
                 }
                 _ => {}
@@ -332,48 +339,18 @@ impl<W: Write> Writer<W> {
 
         for ep in &module.entry_points {
             if matches!(ep.stage, ShaderStage::Mesh | ShaderStage::Task) {
-                needs_mesh_shaders = true;
+                needed.mesh_shaders = true;
             }
-            for binding in ep
+            if let Some(res) = ep.function.result.as_ref().and_then(|a| a.binding.as_ref()) {
+                check_binding(res, &mut needed);
+            }
+            for arg in ep
                 .function
                 .arguments
                 .iter()
                 .filter_map(|a| a.binding.as_ref())
-                .chain(ep.function.result.iter().filter_map(|a| a.binding.as_ref()))
             {
-                match *binding {
-                    crate::Binding::Location {
-                        blend_src: Some(_), ..
-                    } => {
-                        needs_dual_source_blending = true;
-                    }
-                    crate::Binding::BuiltIn(crate::BuiltIn::ClipDistance) => {
-                        needs_clip_distances = true;
-                    }
-                    crate::Binding::BuiltIn(crate::BuiltIn::PrimitiveIndex) => {
-                        needs_primitive_index = true;
-                    }
-                    crate::Binding::Location {
-                        per_primitive: true,
-                        ..
-                    } => {
-                        needs_mesh_shaders = true;
-                    }
-                    crate::Binding::BuiltIn(
-                        crate::BuiltIn::MeshTaskSize
-                        | crate::BuiltIn::CullPrimitive
-                        | crate::BuiltIn::PointIndex
-                        | crate::BuiltIn::LineIndices
-                        | crate::BuiltIn::TriangleIndices
-                        | crate::BuiltIn::VertexCount
-                        | crate::BuiltIn::Vertices
-                        | crate::BuiltIn::PrimitiveCount
-                        | crate::BuiltIn::Primitives,
-                    ) => {
-                        needs_mesh_shaders = true;
-                    }
-                    _ => {}
-                }
+                check_binding(arg, &mut needed);
             }
         }
 
@@ -382,28 +359,28 @@ impl<W: Write> Writer<W> {
             .iter()
             .any(|gv| gv.1.space == crate::AddressSpace::TaskPayload)
         {
-            needs_mesh_shaders = true;
+            needed.mesh_shaders = true;
         }
 
         // Write required declarations
         let mut any_written = false;
-        if needs_f16 {
+        if needed.f16 {
             writeln!(self.out, "enable f16;")?;
             any_written = true;
         }
-        if needs_dual_source_blending {
+        if needed.dual_source_blending {
             writeln!(self.out, "enable dual_source_blending;")?;
             any_written = true;
         }
-        if needs_clip_distances {
+        if needed.clip_distances {
             writeln!(self.out, "enable clip_distances;")?;
             any_written = true;
         }
-        if needs_mesh_shaders {
+        if needed.mesh_shaders {
             writeln!(self.out, "enable wgpu_mesh_shader;")?;
             any_written = true;
         }
-        if needs_primitive_index {
+        if needed.primitive_index {
             writeln!(self.out, "enable primitive_index;")?;
             any_written = true;
         }
