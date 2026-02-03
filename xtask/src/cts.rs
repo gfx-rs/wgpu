2d44631cfa4a8fc9b0c3369ec1428f8cf2e3ebf7
Andy Leiserson <aleiserson@mozilla.com>
2026-02-02 18:25:13 -0800

    env debugging

--- xtask/src/cts.rs
+++ xtask/src/cts.rs
@@ -408,11 +408,22 @@ pub fn run_cts(
             log::info!("Running {}", test.selector.to_string_lossy());
         }
 
-        let cmd = shell
-            .cmd(&bin)
-            .envs(env_vars.clone())
-            .args(cts_bin)
-            .args([&test.selector]);
+        let cmd = if test.selector != "webgpu:shader,execution,expression,call,builtin,textureSample:sampled_1d_coords:*" {
+            shell
+                .cmd(&bin)
+                .envs(env_vars.clone())
+                .args(cts_bin)
+                .args([&test.selector])
+
+        } else {
+            shell
+                .cmd("cargo")
+                .args(&["llvm-cov", "--no-cfg-coverage", "--no-report", "run"])
+                .args(&cargo_opts)
+                .arg("--")
+                .args(cts_bin)
+                .arg(&test.selector)
+        };
 
         match output_filter {
             PrintOutputWhen::TestFails => {
