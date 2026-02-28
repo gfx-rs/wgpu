2bd3ad41f7240985012c6bb9e24f0cccb2886835
Vecvec <vectorsofvectors@gmail.com>
2026-03-01 08:01:56 +1300

    More work on tests

--- tests/tests/wgpu-gpu/ray_tracing/pipelines.rs
+++ tests/tests/wgpu-gpu/ray_tracing/pipelines.rs
@@ -1,10 +1,10 @@
-use wgpu::{Features, Limits};
+use wgpu::{Features, Limits, ShaderModuleDescriptor};
 use wgpu_test::{
     gpu_test, GpuTestConfiguration, GpuTestInitializer, TestParameters, TestingContext,
 };
 
 pub fn all_tests(tests: &mut Vec<GpuTestInitializer>) {
-    tests.extend([PIPELINE_CREATE_USE]);
+    tests.push(PIPELINE_CREATE_USE);
 }
 
 #[gpu_test]
@@ -18,6 +18,66 @@ static PIPELINE_CREATE_USE: GpuTestConfiguration = GpuTestConfiguration::new()
 
 fn pipeline_create_use(ctx: TestingContext) {
     let ray_gen_source = "@group(0) @binding(0) acc_struct: acceleration_structure;
-    
+        var<ray_payload> payload: u32;
+
+        @ray_generation
+        fn gen() {
+            traceRays(acc_struct, RayDesc(), &payload);
+        }
+
+        @closest_hit
+        fn closest() {
+            
+        }
     ";
+
+    let ray_closest_source = "
+        var<incoming_ray_payload> payload: u32;
+
+        @incoming_payload(payload)
+        @closest_hit
+        fn closest() {
+            
+        }
+    ";
+
+    let ray_any_source = "
+        var<incoming_ray_payload> payload: u32;
+
+        @incoming_payload(payload)
+        @any_hit
+        fn any() {
+            
+        }
+    ";
+
+    let ray_miss_source = "
+        var<incoming_ray_payload> payload: u32;
+
+        @incoming_payload(payload)
+        @any_hit
+        fn miss() {
+            
+        }
+    ";
+
+    let ray_gen = ctx.device.create_shader_module(ShaderModuleDescriptor {
+        label: Some("ray generation shader"),
+        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(ray_gen_source)),
+    });
+
+    let ray_closest = ctx.device.create_shader_module(ShaderModuleDescriptor {
+        label: Some("ray closest hit shader"),
+        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(ray_closest_source)),
+    });
+
+    let ray_any = ctx.device.create_shader_module(ShaderModuleDescriptor {
+        label: Some("ray any hit shader"),
+        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(ray_any_source)),
+    });
+
+    let ray_miss = ctx.device.create_shader_module(ShaderModuleDescriptor {
+        label: Some("ray miss shader"),
+        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(ray_miss_source)),
+    });
 }
