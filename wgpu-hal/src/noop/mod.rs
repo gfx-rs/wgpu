0b8809c3a0fcbc5662b3ccb17bba510d92889e1f
Erich Gubler <erichdongubler@gmail.com>
2026-03-05 16:38:03 -0700

    refactor: DRY for `i32::MAX` in `wgpu_hal::noop::CAPABILITIES`

--- wgpu-hal/src/noop/mod.rs
+++ wgpu-hal/src/noop/mod.rs
@@ -157,6 +157,9 @@ pub const CAPABILITIES: crate::Capabilities = {
     /// Guaranteed to be no bigger than isize::MAX which is the maximum size of an allocation,
     /// except on 16-bit platforms which we certainly don’t fit in.
     const ALLOC_MAX_U32: u32 = i32::MAX as u32;
+    /// Guaranteed to be no bigger than isize::MAX which is the maximum size of an allocation,
+    /// except on 16-bit platforms which we certainly don’t fit in.
+    const ALLOC_MAX_U64: u64 = i32::MAX as u64;
 
     crate::Capabilities {
         limits: wgt::Limits {
@@ -176,10 +179,10 @@ pub const CAPABILITIES: crate::Capabilities = {
             max_uniform_buffers_per_shader_stage: ALLOC_MAX_U32,
             max_binding_array_elements_per_shader_stage: ALLOC_MAX_U32,
             max_binding_array_sampler_elements_per_shader_stage: ALLOC_MAX_U32,
-            max_uniform_buffer_binding_size: ALLOC_MAX_U32 as u64,
-            max_storage_buffer_binding_size: ALLOC_MAX_U32 as u64,
+            max_uniform_buffer_binding_size: ALLOC_MAX_U64,
+            max_storage_buffer_binding_size: ALLOC_MAX_U64,
             max_vertex_buffers: ALLOC_MAX_U32,
-            max_buffer_size: ALLOC_MAX_U32 as u64,
+            max_buffer_size: ALLOC_MAX_U64,
             max_vertex_attributes: ALLOC_MAX_U32,
             max_vertex_buffer_array_stride: ALLOC_MAX_U32,
             max_inter_stage_shader_variables: ALLOC_MAX_U32,
