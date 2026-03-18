be4b0899130810578c58b78db32ca9543d4ce4b9
Inner-Daemons <magnus.larsson.mn@gmail.com>
2026-03-17 21:44:48 -0500

    Added to readme & realphabetized examples list

--- examples/features/src/main.rs
+++ examples/features/src/main.rs
@@ -93,6 +93,12 @@ const EXAMPLES: &[ExampleDesc] = &[
         webgpu: true,
     },
     ExampleDesc {
+        name: "render_with_compute",
+        function: wgpu_examples::render_with_compute::main,
+        webgl: false,
+        webgpu: true,
+    },
+    ExampleDesc {
         name: "repeated_compute",
         function: wgpu_examples::repeated_compute::main,
         webgl: false, // No compute
@@ -150,12 +156,6 @@ const EXAMPLES: &[ExampleDesc] = &[
         name: "water",
         function: wgpu_examples::water::main,
         webgl: false, // No RODS
-        webgpu: true,
-    },
-    ExampleDesc {
-        name: "render_with_compute",
-        function: wgpu_examples::render_with_compute::main,
-        webgl: false,
         webgpu: true,
     },
     ExampleDesc {
