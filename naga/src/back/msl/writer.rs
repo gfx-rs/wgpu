1c2db47ad144acc2a36d5b193928f50aaacd4819
Ali <alimilhim5@gmail.com>
2026-04-10 19:39:27 +0300

    Update naga/src/back/msl/writer.rs
    Co-authored-by: atlv <email@atlasdostal.com>

--- naga/src/back/msl/writer.rs
+++ naga/src/back/msl/writer.rs
@@ -4473,16 +4473,11 @@ impl<W: Write> Writer<W> {
                     ref format,
                     ref arguments,
                 } => {
-                    write!(self.out, "{level}")?;
-
-                    write!(self.out, "metal::os_log_default.log_info(\"{}\"", format)?;
-
+                    write!(self.out, "{level}metal::os_log_default.log_info(\"{}\"", format)?;
                     for &arg in arguments {
                         write!(self.out, ", ")?;
-                        // msl backend uses put_expression for code gen
                         self.put_expression(arg, &context.expression, true)?;
                     }
-
                     writeln!(self.out, ");")?;
                 }
             }
