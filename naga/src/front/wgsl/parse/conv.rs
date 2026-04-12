356c1d68d2cd5ea995b7e9ca07a71e32cf8e5170
atlas dostal <rodol@rivalrebels.com>
2026-04-12 09:43:30 -0400

    Rename PerVertex to WgpuPerVertex for consistency

--- naga/src/front/wgsl/parse/conv.rs
+++ naga/src/front/wgsl/parse/conv.rs
@@ -182,7 +182,7 @@ pub fn map_interpolation(
         "flat" => Ok(crate::Interpolation::Flat),
         "perspective" => Ok(crate::Interpolation::Perspective),
         "per_vertex" => {
-            enable_extensions.require(ImplementedEnableExtension::PerVertex, span)?;
+            enable_extensions.require(ImplementedEnableExtension::WgpuPerVertex, span)?;
             Ok(crate::Interpolation::PerVertex)
         }
         _ => Err(Box::new(Error::UnknownAttribute(span))),
