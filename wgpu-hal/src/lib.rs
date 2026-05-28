5a36a7afbd09e038b001ccd2343c841d3a16b4d9
Vecvec <vectorsofvectors@gmail.com>
2026-05-28 14:14:51 +1200

    Format

--- wgpu-hal/src/lib.rs
+++ wgpu-hal/src/lib.rs
@@ -634,7 +634,7 @@ pub trait Api: Clone + fmt::Debug + Sized + WasmNotSendSync + 'static {
     /// before a lower-valued operation, then waiting for the fence to reach the
     /// lower value could return before the lower-valued operation has actually
     /// finished.
-    /// 
+    ///
     /// Fences are internally syncronised by the hal, and so should not need to be
     /// contained in external synchronisation primitives.
     type Fence: DynFence;
@@ -719,7 +719,7 @@ pub trait Surface: WasmNotSendSync {
     /// If you do not wish to display the texture, you must pass the
     /// [`SurfaceTexture`] to [`self.discard_texture`], so that it can be reused
     /// by future acquisitions.
-    /// 
+    ///
     /// The fence is internally synchronised by the hal.
     ///
     /// # Portability
