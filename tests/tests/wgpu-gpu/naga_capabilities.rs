73b2841036a873052f1df6427eac134397ea471e
Andy Leiserson <aleiserson@mozilla.com>
2026-07-10 11:33:21 -0700

    test: Annotate expected failures on Metal and MoltenVK
    Mostly from #9849, but there is one that seems like a newly added test
    matching a pre-existing failure symptom associated with #9184.

--- tests/tests/wgpu-gpu/naga_capabilities.rs
+++ tests/tests/wgpu-gpu/naga_capabilities.rs
@@ -16,7 +16,14 @@ pub fn validate_capabilities(ctx: TestingContext) {
         wgpu::Backend::Vulkan => naga::back::spv::supported_capabilities(),
         // TODO: when mesh shaders land, change this
         wgpu::Backend::Dx12 => naga::back::hlsl::supported_capabilities() | Caps::MESH_SHADER,
-        wgpu::Backend::Metal => naga::back::msl::supported_capabilities() | Caps::MESH_SHADER,
+        wgpu::Backend::Metal => {
+            naga::back::msl::supported_capabilities()
+                | Caps::MESH_SHADER
+                // TODO(https://github.com/gfx-rs/wgpu/issues/9849): mask off
+                // BUFFER_BINDING_ARRAY because it is not currently reported by
+                // naga's MSL backend.
+                | Caps::BUFFER_BINDING_ARRAY
+        }
         wgpu::Backend::Gl => naga::back::glsl::supported_capabilities(),
         wgpu::Backend::BrowserWebGpu => naga::back::wgsl::supported_capabilities(),
         wgpu::Backend::Noop => Caps::all(),
