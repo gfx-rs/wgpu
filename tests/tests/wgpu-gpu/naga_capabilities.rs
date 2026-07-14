4b34512cbf6076abba040326a0add0e65e08c023
Andy Leiserson <aleiserson@mozilla.com>
2026-07-14 14:33:06 -0700

    remove stale mesh shader exception

--- tests/tests/wgpu-gpu/naga_capabilities.rs
+++ tests/tests/wgpu-gpu/naga_capabilities.rs
@@ -14,11 +14,9 @@ pub fn validate_capabilities(ctx: TestingContext) {
     );
     let max_caps = match ctx.adapter.get_info().backend {
         wgpu::Backend::Vulkan => naga::back::spv::supported_capabilities(),
-        // TODO: when mesh shaders land, change this
-        wgpu::Backend::Dx12 => naga::back::hlsl::supported_capabilities() | Caps::MESH_SHADER,
+        wgpu::Backend::Dx12 => naga::back::hlsl::supported_capabilities(),
         wgpu::Backend::Metal => {
             naga::back::msl::supported_capabilities()
-                | Caps::MESH_SHADER
                 // TODO(https://github.com/gfx-rs/wgpu/issues/9849): mask off
                 // BUFFER_BINDING_ARRAY because it is not currently reported by
                 // naga's MSL backend.
