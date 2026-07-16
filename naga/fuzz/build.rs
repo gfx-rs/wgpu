824d0309657e37a97cf693d7a7afc1c1958a6f69
Erich Gubler <erichdongubler@gmail.com>
2026-07-16 14:09:16 +0200

    chore(ci): work around `semicolon_in_expressions_from_macros` lint from `cfg_aliases!`

--- naga/fuzz/build.rs
+++ naga/fuzz/build.rs
@@ -1,3 +1,8 @@
+#![allow(
+    semicolon_in_expressions_from_macros,
+    reason = "work around <https://github.com/katharostech/cfg_aliases/issues/16>"
+)]
+
 fn main() {
     cfg_aliases::cfg_aliases! {
         fuzzable_platform: { not(any(target_arch = "wasm32", target_os = "ios", target_os = "openbsd", all(windows, target_arch = "aarch64"))) },
--- wgpu-hal/build.rs
+++ wgpu-hal/build.rs
@@ -1,3 +1,8 @@
+#![allow(
+    semicolon_in_expressions_from_macros,
+    reason = "work around <https://github.com/katharostech/cfg_aliases/issues/16>"
+)]
+
 fn main() {
     cfg_aliases::cfg_aliases! {
         native: { not(target_family = "wasm") },
