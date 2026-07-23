367ab17b6a6e2631696aff84aa71c46ef0073e76
ozongzi <ozongzi@icloud.com>
2026-07-23 07:26:32 +0000

    hal/vulkan: link OHOS_SURFACE_EXTENSION_NAME to Instance::create_surface_ohos

--- wgpu-hal/src/vulkan/instance.rs
+++ wgpu-hal/src/vulkan/instance.rs
@@ -11,7 +11,7 @@ use arrayvec::ArrayVec;
 use ash::{ext, khr, vk};
 use parking_lot::RwLock;
 
-/// Name of the `VK_OHOS_surface` extension. Used with [`create_surface_ohos`].
+/// Name of the `VK_OHOS_surface` extension. Used with [`super::Instance::create_surface_ohos`].
 #[cfg(target_env = "ohos")]
 const OHOS_SURFACE_EXTENSION_NAME: &CStr = c"VK_OHOS_surface";
 
