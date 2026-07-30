d0ecda96cd1490feda71d9f5c345efee1a105e1c
Kevin Reid <kpreid@switchb.org>
2026-07-30 03:16:43 -0700

    Replace `wgpu-macros` package with use of `macro_rules_attribute`. (#9963)

--- tests/tests/wgpu-gpu/naga_capabilities.rs
+++ tests/tests/wgpu-gpu/naga_capabilities.rs
@@ -1,5 +1,5 @@
 use wgpu_test::{
-    gpu_test, GpuTestConfiguration, GpuTestInitializer, TestParameters, TestingContext,
+    apply, gpu_test, GpuTestConfiguration, GpuTestInitializer, TestParameters, TestingContext,
 };
 
 pub fn all_tests(vec: &mut Vec<GpuTestInitializer>) {
@@ -30,7 +30,7 @@ pub fn validate_capabilities(ctx: TestingContext) {
     assert_eq!(diff, Caps::empty());
 }
 
-#[gpu_test]
+#[apply(gpu_test!)]
 static VALIDATE_CAPABILITIES: GpuTestConfiguration = GpuTestConfiguration::new()
     .parameters(
         TestParameters::default()
