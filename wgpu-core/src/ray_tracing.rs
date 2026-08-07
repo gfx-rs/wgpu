1fc630056b5858c521c7ff1833aee30efd9b1f7c
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-07 07:29:27 +0200

    [core] Do not use IDs as default types in RayTracing types
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- wgpu-core/src/ray_tracing.rs
+++ wgpu-core/src/ray_tracing.rs
@@ -23,9 +23,8 @@ use crate::command::serde_object_reference_struct;
 use crate::{
     command::{ArcReferences, EncoderStateError, IdReferences, ReferenceType},
     device::{DeviceError, MissingFeatures},
-    id::{BlasId, BufferId, TlasId},
     resource::{
-        Blas, BlasCompactCallback, BlasPrepareCompactResult, DestroyedResourceError,
+        self, Blas, BlasCompactCallback, BlasPrepareCompactResult, DestroyedResourceError,
         InvalidResourceError, MissingBufferUsageError, ResourceErrorIdent, Tlas,
     },
 };
@@ -278,7 +277,7 @@ impl WebGpuError for ValidateAsActionsError {
 }
 
 #[derive(Debug)]
-pub struct BlasTriangleGeometry<'a, Buffer = BufferId> {
+pub struct BlasTriangleGeometry<'a, Buffer = Arc<resource::Buffer>> {
     pub size: &'a wgt::BlasTriangleGeometrySizeDescriptor,
     pub vertex_buffer: Buffer,
     pub index_buffer: Option<Buffer>,
@@ -290,40 +289,40 @@ pub struct BlasTriangleGeometry<'a, Buffer = BufferId> {
 }
 
 #[derive(Debug)]
-pub struct BlasAabbGeometry<'a, Buffer = BufferId> {
+pub struct BlasAabbGeometry<'a, Buffer = Arc<resource::Buffer>> {
     pub size: &'a wgt::BlasAABBGeometrySizeDescriptor,
     pub stride: BufferAddress,
     pub aabb_buffer: Buffer,
     pub primitive_offset: u32,
 }
 
-pub enum BlasGeometries<'a, Buffer = BufferId> {
+pub enum BlasGeometries<'a, Buffer = Arc<resource::Buffer>> {
     TriangleGeometries(Box<dyn Iterator<Item = BlasTriangleGeometry<'a, Buffer>> + 'a>),
     AabbGeometries(Box<dyn Iterator<Item = BlasAabbGeometry<'a, Buffer>> + 'a>),
 }
 
-pub struct BlasBuildEntry<'a, Blas = BlasId, Buffer = BufferId> {
+pub struct BlasBuildEntry<'a, Blas = Arc<resource::Blas>, Buffer = Arc<resource::Buffer>> {
     pub blas: Blas,
     pub geometries: BlasGeometries<'a, Buffer>,
 }
 
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct TlasBuildEntry<Tlas = TlasId, Buffer = BufferId> {
+pub struct TlasBuildEntry<Tlas = Arc<resource::Tlas>, Buffer = Arc<resource::Buffer>> {
     pub tlas: Tlas,
     pub instance_buffer: Buffer,
     pub instance_count: u32,
 }
 
 #[derive(Debug)]
-pub struct TlasInstance<'a, Blas = BlasId> {
+pub struct TlasInstance<'a, Blas = Arc<resource::Blas>> {
     pub blas: Blas,
     pub transform: &'a [f32; 12],
     pub custom_data: u32,
     pub mask: u8,
 }
 
-pub struct TlasPackage<'a, Tlas = TlasId, Blas = BlasId> {
+pub struct TlasPackage<'a, Tlas = Arc<resource::Tlas>, Blas = Arc<resource::Blas>> {
     pub tlas: Tlas,
     pub instances: Box<dyn Iterator<Item = Option<TlasInstance<'a, Blas>>> + 'a>,
     pub lowest_unmodified: u32,
