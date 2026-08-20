5d1d6f5eb1550e6569f8ead589dc888421cf5545
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-20 07:24:14 +0200

    [core-remote] Add RenderBundleEncoderCommand type and handle it
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- wgpu-core-remote/src/global/bundle.rs
+++ wgpu-core-remote/src/global/bundle.rs
@@ -1,3 +1,7 @@
+use wgpu_core_remote_types::encoders::{
+    BindingCommand, DebugCommand, RenderBundleEncoderCommand, RenderCommand,
+};
+
 use crate::hub::Hub;
 use crate::id;
 
@@ -182,5 +186,117 @@ impl crate::global::Global {
         let bundle_encoder = hub.render_bundle_encoders.get_mut(bundle_encoder);
 
         bundle_encoder.insert_debug_marker(label)
+    }
+}
+
+impl crate::global::Global {
+    pub fn handle_render_bundle_encoder_command<'a>(
+        &self,
+        bundle_encoder: id::RenderBundleEncoderId,
+        command: RenderBundleEncoderCommand<'a>,
+    ) {
+        match command {
+            RenderBundleEncoderCommand::BindingCommand(binding_command) => match binding_command {
+                BindingCommand::SetBindGroup {
+                    index,
+                    bind_group,
+                    dynamic_offsets,
+                } => self.render_bundle_encoder_set_bind_group(
+                    bundle_encoder,
+                    index,
+                    bind_group,
+                    &dynamic_offsets,
+                ),
+                BindingCommand::SetImmediates { range_offset, data } => {
+                    self.render_bundle_encoder_set_immediates(bundle_encoder, range_offset, &data)
+                }
+            },
+            RenderBundleEncoderCommand::RenderCommand(render_command) => match render_command {
+                RenderCommand::SetPipeline(id) => {
+                    self.render_bundle_encoder_set_pipeline(bundle_encoder, id)
+                }
+                RenderCommand::SetIndexBuffer {
+                    buffer,
+                    index_format,
+                    offset,
+                    size,
+                } => self.render_bundle_encoder_set_index_buffer(
+                    bundle_encoder,
+                    buffer,
+                    index_format,
+                    offset,
+                    size.and_then(core::num::NonZeroU64::new), // pass size directly once https://github.com/gfx-rs/wgpu/issues/3170 is resolved
+                ),
+                RenderCommand::SetVertexBuffer {
+                    slot,
+                    buffer,
+                    offset,
+                    size,
+                } => self.render_bundle_encoder_set_vertex_buffer(
+                    bundle_encoder,
+                    slot,
+                    buffer,
+                    offset,
+                    size.and_then(core::num::NonZeroU64::new), // pass size directly once https://github.com/gfx-rs/wgpu/issues/3170 is resolved
+                ),
+                RenderCommand::Draw {
+                    vertex_count,
+                    instance_count,
+                    first_vertex,
+                    first_instance,
+                } => self.render_bundle_encoder_draw(
+                    bundle_encoder,
+                    vertex_count,
+                    instance_count,
+                    first_vertex,
+                    first_instance,
+                ),
+                RenderCommand::DrawIndexed {
+                    index_count,
+                    instance_count,
+                    first_index,
+                    base_vertex,
+                    first_instance,
+                } => self.render_bundle_encoder_draw_indexed(
+                    bundle_encoder,
+                    index_count,
+                    instance_count,
+                    first_index,
+                    base_vertex,
+                    first_instance,
+                ),
+                RenderCommand::DrawIndirect {
+                    indirect_buffer,
+                    indirect_offset,
+                } => self.render_bundle_encoder_draw_indirect(
+                    bundle_encoder,
+                    indirect_buffer,
+                    indirect_offset,
+                ),
+                RenderCommand::DrawIndexedIndirect {
+                    indirect_buffer,
+                    indirect_offset,
+                } => self.render_bundle_encoder_draw_indexed_indirect(
+                    bundle_encoder,
+                    indirect_buffer,
+                    indirect_offset,
+                ),
+            },
+            RenderBundleEncoderCommand::DebugCommand(debug_command) => match debug_command {
+                DebugCommand::PushDebugGroup(label) => {
+                    self.render_bundle_encoder_push_debug_group(bundle_encoder, &label)
+                }
+                DebugCommand::PopDebugGroup => {
+                    self.render_bundle_encoder_pop_debug_group(bundle_encoder)
+                }
+                DebugCommand::InsertDebugMarker(label) => {
+                    self.render_bundle_encoder_insert_debug_marker(bundle_encoder, &label)
+                }
+            },
+            RenderBundleEncoderCommand::Finish {
+                desc,
+                render_bundle_id,
+            } => self.render_bundle_encoder_finish(bundle_encoder, desc, render_bundle_id),
+        }
     }
 }
