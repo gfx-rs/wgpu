gfx-rs/wgpu

commit 81569dd6c3ad58dea3d42c36c4c4b71087572ca5

Updated Dxc integration for DX12 backend (#3356)
Co-authored-by: unknown <alimilhim5@gmail.com>
Co-authored-by: Connor Fitzgerald <connorwadefitzgerald@gmail.com>
Closes https://github.com/gfx-rs/wgpu/issues/2722
closes https://github.com/gfx-rs/wgpu/pull/3147

wgpu-hal/src/dx12/shader_compilation.rs
@@ -1,0 +1,255 @@
+use std::ptr;
+
+pub(super) use dxc::{compile_dxc, get_dxc_container, DxcContainer};
+use winapi::um::d3dcompiler;
+
+use crate::auxil::dxgi::result::HResult;
+
+// This exists so that users who don't want to use dxc can disable the dxc_shader_compiler feature
+// and not have to compile hassle_rs.
+// Currently this will use Dxc if it is chosen as the dx12 compiler at `Instance` creation time, and will
+// fallback to FXC if the Dxc libraries (dxil.dll and dxcompiler.dll) are not found, or if Fxc is chosen at'
+// `Instance` creation time.
+
+pub(super) fn compile_fxc(
+    device: &super::Device,
+    source: &String,
+    source_name: &str,
+    raw_ep: &std::ffi::CString,
+    stage_bit: wgt::ShaderStages,
+    full_stage: String,
+) -> (
+    Result<super::CompiledShader, crate::PipelineError>,
+    log::Level,
+) {
+    profiling::scope!("compile_fxc");
+    let mut shader_data = native::Blob::null();
+    let mut compile_flags = d3dcompiler::D3DCOMPILE_ENABLE_STRICTNESS;
+    if device
+        .private_caps
+        .instance_flags
+        .contains(crate::InstanceFlags::DEBUG)
+    {
+        compile_flags |= d3dcompiler::D3DCOMPILE_DEBUG | d3dcompiler::D3DCOMPILE_SKIP_OPTIMIZATION;
+    }
+    let mut error = native::Blob::null();
+    let hr = unsafe {
+        profiling::scope!("d3dcompiler::D3DCompile");
+        d3dcompiler::D3DCompile(
+            source.as_ptr().cast(),
+            source.len(),
+            source_name.as_ptr().cast(),
+            ptr::null(),
+            ptr::null_mut(),
+            raw_ep.as_ptr(),
+            full_stage.as_ptr().cast(),
+            compile_flags,
+            0,
+            shader_data.mut_void().cast(),
+            error.mut_void().cast(),
+        )
+    };
+
+    match hr.into_result() {
+        Ok(()) => (
+            Ok(super::CompiledShader::Fxc(shader_data)),
+            log::Level::Info,
+        ),
+        Err(e) => {
+            let mut full_msg = format!("FXC D3DCompile error ({})", e);
+            if !error.is_null() {
+                use std::fmt::Write as _;
+                let message = unsafe {
+                    std::slice::from_raw_parts(
+                        error.GetBufferPointer() as *const u8,
+                        error.GetBufferSize(),
+                    )
+                };
+                let _ = write!(full_msg, ": {}", String::from_utf8_lossy(message));
+                unsafe {
+                    error.destroy();
+                }
+            }
+            (
+                Err(crate::PipelineError::Linkage(stage_bit, full_msg)),
+                log::Level::Warn,
+            )
+        }
+    }
+}
+
+// The Dxc implementation is behind a feature flag so that users who don't want to use dxc can disable the feature.
+#[cfg(feature = "dxc_shader_compiler")]
+mod dxc {
+    use std::path::PathBuf;
+
+    pub(crate) struct DxcContainer {
+        pub compiler: hassle_rs::DxcCompiler,
+        pub library: hassle_rs::DxcLibrary,
+        // Has to be held onto for the lifetime of the device otherwise shaders will fail to compile
+        _dxc: hassle_rs::Dxc,
+    }
+
+    pub(crate) fn get_dxc_container(
+        dxc_path: Option<PathBuf>,
+        dxil_path: Option<PathBuf>,
+    ) -> Result<Option<DxcContainer>, crate::DeviceError> {
+        // Make sure that dxil.dll exists.
+        match hassle_rs::Dxil::new(dxil_path) {
+            Ok(_) => (),
+            Err(e) => {
+                log::warn!("Failed to load dxil.dll. Defaulting to Fxc instead: {}", e);
+                return Ok(None);
+            }
+        };
+
+        let dxc = match hassle_rs::Dxc::new(dxc_path) {
+            Ok(dxc) => dxc,
+            Err(e) => {
+                log::warn!(
+                    "Failed to load dxcompiler.dll. Defaulting to Fxc instead: {}",
+                    e
+                );
+                return Ok(None);
+            }
+        };
+        let dxc_compiler = dxc.create_compiler()?;
+        let dxc_library = dxc.create_library()?;
+
+        Ok(Some(DxcContainer {
+            _dxc: dxc,
+            compiler: dxc_compiler,
+            library: dxc_library,
+        }))
+    }
+
+    pub(crate) fn compile_dxc(
+        device: &crate::dx12::Device,
+        source: &str,
+        source_name: &str,
+        raw_ep: &str,
+        stage_bit: wgt::ShaderStages,
+        full_stage: String,
+        dxc_container: &DxcContainer,
+    ) -> (
+        Result<crate::dx12::CompiledShader, crate::PipelineError>,
+        log::Level,
+    ) {
+        profiling::scope!("compile_dxc");
+        let mut compile_flags = arrayvec::ArrayVec::<&str, 3>::new_const();
+        compile_flags.push("-Ges"); // d3dcompiler::D3DCOMPILE_ENABLE_STRICTNESS
+        if device
+            .private_caps
+            .instance_flags
+            .contains(crate::InstanceFlags::DEBUG)
+        {
+            compile_flags.push("-Zi"); // d3dcompiler::D3DCOMPILE_SKIP_OPTIMIZATION
+            compile_flags.push("-Od"); // d3dcompiler::D3DCOMPILE_DEBUG
+        }
+
+        let blob = match dxc_container
+            .library
+            .create_blob_with_encoding_from_str(source)
+            .map_err(|e| crate::PipelineError::Linkage(stage_bit, format!("DXC blob error: {}", e)))
+        {
+            Ok(blob) => blob,
+            Err(e) => return (Err(e), log::Level::Error),
+        };
+
+        // DXC will automatically validate the shaders during compilation as long as dxil.dll is available, so we don't need to do it ourselves.
+        let compiled = dxc_container.compiler.compile(
+            &blob,
+            source_name,
+            raw_ep,
+            &full_stage,
+            &compile_flags,
+            None,
+            &[],
+        );
+
+        let (result, log_level) = match compiled {
+            Ok(dxc_result) => match dxc_result.get_result() {
+                Ok(dxc_blob) => (
+                    Ok(crate::dx12::CompiledShader::Dxc(dxc_blob.to_vec())),
+                    log::Level::Info,
+                ),
+                Err(e) => (
+                    Err(crate::PipelineError::Linkage(
+                        stage_bit,
+                        format!("DXC compile error: {}", e),
+                    )),
+                    log::Level::Error,
+                ),
+            },
+            Err(e) => (
+                Err(crate::PipelineError::Linkage(
+                    stage_bit,
+                    format!("DXC compile error: {:?}", e),
+                )),
+                log::Level::Error,
+            ),
+        };
+
+        (result, log_level)
+    }
+
+    impl From<hassle_rs::HassleError> for crate::DeviceError {
+        fn from(value: hassle_rs::HassleError) -> Self {
+            match value {
+                hassle_rs::HassleError::Win32Error(e) => {
+                    // TODO: This returns an HRESULT, should we try and use the associated Windows error message?
+                    log::error!("Win32 error: {e:?}");
+                    crate::DeviceError::Lost
+                }
+                hassle_rs::HassleError::LoadLibraryError { filename, inner } => {
+                    log::error!("Failed to load dxc library {filename:?}. Inner error: {inner:?}");
+                    crate::DeviceError::Lost
+                }
+                hassle_rs::HassleError::LibLoadingError(e) => {
+                    log::error!("Failed to load dxc library. {e:?}");
+                    crate::DeviceError::Lost
+                }
+                hassle_rs::HassleError::WindowsOnly(e) => {
+                    log::error!("Signing with dxil.dll is only supported on Windows. {e:?}");
+                    crate::DeviceError::Lost
+                }
+                // `ValidationError` and `CompileError` should never happen in a context involving `DeviceError`
+                hassle_rs::HassleError::ValidationError(_e) => unimplemented!(),
+                hassle_rs::HassleError::CompileError(_e) => unimplemented!(),
+            }
+        }
+    }
+}
+
+// These are stubs for when the `dxc_shader_compiler` feature is disabled.
+#[cfg(not(feature = "dxc_shader_compiler"))]
+mod dxc {
+    use std::path::PathBuf;
+
+    pub(crate) struct DxcContainer {}
+
+    pub(crate) fn get_dxc_container(
+        _dxc_path: Option<PathBuf>,
+        _dxil_path: Option<PathBuf>,
+    ) -> Result<Option<DxcContainer>, crate::DeviceError> {
+        // Falls back to Fxc and logs an error.
+        log::error!("DXC shader compiler was requested on Instance creation, but the DXC feature is disabled. Enable the `dxc_shader_compiler` feature on wgpu_hal to use DXC.");
+        Ok(None)
+    }
+
+    // It shouldn't be possible that this gets called with the `dxc_shader_compiler` feature disabled.
+    pub(crate) fn compile_dxc(
+        _device: &crate::dx12::Device,
+        _source: &str,
+        _source_name: &str,
+        _raw_ep: &str,
+        _stage_bit: wgt::ShaderStages,
+        _full_stage: String,
+        _dxc_container: &DxcContainer,
+    ) -> (
+        Result<crate::dx12::CompiledShader, crate::PipelineError>,
+        log::Level,
+    ) {
+        unimplemented!("Something went really wrong, please report this. Attempted to compile shader with DXC, but the DXC feature is disabled. Enable the `dxc_shader_compiler` feature on wgpu_hal to use DXC.");
+    }
+}

wgpu-hal/src/dx12/mod.rs
@@ -39,6 +39,7 @@ mod conv;
 mod descriptor;
 mod device;
 mod instance;
+mod shader_compilation;
 mod suballocation;
 mod view;
 
@@ -92,6 +93,7 @@ pub struct Instance {
     supports_allow_tearing: bool,
     _lib_dxgi: native::DxgiLib,
     flags: crate::InstanceFlags,
+    dx12_shader_compiler: wgt::Dx12Compiler,
 }
 
 impl Instance {
@@ -173,6 +175,7 @@ pub struct Adapter {
     //Note: this isn't used right now, but we'll need it later.
     #[allow(unused)]
     workarounds: Workarounds,
+    dx12_shader_compiler: wgt::Dx12Compiler,
 }
 
 unsafe impl Send for Adapter {}
@@ -241,6 +244,7 @@ pub struct Device {
     render_doc: crate::auxil::renderdoc::RenderDoc,
     null_rtv_handle: descriptor::Handle,
     mem_allocator: Option<Mutex<suballocation::GpuAllocatorWrapper>>,
+    dxc_container: Option<shader_compilation::DxcContainer>,
 }
 
 unsafe impl Send for Device {}
@@ -536,6 +540,30 @@ pub struct PipelineLayout {
 pub struct ShaderModule {
     naga: crate::NagaShader,
     raw_name: Option<ffi::CString>,
+}
+
+pub(super) enum CompiledShader {
+    #[allow(unused)]
+    Dxc(Vec<u8>),
+    Fxc(native::Blob),
+}
+
+impl CompiledShader {
+    fn create_native_shader(&self) -> native::Shader {
+        match *self {
+            CompiledShader::Dxc(ref shader) => native::Shader::from_raw(shader),
+            CompiledShader::Fxc(shader) => native::Shader::from_blob(shader),
+        }
+    }
+
+    unsafe fn destroy(self) {
+        match self {
+            CompiledShader::Dxc(_) => {}
+            CompiledShader::Fxc(shader) => unsafe {
+                shader.destroy();
+            },
+        }
+    }
 }
 
 pub struct RenderPipeline {

wgpu/tests/common/mod.rs
@@ -331,8 +331,12 @@ pub fn initialize_test(parameters: TestParameters, test_function: impl FnOnce(Te
 }
 
 fn initialize_adapter() -> (Adapter, SurfaceGuard) {
-    let backend_bits = wgpu::util::backend_bits_from_env().unwrap_or_else(Backends::all);
-    let instance = Instance::new(backend_bits);
+    let backends = wgpu::util::backend_bits_from_env().unwrap_or_else(Backends::all);
+    let dx12_shader_compiler = wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default();
+    let instance = Instance::new(wgpu::InstanceDescriptor {
+        backends,
+        dx12_shader_compiler,
+    });
     let surface_guard;
     let compatible_surface;
 
@@ -358,7 +362,7 @@ fn initialize_adapter() -> (Adapter, SurfaceGuard) {
     let compatible_surface: Option<&Surface> = compatible_surface.as_ref();
     let adapter = pollster::block_on(wgpu::util::initialize_adapter_from_env_or_default(
         &instance,
-        backend_bits,
+        backends,
         compatible_surface,
     ))
     .expect("could not find suitable adapter on the system");

Cargo.toml
@@ -100,6 +100,7 @@ gpu-allocator = { version = "0.21", default_features = false, features = ["d3d12
 native = { package = "d3d12", version = "0.5.0" }
 range-alloc = "0.1"
 winapi = "0.3"
+hassle-rs = "0.9.0"
 
 # Gles dependencies
 egl = { package = "khronos-egl", version = "4.1" }

deno_webgpu/src/lib.rs
@@ -252,7 +252,10 @@ pub async fn op_webgpu_request_adapter(
         state.put(wgpu_core::hub::Global::new(
             "webgpu",
             wgpu_core::hub::IdentityManagerFactory,
-            backends,
+            wgpu_types::InstanceDescriptor {
+                backends,
+                dx12_shader_compiler: wgpu_types::Dx12Compiler::Fxc,
+            },
         ));
         state.borrow::<Instance>()
     };

player/src/bin/play.rs
@@ -43,7 +43,11 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    let global = wgc::hub::Global::new("player", IdentityPassThroughFactory, wgt::Backends::all());
+    let global = wgc::hub::Global::new(
+        "player",
+        IdentityPassThroughFactory,
+        wgt::InstanceDescriptor::default(),
+    );
     let mut command_buffer_id_manager = wgc::hub::IdentityManager::default();
 
     #[cfg(feature = "winit")]

player/tests/test.rs
@@ -178,7 +178,14 @@ impl Corpus {
         let dir = path.parent().unwrap();
         let corpus: Corpus = ron::de::from_reader(File::open(&path).unwrap()).unwrap();
 
-        let global = wgc::hub::Global::new("test", IdentityPassThroughFactory, corpus.backends);
+        let global = wgc::hub::Global::new(
+            "test",
+            IdentityPassThroughFactory,
+            wgt::InstanceDescriptor {
+                backends: corpus.backends,
+                dx12_shader_compiler: wgt::Dx12Compiler::Fxc,
+            },
+        );
         for &backend in BACKENDS {
             if !corpus.backends.contains(backend.into()) {
                 continue;

wgpu-core/src/hub.rs
@@ -1108,10 +1108,10 @@ pub struct Global<G: GlobalIdentityHandlerFactory> {
 }
 
 impl<G: GlobalIdentityHandlerFactory> Global<G> {
-    pub fn new(name: &str, factory: G, backends: wgt::Backends) -> Self {
+    pub fn new(name: &str, factory: G, instance_desc: wgt::InstanceDescriptor) -> Self {
         profiling::scope!("Global::new");
         Self {
-            instance: Instance::new(name, backends),
+            instance: Instance::new(name, instance_desc),
             surfaces: Registry::without_backend(&factory, "Surface"),
             hubs: Hubs::new(&factory),
         }

commit 4f142124b483bb414b3f5968ed24d88ff7f57131

[dx12] anchor the DXC source name to the current directory
`compile_dxc` passes the shader module's label to DXC as the first positional
argument, which DXC reads as the input file's path. The virtual file system
registers the source blob under `./<label>`, but hands clang the label
unprefixed, and clang's lookup probes the file's parent directory.

`llvm::sys::path::filename_pos` splits a slash-less path at its last colon, to
support drive-relative `C:file` paths. So `Renderer::shader` gets a parent
directory of `Renderer:`, which matches nothing, and the compile fails with
`error reading '<label>'`, giving no hint that the label is at fault.

Any single colon triggers this, not just `::`; `A:B` escapes only because `A:`
is a real drive designator that clang special-cases. Verified against statically
linked DXC on Windows 10: `Renderer:shader`, `Renderer::shader` and `A::B` fail,
while `./Renderer::shader`, `a/Renderer::shader`, `shader:` and `A:B` compile.
`/`, `-`, spaces, `;` and `*` are unaffected. FXC is not affected, it uses the
name for diagnostics only.

Not a regression from the windows-rs migration: `IDxcCompiler::Compile`, which
hassle-rs used before it, takes the name as a dedicated `pSourceName` parameter
and fails identically. This has been broken since #3356 (v0.15.0).

Prefix the name with `./` so a slash is always present and the colon split never
applies, and drop an empty label rather than passing a bare `./`, which is a
directory.

wgpu-hal/src/dx12/shader_compilation.rs
@@ -373,9 +373,15 @@ fn compile_dxc(
 ) -> Result<crate::dx12::CompiledShader, crate::PipelineError> {
     profiling::scope!("compile_dxc");
 
-    let source_name = source_name.and_then(|cstr| cstr.to_str().ok());
-
-    let source_name = source_name.map(OPCWSTR::new);
+    // DXC reads this name as the input file's path, then probes its parent
+    // directory. A name with a colon but no slash is parsed as a `C:file`
+    // drive-relative path there and never resolves, so anchor it to the current
+    // directory. An empty label would leave a bare `./`, which would error since
+    // it is a directory, so we filter those.
+    let source_name = source_name
+        .and_then(|cstr| cstr.to_str().ok())
+        .filter(|name| !name.is_empty())
+        .map(|name| OPCWSTR::new(&format!("./{name}")));
     let raw_ep = OPCWSTR::new(raw_ep);
     let full_stage = OPCWSTR::new(full_stage);
 

tests/tests/wgpu-gpu/shader/mod.rs
@@ -18,6 +18,7 @@ use wgpu_test::{GpuTestInitializer, TestingContext};
 pub mod array_size_overrides;
 pub mod compilation_messages;
 pub mod data_builtins;
+pub mod module_label;
 pub mod numeric_builtins;
 pub mod struct_layout;
 pub mod workgroup_size_overrides;
@@ -27,6 +28,7 @@ pub fn all_tests(tests: &mut Vec<GpuTestInitializer>) {
     array_size_overrides::all_tests(tests);
     compilation_messages::all_tests(tests);
     data_builtins::all_tests(tests);
+    module_label::all_tests(tests);
     numeric_builtins::all_tests(tests);
     struct_layout::all_tests(tests);
     workgroup_size_overrides::all_tests(tests);

tests/tests/wgpu-gpu/shader/module_label.rs
@@ -1,0 +1,60 @@
+//! Shader module labels are handed to the backend's shader compiler, which may
+//! interpret them as more than an opaque string. DXC in particular reads the
+//! label as the input file's path, so a label containing a `:` used to fail to
+//! compile at all.
+
+use wgpu::{DownlevelFlags, Limits};
+use wgpu_test::{apply, gpu_test, GpuTestConfiguration, TestParameters, TestingContext};
+
+pub fn all_tests(vec: &mut Vec<wgpu_test::GpuTestInitializer>) {
+    vec.push(SHADER_MODULE_LABEL_PUNCTUATION);
+}
+
+const SHADER: &str = r#"
+    @compute @workgroup_size(1)
+    fn main() {}
+"#;
+
+/// Labels containing punctuation that a shader compiler might read as path
+/// syntax must not affect compilation.
+#[apply(gpu_test!)]
+static SHADER_MODULE_LABEL_PUNCTUATION: GpuTestConfiguration = GpuTestConfiguration::new()
+    .parameters(
+        TestParameters::default()
+            .downlevel_flags(DownlevelFlags::COMPUTE_SHADERS)
+            .limits(Limits::downlevel_defaults()),
+    )
+    .run_sync(|ctx| {
+        for label in [
+            "Renderer::shader",
+            "Renderer:shader",
+            "a::b::c",
+            "renderer/shader",
+            "C:/shader",
+            "shader with spaces",
+            "",
+        ] {
+            create_pipeline(&ctx, label);
+        }
+    });
+
+fn create_pipeline(ctx: &TestingContext, label: &str) {
+    let module = ctx
+        .device
+        .create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some(label),
+            source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(SHADER)),
+        });
+
+    // The backend compiles the shader here, not in `create_shader_module`.
+    let _pipeline = ctx
+        .device
+        .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some(label),
+            layout: None,
+            module: &module,
+            entry_point: Some("main"),
+            compilation_options: Default::default(),
+            cache: None,
+        });
+}