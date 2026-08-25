5e353dc71307bbc911f1068c3981293cd9b39322
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-25 23:22:04 +0200

    [remote] RenderPipelineDescriptor
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- wgpu-core-remote-types/src/pipelines.rs
+++ wgpu-core-remote-types/src/pipelines.rs
@@ -39,3 +39,56 @@ pub struct ComputePipelineDescriptor<'a> {
     /// The compiled compute stage and its entry point.
     pub stage: ProgrammableStageDescriptor<'a>,
 }
+
+/// Describes how the vertex buffer is interpreted.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct VertexBufferLayout<'a> {
+    /// The stride, in bytes, between elements of this buffer.
+    pub array_stride: wgt::BufferAddress,
+    /// How often this vertex buffer is "stepped" forward.
+    pub step_mode: wgt::VertexStepMode,
+    /// The list of attributes which comprise a single vertex.
+    pub attributes: Cow<'a, [wgt::VertexAttribute]>,
+}
+
+/// Describes the vertex process in a render pipeline.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+/// cbindgen:ignore
+pub struct VertexState<'a> {
+    /// The compiled vertex stage and its entry point.
+    pub stage: ProgrammableStageDescriptor<'a>,
+    /// The format of any vertex buffers used with this pipeline.
+    pub buffers: Cow<'a, [Option<VertexBufferLayout<'a>>]>,
+}
+
+/// Describes fragment processing in a render pipeline.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+/// cbindgen:ignore
+pub struct FragmentState<'a> {
+    /// The compiled fragment stage and its entry point.
+    pub stage: ProgrammableStageDescriptor<'a>,
+    /// The effect of draw calls on the color aspect of the output target.
+    pub targets: Cow<'a, [Option<wgt::ColorTargetState>]>,
+}
+
+/// Describes a render (graphics) pipeline.
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct RenderPipelineDescriptor<'a> {
+    pub label: Label<'a>,
+    /// The layout of bind groups for this pipeline.
+    pub layout: Option<id::PipelineLayoutId>,
+    /// The vertex processing state for this pipeline.
+    pub vertex: VertexState<'a>,
+    /// The properties of the pipeline at the primitive assembly and rasterization level.
+    #[serde(default)]
+    pub primitive: wgt::PrimitiveState,
+    /// The effect of draw calls on the depth and stencil aspects of the output target, if any.
+    #[serde(default)]
+    pub depth_stencil: Option<wgt::DepthStencilState>,
+    /// The multi-sampling properties of the pipeline.
+    #[serde(default)]
+    pub multisample: wgt::MultisampleState,
+    /// The fragment processing state for this pipeline.
+    pub fragment: Option<FragmentState<'a>>,
+}
