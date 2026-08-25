f13251b93de6d33239469b8c8f8258df18d5fc7f
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-25 21:52:35 +0200

    [core-remote] support importing and resolving all resources
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- wgpu-core-remote/src/global/mod.rs
+++ wgpu-core-remote/src/global/mod.rs
@@ -1,13 +1,19 @@
 use alloc::sync::Arc;
 use core::cell::RefCell;
 use core::fmt;
-use wgpu_core::binding_model::{BindGroupLayout, PipelineLayout};
-use wgpu_core::command::{CommandBuffer, CommandEncoder};
+use wgpu_core::binding_model::{BindGroup, BindGroupLayout, PipelineLayout};
+use wgpu_core::command::{
+    CommandBuffer, CommandEncoder, ComputePass, RenderBundle, RenderBundleEncoder, RenderPass,
+};
 use wgpu_core::device::queue::Queue;
 use wgpu_core::device::Device;
 use wgpu_core::instance::{Adapter, Instance};
-use wgpu_core::pipeline::{ComputePipeline, RenderPipeline};
-use wgpu_core::resource::{Buffer, QuerySet, Texture};
+use wgpu_core::pipeline::{ComputePipeline, RenderPipeline, ShaderModule};
+use wgpu_core::resource::{Buffer, ExternalTexture, QuerySet, Sampler, Texture, TextureView};
+use wgpu_core_remote_types::id::{
+    BindGroupId, ComputePassEncoderId, ExternalTextureId, RenderBundleEncoderId, RenderBundleId,
+    RenderPassEncoderId, SamplerId, ShaderModuleId, TextureViewId,
+};
 
 use crate::hub::Hub;
 use crate::id::{
@@ -117,6 +123,22 @@ impl Global {
         self.hub.borrow().pipeline_layouts.get(pipeline_layout_id)
     }
 
+    /// Import [`Arc<ShaderModule>`] into the global hub,
+    /// returning a [`ShaderModuleId`] under which the shader module is stored.
+    pub fn import_shader_module(
+        &self,
+        shader_module: Arc<ShaderModule>,
+        id_in: ShaderModuleId,
+    ) -> ShaderModuleId {
+        let mut hub = self.hub.borrow_mut();
+        hub.shader_modules.assign(id_in, shader_module)
+    }
+
+    /// Resolve a [`ShaderModuleId`] to the corresponding [`Arc<ShaderModule>`] in the global hub.
+    pub fn resolve_shader_module_id(&self, shader_module_id: ShaderModuleId) -> Arc<ShaderModule> {
+        self.hub.borrow().shader_modules.get(shader_module_id)
+    }
+
     /// Import [`Arc<BindGroupLayout>`] into the global hub,
     /// returning a [`BindGroupLayoutId`] under which the bind group layout is stored.
     pub fn import_bind_group_layout(
@@ -137,6 +159,18 @@ impl Global {
             .borrow()
             .bind_group_layouts
             .get(bind_group_layout_id)
+    }
+
+    /// Import [`Arc<BindGroup>`] into the global hub,
+    /// returning a [`BindGroupId`] under which the bind group is stored.
+    pub fn import_bind_group(&self, bind_group: Arc<BindGroup>, id_in: BindGroupId) -> BindGroupId {
+        let mut hub = self.hub.borrow_mut();
+        hub.bind_groups.assign(id_in, bind_group)
+    }
+
+    /// Resolve a [`BindGroupId`] to the corresponding [`Arc<BindGroup>`] in the global hub.
+    pub fn resolve_bind_group_id(&self, bind_group_id: BindGroupId) -> Arc<BindGroup> {
+        self.hub.borrow().bind_groups.get(bind_group_id)
     }
 
     /// Import [`Arc<CommandEncoder>`] into the global hub,
@@ -175,6 +209,22 @@ impl Global {
         command_buffer_id: CommandBufferId,
     ) -> Arc<CommandBuffer> {
         self.hub.borrow().command_buffers.get(command_buffer_id)
+    }
+
+    /// Import [`Arc<RenderBundle>`] into the global hub,
+    /// returning a [`RenderBundleId`] under which the render bundle is stored.
+    pub fn import_render_bundle(
+        &self,
+        render_bundle: Arc<RenderBundle>,
+        id_in: RenderBundleId,
+    ) -> RenderBundleId {
+        let mut hub = self.hub.borrow_mut();
+        hub.render_bundles.assign(id_in, render_bundle)
+    }
+
+    /// Resolve a [`RenderBundleId`] to the corresponding [`Arc<RenderBundle>`] in the global hub.
+    pub fn resolve_render_bundle_id(&self, render_bundle_id: RenderBundleId) -> Arc<RenderBundle> {
+        self.hub.borrow().render_bundles.get(render_bundle_id)
     }
 
     /// Import [`Arc<RenderPipeline>`] into the global hub,
@@ -249,6 +299,87 @@ impl Global {
     /// Resolve a [`TextureId`] to the corresponding [`Arc<Texture>`] in the global hub.
     pub fn resolve_texture_id(&self, texture_id: TextureId) -> Arc<Texture> {
         self.hub.borrow().textures.get(texture_id)
+    }
+
+    /// Import [`Arc<TextureView>`] into the global hub,
+    /// returning a [`TextureViewId`] under which the texture view is stored.
+    pub fn import_texture_view(
+        &self,
+        texture_view: Arc<TextureView>,
+        id_in: TextureViewId,
+    ) -> TextureViewId {
+        let mut hub = self.hub.borrow_mut();
+        hub.texture_views.assign(id_in, texture_view)
+    }
+
+    /// Resolve a [`TextureViewId`] to the corresponding [`Arc<TextureView>`] in the global hub.
+    pub fn resolve_texture_view_id(&self, texture_view_id: TextureViewId) -> Arc<TextureView> {
+        self.hub.borrow().texture_views.get(texture_view_id)
+    }
+
+    /// Import [`Arc<ExternalTexture>`] into the global hub,
+    /// returning a [`ExternalTextureId`] under which the external texture is stored.
+    pub fn import_external_texture(
+        &self,
+        external_texture: Arc<ExternalTexture>,
+        id_in: ExternalTextureId,
+    ) -> ExternalTextureId {
+        let mut hub = self.hub.borrow_mut();
+        hub.external_textures.assign(id_in, external_texture)
+    }
+
+    /// Resolve a [`ExternalTextureId`] to the corresponding [`Arc<ExternalTexture>`] in the global hub.
+    pub fn resolve_external_texture_id(
+        &self,
+        external_texture_id: ExternalTextureId,
+    ) -> Arc<ExternalTexture> {
+        self.hub.borrow().external_textures.get(external_texture_id)
+    }
+
+    /// Import [`Arc<Sampler>`] into the global hub,
+    /// returning a [`SamplerId`] under which the sampler is stored.
+    pub fn import_sampler(&self, sampler: Arc<Sampler>, id_in: SamplerId) -> SamplerId {
+        let mut hub = self.hub.borrow_mut();
+        hub.samplers.assign(id_in, sampler)
+    }
+
+    /// Resolve a [`SamplerId`] to the corresponding [`Arc<Sampler>`] in the global hub.
+    pub fn resolve_sampler_id(&self, sampler_id: SamplerId) -> Arc<Sampler> {
+        self.hub.borrow().samplers.get(sampler_id)
+    }
+
+    /// Import [`RenderPass`] into the global hub,
+    /// returning a [`RenderPassEncoderId`] under which the render pass is stored.
+    pub fn import_render_pass(
+        &self,
+        render_pass: RenderPass,
+        id_in: RenderPassEncoderId,
+    ) -> RenderPassEncoderId {
+        let mut hub = self.hub.borrow_mut();
+        hub.render_passes.assign(id_in, render_pass)
+    }
+
+    /// Import [`ComputePass`] into the global hub,
+    /// returning a [`ComputePassEncoderId`] under which the compute pass is stored.
+    pub fn import_compute_pass(
+        &self,
+        compute_pass: ComputePass,
+        id_in: ComputePassEncoderId,
+    ) -> ComputePassEncoderId {
+        let mut hub = self.hub.borrow_mut();
+        hub.compute_passes.assign(id_in, compute_pass)
+    }
+
+    /// Import [`RenderBundleEncoder`] into the global hub,
+    /// returning a [`RenderBundleEncoderId`] under which the render bundle encoder is stored.
+    pub fn import_render_bundle_encoder(
+        &self,
+        render_bundle_encoder: RenderBundleEncoder,
+        id_in: RenderBundleEncoderId,
+    ) -> RenderBundleEncoderId {
+        let mut hub = self.hub.borrow_mut();
+        hub.render_bundle_encoders
+            .assign(id_in, render_bundle_encoder)
     }
 }
 
