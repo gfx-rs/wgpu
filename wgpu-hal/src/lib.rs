b218862c563ac76c486e3c05ec9a88628cb965ce
Vecvec <vectorsofvectors@gmail.com>
2026-08-29 08:01:31 +1200

    Documentation

--- wgpu-hal/src/lib.rs
+++ wgpu-hal/src/lib.rs
@@ -1203,6 +1203,9 @@ pub trait Device: WasmNotSendSync {
         &self,
         acceleration_structure: <Self::A as Api>::AccelerationStructure,
     );
+    /// Converts the `TlasInstance` into a implementation defined format, appending it to
+    /// `to_extend`. The vector must be have a length exactly the old length plus
+    /// `Alignments::raw_tlas_instance_size`
     fn tlas_instance_to_bytes(&self, instance: TlasInstance, to_extend: &mut Vec<u8>);
 
     fn get_internal_counters(&self) -> wgt::HalCounters;
