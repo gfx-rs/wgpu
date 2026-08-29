fae070bf21fa5b1ea46e6621d035afaa6cbb9673
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2026-08-29 18:59:58 -0400

    Convert wiki and api-specs into documentation modules (#9779)

---
+++ wgpu/src/documentation/internals/reporting_driver_bugs.rs
@@ -0,0 +1,35 @@
+/*!
+# Reporting Driver Bugs
+
+It can sometimes be hard to figure out where to report driver bugs to the
+vendor. Here is a collection of known places for issues to go, and whether
+reports have been successful.
+
+Proprietary:
+
+- NVIDIA Windows/Linux:
+  [forum](https://forums.developer.nvidia.com/c/gaming-and-visualization-technologies/apis/489)
+  (success)
+- AMD (Windows — GL/VK):
+  [forum](https://community.amd.com/t5/opengl-vulkan/bd-p/opengl-and-vulkan-discussions)
+  (unknown)
+- Intel (Windows):
+  [GitHub](https://github.com/IGCIT/Intel-GPU-Community-Issue-Tracker-IGCIT)
+  (unknown)
+- Qualcomm:
+  [forum](https://developer.qualcomm.com/forums/software/adreno-gpu-sdk)
+  (success)
+- WARP: ping `@soldieroflight` in `#dx12-developers` on the
+  [DX12 Discord](https://discord.gg/directx)
+
+Open source:
+
+- AMDVLK (Linux): [GitHub](https://github.com/GPUOpen-Drivers/AMDVLK)
+- Mesa: [GitLab](https://gitlab.freedesktop.org/mesa/mesa)
+  - AMD (Linux) RADV
+  - Intel (Linux)
+  - Raspberry Pi
+  - llvmpipe
+  - lavapipe
+- MoltenVK: [GitHub](https://github.com/KhronosGroup/MoltenVK)
+*/
