68b407883f720ce22386eb68fd5b763ac72e95e5
sagudev <16504129+sagudev@users.noreply.github.com>
2026-08-29 08:13:15 +0200

    Generalize `BufferDescriptor` and use `BufferUsagesWebGPU` in remote
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- wgpu-core-remote/src/global/device.rs
+++ wgpu-core-remote/src/global/device.rs
@@ -70,7 +70,17 @@ impl Global {
 
         let device = devices.get(device_id);
 
-        let buffer = device.create_buffer(desc);
+        let desc = resource::BufferDescriptor {
+            label: desc.label.as_ref().map(|s| Cow::Borrowed(s.deref())),
+            size: desc.size,
+            usage: wgt::BufferUsages::from_internal_flags(
+                desc.usage,
+                wgt::BufferUsagesWGPU::empty(),
+            ),
+            mapped_at_creation: desc.mapped_at_creation,
+        };
+
+        let buffer = device.create_buffer(&desc);
 
         buffers.assign(id_in, buffer);
     }
@@ -114,7 +124,16 @@ impl Global {
             buffers, devices, ..
         } = &mut *hub;
         let device = devices.get(device_id);
-        buffers.assign(id_in, resource::Buffer::invalid(device, desc));
+        let desc = resource::BufferDescriptor {
+            label: desc.label.as_ref().map(|s| Cow::Borrowed(s.deref())),
+            size: desc.size,
+            usage: wgt::BufferUsages::from_internal_flags(
+                desc.usage,
+                wgt::BufferUsagesWGPU::empty(),
+            ),
+            mapped_at_creation: desc.mapped_at_creation,
+        };
+        buffers.assign(id_in, resource::Buffer::invalid(device, &desc));
     }
 
     /// Assign `id_in` an error with the given `label`.
@@ -295,7 +314,17 @@ impl Global {
 
         let device = devices.get(device_id);
 
-        let (buffer, err) = unsafe { device.create_buffer_from_hal(Box::new(hal_buffer), desc) };
+        let desc = resource::BufferDescriptor {
+            label: desc.label.as_ref().map(|s| Cow::Borrowed(s.deref())),
+            size: desc.size,
+            usage: wgt::BufferUsages::from_internal_flags(
+                desc.usage,
+                wgt::BufferUsagesWGPU::empty(),
+            ),
+            mapped_at_creation: desc.mapped_at_creation,
+        };
+
+        let (buffer, err) = unsafe { device.create_buffer_from_hal(Box::new(hal_buffer), &desc) };
 
         let id = buffers.assign(id_in, buffer);
 
